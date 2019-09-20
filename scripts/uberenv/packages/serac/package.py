87e9230386a58bd0d4a6f5ed4eced9aaf1cc51df
Adrien M. Bernede <bernede1@llnl.gov>
2019-09-20 13:44:02 -0700

    Host-config file generation in Spack package
    This is the method use by uberenv to retrieve the dependencies
    installed with spack. This is in fact more general than the
    uberenv scope.

--- scripts/uberenv/packages/serac/package.py
+++ scripts/uberenv/packages/serac/package.py
@@ -22,6 +22,25 @@
 
 from spack import *
 
+import socket
+import os
+
+import llnl.util.tty as tty
+from os import environ as env
+
+
+def cmake_cache_entry(name, value, vtype=None):
+    """
+    Helper that creates CMake cache entry strings used in
+    'host-config' files.
+    """
+    if vtype is None:
+        if value == "ON" or value == "OFF":
+            vtype = "BOOL"
+        else:
+            vtype = "PATH"
+    return 'set({0} "{1}" CACHE {2} "")\n\n'.format(name, value, vtype)
+
 
 class Serac(CMakePackage):
     """FIXME: Put a proper description of your package here."""
@@ -54,4 +73,121 @@ class Serac(CMakePackage):
             '-DSUPERLUDIST_DIR=%s' % spec['superlu-dist'].prefix
             ]
         return args
+
+    def create_host_config(self, spec, prefix, py_site_pkgs_dir=None):
+        """
+        This method creates a 'host-config' file that specifies
+        all of the options used to configure and build serac.
+
+        For more details about 'host-config' files see:
+            http://software.llnl.gov/conduit/building.html
+
+        Note:
+          The `py_site_pkgs_dir` arg exists to allow a package that
+          subclasses this package provide a specific site packages
+          dir when calling this function. `py_site_pkgs_dir` should
+          be an absolute path or `None`.
+
+          This is necessary because the spack `site_packages_dir`
+          var will not exist in the base class. For more details
+          on this issue see: https://github.com/spack/spack/issues/6261
+        """
+
+        #######################
+        # Compiler Info
+        #######################
+        c_compiler = env["SPACK_CC"]
+        cpp_compiler = env["SPACK_CXX"]
+        f_compiler = None
+
+        if self.compiler.fc:
+            # even if this is set, it may not exist so do one more sanity check
+            f_compiler = which(env["SPACK_FC"])
+
+        #######################################################################
+        # By directly fetching the names of the actual compilers we appear
+        # By directly fetching the names of the actual compilers we appear
+        # to doing something evil here, but this is necessary to create a
+        # 'host config' file that works outside of the spack install env.
+        #######################################################################
+
+        sys_type = spec.architecture
+        # if on llnl systems, we can use the SYS_TYPE
+        if "SYS_TYPE" in env:
+            sys_type = env["SYS_TYPE"]
+
+        ##############################################
+        # Find and record what CMake is used
+        ##############################################
+
+        cmake_exe = spec['cmake'].command.path
+        host_cfg_fname = "%s-%s-%s-serac.cmake" % (socket.gethostname(),
+                                                     sys_type,
+                                                     spec.compiler)
+
+        cfg = open(host_cfg_fname, "w")
+        cfg.write("##################################\n")
+        cfg.write("# spack generated host-config\n")
+        cfg.write("##################################\n")
+        cfg.write("# {0}-{1}\n".format(sys_type, spec.compiler))
+        cfg.write("##################################\n\n")
+
+        # Include path to cmake for reference
+        cfg.write("# cmake from spack \n")
+        cfg.write("# cmake executable path: %s\n\n" % cmake_exe)
+
+        #######################
+        # Compiler Settings
+        #######################
+
+        cfg.write("#######\n")
+        cfg.write("# using %s compiler spec\n" % spec.compiler)
+        cfg.write("#######\n\n")
+        cfg.write("# c compiler used by spack\n")
+        cfg.write(cmake_cache_entry("CMAKE_C_COMPILER", c_compiler))
+        cfg.write("# cpp compiler used by spack\n")
+        cfg.write(cmake_cache_entry("CMAKE_CXX_COMPILER", cpp_compiler))
+
+        #######################
+        # MPI
+        #######################
+
+        cfg.write(cmake_cache_entry("ENABLE_MPI", "ON"))
+        cfg.write(cmake_cache_entry("MPI_C_COMPILER", spec['mpi'].mpicc))
+        cfg.write(cmake_cache_entry("MPI_CXX_COMPILER",
+                                    spec['mpi'].mpicxx))
+        cfg.write(cmake_cache_entry("MPI_Fortran_COMPILER",
+                                    spec['mpi'].mpifc))
+        mpiexe_bin = join_path(spec['mpi'].prefix.bin, 'mpiexec')
+        if os.path.isfile(mpiexe_bin):
+            # starting with cmake 3.10, FindMPI expects MPIEXEC_EXECUTABLE
+            # vs the older versions which expect MPIEXEC
+            if self.spec["cmake"].satisfies('@3.10:'):
+                cfg.write(cmake_cache_entry("MPIEXEC_EXECUTABLE",
+                                            mpiexe_bin))
+            else:
+                cfg.write(cmake_cache_entry("MPIEXEC",
+                                                mpiexe_bin))
+
+        #######################
+        # Adding dependencies
+        #######################
+
+        cfg.write(cmake_cache_entry("MFEM_DIR", spec['mfem'].prefix))
+        cfg.write(cmake_cache_entry("HYPRE_DIR", spec['hypre'].prefix))
+        cfg.write(cmake_cache_entry("PARMETIS_DIR", spec['parmetis'].prefix))
+        cfg.write(cmake_cache_entry("METIS_DIR", spec['metis'].prefix))
+        cfg.write(cmake_cache_entry("SUPERLUDIST_DIR", spec['superlu-dist'].prefix))
+        cfg.write(cmake_cache_entry("LAPACK_DIR", spec['openblas'].prefix))
+        cfg.write(cmake_cache_entry("BLAS_DIR", spec['openblas'].prefix))
+        cfg.write(cmake_cache_entry("ZLIB_DIR", spec['zlib'].prefix))
+
+        cfg.write("##################################\n")
+        cfg.write("# end spack generated host-config\n")
+        cfg.write("##################################\n")
+        cfg.close()
+
+        host_cfg_fname = os.path.abspath(host_cfg_fname)
+        tty.info("spack generated conduit host-config file: " + host_cfg_fname)
+        return host_cfg_fname
 
