5ae964bef42bc74ca8d1914e61c5886a470fcaf6
Adrien M. Bernede <bernede1@llnl.gov>
2019-10-16 17:48:10 -0700

    remove perso defaults

--- cmake/defaults.cmake
+++ cmake/defaults.cmake
@@ -8,14 +8,14 @@
 # Third party libraries
 ###########################################################
 
-set(CMAKE_CXX_COMPILER "/Users/bernede1/Projects/spack/opt/spack/darwin-mojave-x86_64/gcc-9.1.0/openmpi-3.1.4-cabeyqe7om44xcdyam6wkcfy5mksh6bs/bin/mpicxx" CACHE PATH "")
-set(CMAKE_C_COMPILER "/Users/bernede1/Projects/spack/opt/spack/darwin-mojave-x86_64/gcc-9.1.0/openmpi-3.1.4-cabeyqe7om44xcdyam6wkcfy5mksh6bs/bin/mpicc" CACHE PATH "")
-set(CMAKE_Fortran_COMPILER "/Users/bernede1/Projects/spack/opt/spack/darwin-mojave-x86_64/gcc-9.1.0/openmpi-3.1.4-cabeyqe7om44xcdyam6wkcfy5mksh6bs/bin/mpifort" CACHE PATH "")
-set(MFEM_DIR "/Users/bernede1/Projects/spack/opt/spack/darwin-mojave-x86_64/gcc-9.1.0/mfem-3.4.0-oho2e4bg5numofvptbzndxx3x4ga2nvu" CACHE PATH "")
-set(HYPRE_DIR "/Users/bernede1/Projects/spack/opt/spack/darwin-mojave-x86_64/gcc-9.1.0/hypre-2.15.1-jg5kwzgoejfyalyunmvx3m5y4a5cf7op" CACHE PATH "")
-set(PARMETIS_DIR "/Users/bernede1/Projects/spack/opt/spack/darwin-mojave-x86_64/gcc-9.1.0/parmetis-4.0.3-jq6ocj2i2ewijy7j6wexjsysxcfeijcs" CACHE PATH "")
-set(METIS_DIR "/Users/bernede1/Projects/spack/opt/spack/darwin-mojave-x86_64/gcc-9.1.0/metis-5.1.0-awnn5boapegc64ylvbi6tq3um2li63gw" CACHE PATH "")
-set(SUPERLUDIST_DIR "/Users/bernede1/Projects/spack/opt/spack/darwin-mojave-x86_64/gcc-9.1.0/superlu-dist-6.1.1-uyldl4d2hmblr5ddhul53iz5ssyx6tlz" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "" CACHE PATH "")
+set(CMAKE_C_COMPILER "" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "" CACHE PATH "")
+set(MFEM_DIR "" CACHE PATH "")
+set(HYPRE_DIR "" CACHE PATH "")
+set(PARMETIS_DIR "" CACHE PATH "")
+set(METIS_DIR "" CACHE PATH "")
+set(SUPERLUDIST_DIR "" CACHE PATH "")
 
 
 ###########################################################
