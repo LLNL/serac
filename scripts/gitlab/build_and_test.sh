740ebec08236ca1a455bf70677519ee8e1893bf5
Adrien M. Bernede <bernede1@llnl.gov>
2019-11-01 09:21:26 -0700

    Adapting ci to change in uberenv

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -10,7 +10,7 @@ set -e
 
 PROJECT_DIRECTORY="$(pwd)"
 BUILD_DIRECTORY="${BUILD_ROOT}/build_${SYS_TYPE}_${COMPILER}"
-CCONF="${INSTALL_ROOT}_${COMPILER}/${SYS_TYPE}__${COMPILER}__serac.cmake"
+CCONF="${BUILD_ROOT}/${SYS_TYPE}__${COMPILER}__serac.cmake"
 
 # If building, then delete everything first
 if [[ "${1}" != "--test-only" ]]
