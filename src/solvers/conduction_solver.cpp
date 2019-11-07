411d0cdc979872726fc841cc9087b9900f65d55e
Jamie A. Bramwell <bramwell1@llnl.gov>
2019-11-07 09:19:30 -0800

    Added a conduction test, still not working

--- src/solvers/conduction_solver.cpp
+++ src/solvers/conduction_solver.cpp
@@ -6,25 +6,23 @@
 
 #include "conduction_solver.hpp"
 
-using namespace mfem;
-
-ConductionSolver::ConductionSolver(ParFiniteElementSpace &f, double kap)
-   : TimeDependentOperator(f.GetTrueVSize(), 0.0), m_fe_space(f), m_M_form(nullptr), m_K_form(nullptr),
+ConductionSolver::ConductionSolver(mfem::ParFiniteElementSpace &f, double kap)
+   : mfem::TimeDependentOperator(f.GetTrueVSize(), 0.0), m_fe_space(f), m_M_form(nullptr), m_K_form(nullptr),
      m_T_mat(nullptr), m_current_dt(0.0),
      m_M_solver(f.GetComm()), m_T_solver(f.GetComm()), m_z(height)
 {
    const double rel_tol = 1e-8;
    m_kappa = kap;
 
-   m_M_form = new ParBilinearForm(&m_fe_space);
-   m_M_form->AddDomainIntegrator(new MassIntegrator());
+   m_M_form = new mfem::ParBilinearForm(&m_fe_space);
+   m_M_form->AddDomainIntegrator(new mfem::MassIntegrator());
    m_M_form->Assemble(0); // keep sparsity pattern of M and K the same
    m_M_form->FormSystemMatrix(m_ess_tdof_list, m_M_mat);
 
    mfem::ConstantCoefficient kappa_coef(m_kappa);
    
-   m_K_form = new ParBilinearForm(&m_fe_space);
-   m_K_form->AddDomainIntegrator(new DiffusionIntegrator(kappa_coef));
+   m_K_form = new mfem::ParBilinearForm(&m_fe_space);
+   m_K_form->AddDomainIntegrator(new mfem::DiffusionIntegrator(kappa_coef));
    m_K_form->Assemble(0); // keep sparsity pattern of M and K the same
    m_K_form->FormSystemMatrix(m_ess_tdof_list, m_K_mat);
    
@@ -33,7 +31,7 @@ ConductionSolver::ConductionSolver(ParFiniteElementSpace &f, double kap)
    m_M_solver.SetAbsTol(0.0);
    m_M_solver.SetMaxIter(100);
    m_M_solver.SetPrintLevel(0);
-   m_M_prec.SetType(HypreSmoother::Jacobi);
+   m_M_prec.SetType(mfem::HypreSmoother::Jacobi);
    m_M_solver.SetPreconditioner(m_M_prec);
    m_M_solver.SetOperator(m_M_mat);
    
---
+++ tests/serac_conduction_solver.cpp
@@ -0,0 +1,122 @@
+// Copyright (c) 2019, Lawrence Livermore National Security, LLC and
+// other Serac Project Developers. See the top-level LICENSE file for
+// details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause) 
+
+#include <gtest/gtest.h>
+
+#include "mfem.hpp"
+#include "solvers/conduction_solver.hpp"
+#include <fstream>
+
+using namespace std;
+using namespace mfem;
+
+double InitialTemperature(const Vector &x);
+
+TEST(dynamic_solver, dyn_solve)
+{
+   MPI_Barrier(MPI_COMM_WORLD);
+
+   // mesh
+   const char *mesh_file = "../../data/beam-hex.mesh";
+
+   // Open the mesh
+   ifstream imesh(mesh_file);
+   Mesh* mesh = new Mesh(imesh, 1, 1, true);
+   imesh.close();
+
+   // declare pointer to parallel mesh object
+   ParMesh *pmesh = NULL;
+   mesh->UniformRefinement();
+   
+   pmesh = new ParMesh(MPI_COMM_WORLD, *mesh);
+   delete mesh;
+
+   int dim = pmesh->Dimension();
+
+   ODESolver *ode_solver = new BackwardEulerSolver;
+   
+   // Define the finite element spaces for temperature field
+   H1_FECollection fe_coll(2, dim);
+   ParFiniteElementSpace fe_space(pmesh, &fe_coll);
+
+   ParGridFunction u_gf(&fe_space);
+   
+   FunctionCoefficient u_0(InitialTemperature);
+   u_gf.ProjectCoefficient(u_0);
+   Vector u;
+   u_gf.GetTrueDofs(u);
+   u_gf.SetFromTrueDofs(u);
+   
+   int myid;
+   MPI_Comm_rank(MPI_COMM_WORLD, &myid);      
+   
+   {
+      ostringstream mesh_name, sol_name;
+      mesh_name << "mesh." << setfill('0') << setw(6) << myid;
+      ofstream omesh(mesh_name.str().c_str());
+      omesh.precision(8);
+      pmesh->Print(omesh);
+      ofstream osol(sol_name.str().c_str());
+      osol.precision(8);
+      u_gf.Save(osol);
+   }
+   
+   // 9. Initialize the conduction operator and the VisIt visualization.
+   ConductionSolver oper(fe_space, 0.5);
+
+   ode_solver->Init(oper);
+   double t = 0.0;
+   double t_final = 500.0;
+   double dt = 10.0;
+   
+   bool last_step = false;
+   for (int ti = 1; !last_step; ti++)
+   {
+      if (t + dt >= t_final - dt/2)
+      {
+         last_step = true;
+      }
+      ode_solver->Step(u, t, dt);
+   }
+
+   {
+      ostringstream sol_name;
+      sol_name << "final." << setfill('0') << setw(6) << myid;
+      ofstream osol(sol_name.str().c_str());
+      osol.precision(8);
+      u_gf.Save(osol);
+   }
+   
+   delete pmesh;
+   
+   MPI_Barrier(MPI_COMM_WORLD);
+}
+
+
+int main(int argc, char* argv[])
+{
+  int result = 0;
+
+  ::testing::InitGoogleTest(&argc, argv);
+
+  MPI_Init(&argc, &argv);
+  result = RUN_ALL_TESTS();
+  MPI_Finalize();
+
+  return result;
+}
+
+double InitialTemperature(const Vector &x)
+{
+   if (x(0) < 4.0)
+   {
+      return 10.0;
+   }
+   else
+   {
+      return 1.0;
+   }
+}
