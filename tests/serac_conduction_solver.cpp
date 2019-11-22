593f739ba5b67ddc1ea4add451433e4fbb582e9e
Jamie A. Bramwell <bramwell1@llnl.gov>
2019-11-21 20:57:39 -0800

    Added norm check

--- tests/serac_conduction_solver.cpp
+++ tests/serac_conduction_solver.cpp
@@ -2,7 +2,7 @@
 // other Serac Project Developers. See the top-level LICENSE file for
 // details.
 //
-// SPDX-License-Identifier: (BSD-3-Clause) 
+// SPDX-License-Identifier: (BSD-3-Clause)
 
 #include <gtest/gtest.h>
 
@@ -27,7 +27,7 @@ TEST(dynamic_solver, dyn_solve)
    // declare pointer to parallel mesh object
    mfem::ParMesh *pmesh = NULL;
    mesh->UniformRefinement();
-   
+
    pmesh = new mfem::ParMesh(MPI_COMM_WORLD, *mesh);
    delete mesh;
 
@@ -36,41 +36,30 @@ TEST(dynamic_solver, dyn_solve)
    int dim = pmesh->Dimension();
 
    mfem::ODESolver *ode_solver = new mfem::BackwardEulerSolver;
-   
+
    // Define the finite element spaces for temperature field
    mfem::H1_FECollection fe_coll(2, dim);
    mfem::ParFiniteElementSpace fe_space(pmesh, &fe_coll);
 
    mfem::ParGridFunction u_gf(&fe_space);
-   
+
    mfem::FunctionCoefficient u_0(InitialTemperature);
    u_gf.ProjectCoefficient(u_0);
    mfem::Vector u;
    u_gf.GetTrueDofs(u);
    u_gf.SetFromTrueDofs(u);
-   
+
    int myid;
-   MPI_Comm_rank(MPI_COMM_WORLD, &myid);      
-   
-   {
-      std::ostringstream mesh_name, sol_name;
-      mesh_name << "mesh." << std::setfill('0') << std::setw(6) << myid;
-      std::ofstream omesh(mesh_name.str().c_str());
-      omesh.precision(8);
-      pmesh->Print(omesh);
-      std::ofstream osol(sol_name.str().c_str());
-      osol.precision(8);
-      u_gf.Save(osol);
-   }
-   
-   // 9. Initialize the conduction operator and the VisIt visualization.
+   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+   // Initialize the conduction operator and the VisIt visualization.
    ConductionSolver oper(fe_space, 0.5);
 
    ode_solver->Init(oper);
    double t = 0.0;
    double t_final = 5.0;
    double dt = 1.0;
-   
+
    bool last_step = false;
    for (int ti = 1; !last_step; ti++)
    {
@@ -83,16 +72,11 @@ TEST(dynamic_solver, dyn_solve)
 
    u_gf.SetFromTrueDofs(u);
 
-   {
-      std::stringstream sol_name;
-      sol_name << "final." << std::setfill('0') << std::setw(6) << myid;
-      std::ofstream osol(sol_name.str().c_str());
-      osol.precision(8);
-      u_gf.Save(osol);
-   }
-   
+   mfem::ConstantCoefficient zero(0.0);
+   EXPECT_NEAR(2.53521, x_norm, 0.00001);
+
    delete pmesh;
-   
+
    MPI_Barrier(MPI_COMM_WORLD);
 }
 
