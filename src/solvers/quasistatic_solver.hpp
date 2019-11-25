90e65d3279919d4e081a1b2eb4660ad8d00cef4d
Jamie A. Bramwell <bramwell1@llnl.gov>
2019-11-25 13:53:43 -0800

    Fixed whitespace

--- src/solvers/quasistatic_solver.hpp
+++ src/solvers/quasistatic_solver.hpp
@@ -2,7 +2,7 @@
 // other Serac Project Developers. See the top-level LICENSE file for
 // details.
 //
-// SPDX-License-Identifier: (BSD-3-Clause) 
+// SPDX-License-Identifier: (BSD-3-Clause)
 
 #ifndef QUASISTATIC_SOLVER
 #define QUASISTATIC_SOLVER
@@ -14,7 +14,7 @@ class QuasistaticSolver : public mfem::Operator
 protected:
    mfem::ParFiniteElementSpace &fe_space;
    mfem::ParNonlinearForm *Hform;
-   
+
    mutable mfem::Operator *Jacobian;
    const mfem::Vector *x;
 
@@ -24,13 +24,13 @@ protected:
    mfem::Solver *J_solver;
    /// Preconditioner for the Jacobian
    mfem::Solver *J_prec;
-   /// nonlinear material model 
+   /// nonlinear material model
    mfem::HyperelasticModel *model;
 
 public:
    QuasistaticSolver(mfem::ParFiniteElementSpace &fes,
                        mfem::Array<int> &ess_bdr,
-                       mfem::Array<int> &trac_bdr,                         
+                       mfem::Array<int> &trac_bdr,
                        double mu,
                        double K,
                        mfem::VectorCoefficient &trac_coef,
@@ -43,7 +43,7 @@ public:
    /// Required to use the native newton solver
    virtual mfem::Operator &GetGradient(const mfem::Vector &x) const;
    virtual void Mult(const mfem::Vector &k, mfem::Vector &y) const;
-   
+
    /// Driver for the newton solver
    int Solve(mfem::Vector &x) const;
 
