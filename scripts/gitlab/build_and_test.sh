88fff09fba9d78fa6a1a2783012036f05b38d6d1
Chris White <white238@llnl.gov>
2019-12-13 16:52:07 -0800

    Fix systype hostconfig collision

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -10,7 +10,7 @@ set -e
 
 PROJECT_DIRECTORY="$(pwd)"
 BUILD_DIRECTORY="${BUILD_ROOT}/build_${SYS_TYPE}_${COMPILER}"
-CCONF="${BUILD_ROOT}/${COMPILER}.cmake"
+CCONF="${BUILD_ROOT}/${SYS_TYPE}_${COMPILER}.cmake"
 
 # If building, then delete everything first
 if [[ "${1}" != "--test-only" ]]
