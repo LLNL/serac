8c6feeab1df0684f9b0cab31be1de30094d8a9a3
Jamie A. Bramwell <bramwell1@llnl.gov>
2020-01-30 14:50:19 -0800

    Add comments

--- src/solvers/thermal_solver.cpp
+++ src/solvers/thermal_solver.cpp
@@ -15,18 +15,22 @@ ThermalSolver::ThermalSolver(int order, mfem::ParMesh *pmesh) :
   m_fecolls.SetSize(1);
   m_fespaces.SetSize(1);
 
+  // Use H1 nodal basis functions for the temperature field
   m_fecolls[0] = new mfem::H1_FECollection(order, pmesh->Dimension());
   m_fespaces[0] = new mfem::ParFiniteElementSpace(pmesh, m_fecolls[0]);
 
+  // Initialize the state grid function
   m_state_gf.SetSize(1);
   m_state_gf[0] = new mfem::ParGridFunction(m_fespaces[0]);
 
+  // Initialize the state true dof vector
   m_true_vec.SetSize(1);
   m_true_vec[0] = new mfem::HypreParVector(m_fespaces[0]);
 }
 
 void ThermalSolver::SetInitialState(mfem::Coefficient &temp)
 {
+  // Project the coefficient onto the grid function
   m_state_gf[0]->ProjectCoefficient(temp);
   m_gf_initialized = true;
 }
@@ -35,27 +39,33 @@ void ThermalSolver::SetTemperatureBCs(mfem::Array<int> &ess_bdr, mfem::Coefficie
 {
   SetEssentialBCs(ess_bdr, ess_bdr_coef);
 
+  // Get the essential dof indicies and project the coefficient onto them
   m_fespaces[0]->GetEssentialTrueDofs(ess_bdr, m_ess_tdof_list);
   m_state_gf[0]->ProjectBdrCoefficient(*ess_bdr_coef, ess_bdr);
 }
 
 void ThermalSolver::SetFluxBCs(mfem::Array<int> &nat_bdr, mfem::Coefficient *nat_bdr_coef)
 {
+  // Set the natural (integral) boundary condition
   SetNaturalBCs(nat_bdr, nat_bdr_coef);
 }
 
 void ThermalSolver::SetConductivity(mfem::Coefficient &kappa)
 {
+  // Set the conduction coefficient
   m_kappa = &kappa;
 }
 
 void ThermalSolver::SetSource(mfem::Coefficient &source)
 {
+  // Set the body source integral coefficient
   m_source = &source;
 }
 
 void ThermalSolver::SetLinearSolverParameters(const LinearSolverParameters &params)
 {
+  // Save the solver params object
+  // TODO: separate the M and K solver params
   m_lin_params = params;
 }
 
@@ -63,15 +73,18 @@ void ThermalSolver::CompleteSetup(const bool allow_dynamic)
 {
   MFEM_ASSERT(m_kappa != nullptr, "Conductivity not set in ThermalSolver!");
 
+  // Set the true dof vector from the state grid function
   m_state_gf[0]->GetTrueDofs(*m_true_vec[0]);
 
+  // Add the domain diffusion integrator to the K form and assemble the matrix
   m_K_form = new mfem::ParBilinearForm(m_fespaces[0]);
   m_K_form->AddDomainIntegrator(new mfem::DiffusionIntegrator(*m_kappa));
   m_K_form->Assemble(0); // keep sparsity pattern of M and K the same
   m_K_form->Finalize();
 
+  // Add the body source to the RS if specified
   m_l_form = new mfem::ParLinearForm(m_fespaces[0]);
-  if (m_source != nullptr && m_nat_bdr_coef == nullptr) {
+  if (m_source != nullptr) {
     m_l_form->AddDomainIntegrator(new mfem::DomainLFIntegrator(*m_source));
     m_rhs = m_l_form->ParallelAssemble();
   } else {
@@ -79,10 +92,14 @@ void ThermalSolver::CompleteSetup(const bool allow_dynamic)
     *m_rhs = 0.0;
   }
 
+  // Assemble the stiffness matrix
   m_K_mat = m_K_form->ParallelAssemble();
+
+  // Eliminate the essential DOFs from the stiffness matrix and save the RHS
   m_K_mat->EliminateRowsCols(m_ess_tdof_list, *m_true_vec[0], *m_rhs);
 
   if (allow_dynamic) {
+    // If dynamic, assemble the mass matrix
     m_M_mat = new mfem::HypreParMatrix;
     m_M_form = new mfem::ParBilinearForm(m_fespaces[0]);
     m_M_form->AddDomainIntegrator(new mfem::MassIntegrator());
@@ -90,9 +107,12 @@ void ThermalSolver::CompleteSetup(const bool allow_dynamic)
     m_M_form->Finalize();
 
     m_M_mat = m_M_form->ParallelAssemble();
+
+    // Eliminate the essential DOFs from the mass matrix
     mfem::HypreParMatrix *Me = m_M_mat->EliminateRowsCols(m_ess_tdof_list);
     delete Me;
 
+    // Make the time integration operator and set the appropriate matricies
     m_dyn_oper = new DynamicConductionOperator(m_fespaces[0]->GetComm(), m_fespaces[0]->GetTrueVSize(), m_lin_params);
     m_dyn_oper->SetMMatrix(m_M_mat);
     m_dyn_oper->SetKMatrixAndRHS(m_K_mat, m_rhs);
@@ -102,6 +122,8 @@ void ThermalSolver::CompleteSetup(const bool allow_dynamic)
 
 void ThermalSolver::StaticSolve()
 {
+  // Solve the stiffness using CG with Jacobi preconditioning
+  // and the given solverparams
   m_K_solver = new mfem::CGSolver(m_fespaces[0]->GetComm());
   m_K_prec = new mfem::HypreSmoother();
 
@@ -114,7 +136,10 @@ void ThermalSolver::StaticSolve()
   m_K_solver->SetPreconditioner(*m_K_prec);
   m_K_solver->SetOperator(*m_K_mat);
 
+  // Perform the linear solve
   m_K_solver->Mult(*m_rhs, *m_true_vec[0]);
+
+  // Distribute the shared DOFs
   m_state_gf[0]->SetFromTrueDofs(*m_true_vec[0]);
 }
 
@@ -122,9 +147,13 @@ void ThermalSolver::AdvanceTimestep(double dt)
 {
   MFEM_ASSERT(m_dynamic == true, "Solver not setup with dynamic option!");
 
+  // Step the time integrator
   m_ode_solver->Step(*m_true_vec[0], m_time, dt);
+
+  // Distribute the shared DOFs
   m_state_gf[0]->SetFromTrueDofs(*m_true_vec[0]);
 
+  // Increment the time and cycle
   m_time += dt;
   m_cycle += 1;
 }
@@ -149,6 +178,7 @@ DynamicConductionOperator::DynamicConductionOperator(MPI_Comm comm, int height, 
   : mfem::TimeDependentOperator(height, 0.0), m_M_solver(nullptr), m_T_solver(nullptr), m_M_prec(nullptr),
     m_T_prec(nullptr), m_M_mat(nullptr), m_K_mat(nullptr), m_T_mat(nullptr), m_true_rhs(nullptr), m_z(height)
 {
+  // Set the mass solver options (CG and Jacobi for now)
   m_M_solver = new mfem::CGSolver(comm);
   m_M_prec = new mfem::HypreSmoother();
   m_M_solver->iterative_mode = false;
@@ -159,6 +189,7 @@ DynamicConductionOperator::DynamicConductionOperator(MPI_Comm comm, int height, 
   m_M_prec->SetType(mfem::HypreSmoother::Jacobi);
   m_M_solver->SetPreconditioner(*m_M_prec);
 
+  // Use the same options for the T (= M + dt K) solver
   m_T_solver = new mfem::CGSolver(comm);
   m_T_prec = new mfem::HypreSmoother();
   m_T_solver->iterative_mode = false;
@@ -172,12 +203,14 @@ DynamicConductionOperator::DynamicConductionOperator(MPI_Comm comm, int height, 
 
 void DynamicConductionOperator::SetMMatrix(mfem::HypreParMatrix *M_mat)
 {
+  // Set the mass matrix
   m_M_mat = M_mat;
   m_M_solver->SetOperator(*m_M_mat);
 }
 
 void DynamicConductionOperator::SetKMatrixAndRHS(mfem::HypreParMatrix *K_mat, mfem::Vector *rhs)
 {
+  // Set the stiffness matrix and RHS
   m_K_mat = K_mat;
   m_true_rhs = rhs;
 }
