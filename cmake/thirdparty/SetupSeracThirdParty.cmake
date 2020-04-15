670f4f316144f5a99b29a218d5956cb6f37a5e54
Chris White <white238@llnl.gov>
2020-04-15 16:01:23 -0700

    Fix Axom importing

--- cmake/thirdparty/SetupSeracThirdParty.cmake
+++ cmake/thirdparty/SetupSeracThirdParty.cmake
@@ -18,22 +18,25 @@ if(AXOM_DIR)
         set(_target_file ${AXOM_DIR}/lib/cmake/${_library}-targets.cmake)
 
         if(NOT EXISTS ${_target_file})
-            MESSAGE(FATAL_ERROR "Could not find Axom CMake exported target file (${_target_file})")
+            message(FATAL_ERROR "Could not find Axom CMake exported target file (${_target_file})")
         endif()
 
         include(${_target_file})
+
+        if(TARGET ${_library})
+            message(STATUS "Axom CMake exported library loaded: ${_library}")
+        else()
+            message(FATAL_ERROR "Could not load Axom CMake exported library: ${_library}")
+        endif()
 
         # Set include dir to system
         set_property(TARGET ${_library}
                     APPEND PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES
                     ${AXOM_INCLUDE_DIR})
-    endforeach()
 
-    # Sets AXOM_FOUND if AXOM_INCLUDE_DIRS and AXOM_LIBRARIES are not empty
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(AXOM  DEFAULT_MSG
-                                      AXOM_INCLUDE_DIRS
-                                      AXOM_LIBRARIES )
+        string(TOUPPER ${_library} _ucname)
+        set(${_ucname}_FOUND TRUE CACHE BOOL "")
+    endforeach()
 else()
     message(STATUS "Axom support is OFF")
     set(AXOM_FOUND FALSE CACHE BOOL "")
