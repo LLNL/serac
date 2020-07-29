da09589745461ddf47961a80ec3f287490d3aaff
Josh Essman <essman1@llnl.gov>
2020-07-29 15:19:52 -0500

    testing: added a test for named return value optimization

---
+++ tests/copy_elision.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2019-2020, Lawrence Livermore National Security, LLC and
+// other Serac Project Developers. See the top-level LICENSE file for
+// details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#include <gtest/gtest.h>
+
+#include "mfem.hpp"
+
+class ArrayCtr
+{
+  private:
+    mfem::Array<double> t;
+    static std::size_t copy_;
+    static std::size_t default_;
+  public:
+    ArrayCtr() {defaultCalled();}
+    ArrayCtr(const ArrayCtr& other) : t(other.t) {copyCalled();}
+
+    void defaultCalled() {default_++;}
+    void copyCalled() {copy_++;}
+
+    static auto numDefaultCalls() {return default_;}
+    static auto numCopyCalls() {return copy_;}
+
+    void Append(double elem) {t.Append(elem);}
+
+};
+
+std::size_t ArrayCtr::copy_ = 0;
+std::size_t ArrayCtr::default_ = 0;
+
+ArrayCtr doubleArrayMaker() {
+    ArrayCtr result;
+    // Just a random loop - if nothing was done to the array before returning,
+    // the compiler might be able to do a non-returned value
+    for (auto i = 0; i < 10; i++) {
+        if (i % 2) {
+            result.Append(i + 7 % 3);
+        }
+    }
+    return result;
+}
+
+TEST(NRVO, NRVO_mfem_array) 
+{
+    ArrayCtr arr = doubleArrayMaker();
+    ASSERT_EQ(1, ArrayCtr::numDefaultCalls());
+    ASSERT_EQ(0, ArrayCtr::numCopyCalls());
+}
