909a4e28e537821c7fe8eddba5d10380d1997ad2
Jamie A. Bramwell <bramwell1@llnl.gov>
2020-08-03 10:57:19 -0700

    Adding a new utility function for reading a mesh from a std::string file name

---
+++ src/common/mesh_utils.cpp
@@ -0,0 +1,50 @@
+// Copyright (c) 2019-2020, Lawrence Livermore National Security, LLC and
+// other Serac Project Developers. See the top-level LICENSE file for
+// details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#include "mesh_utils.hpp"
+
+#include "common/logger.hpp"
+#include "fmt/fmt.hpp"
+
+namespace serac {
+
+std::shared_ptr<mfem::ParMesh> buildParallelMesh(const std::string& mesh_file, int refine_serial, int refine_parallel,
+                                                 MPI_Comm comm)
+{
+  // Get the MPI rank for logging purposes
+  int rank = 0;
+  MPI_Comm_rank(comm, &rank);
+
+  // Open the mesh
+  std::string msg = fmt::format("Opening mesh file: {0}", mesh_file);
+  SLIC_INFO_ROOT(rank, msg);
+  std::ifstream imesh(mesh_file);
+
+  if (!imesh) {
+    serac::logger::flush();
+    std::string err_msg = fmt::format("Can not open mesh file: {0}", mesh_file);
+    SLIC_ERROR_ROOT(rank, err_msg);
+    serac::exitGracefully();
+  }
+
+  auto mesh = std::make_unique<mfem::Mesh>(imesh, 1, 1, true);
+  imesh.close();
+
+  // mesh refinement if specified in input
+  for (int lev = 0; lev < refine_serial; lev++) {
+    mesh->UniformRefinement();
+  }
+
+  // create the parallel mesh
+  auto pmesh = std::make_shared<mfem::ParMesh>(comm, *mesh);
+  for (int lev = 0; lev < refine_parallel; lev++) {
+    pmesh->UniformRefinement();
+  }
+
+  return pmesh;
+}
+
+}  // namespace serac
