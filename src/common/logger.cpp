a3ef1bc923129d12f1e49d7272e5ea1eddcfd522
Josh Essman <essman1@llnl.gov>
2020-08-07 16:24:18 -0500

    fix: write (hopefully) MPI-compliant signal handler

--- src/common/logger.cpp
+++ src/common/logger.cpp
@@ -13,10 +13,42 @@
 #include "mpi.h"
 
 namespace {
+/**
+ * The actual signal handler - must have this exact signature
+ * MPI is probably dead by this point, SLIC is definitely dead
+ */
 void signalHandler(int signal)
 {
-  // Will clean up MPI via call to axom::Utilities::processAbort()
-  SLIC_ERROR("Received signal " << signal << ", exiting");
+  int mpi_finalized;
+  MPI_Finalized(&mpi_finalized);
+  if (!mpi_finalized) {
+    MPI_Finalize();
+  }
+  // This works, but is it UB to call MPI_Comm_rank here?
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank == 0) {
+    std::cerr << "[SIGNAL]: Received signal " << signal << ", exiting" << std::endl;
+  }
+  exit(EXIT_FAILURE);
+}
+
+/**
+ * Registers the signalHandler function to handle various fatal
+ * signals
+ * @note The behavior of MPI when a signal is sent to the mpirun
+ * process is implementation-defined.  OpenMPI will send a SIGTERM
+ * (which can be caught) and then a SIGKILL (which cannot) a few
+ * seconds later.  PlatformMPI will propagate the signal to the
+ * individual processes.  Before propagating or sending a signal,
+ * MPI may or may not call MPI_Finalize/MPI_Abort.
+ */
+void registerSignals()
+{
+  std::signal(SIGINT, signalHandler);
+  std::signal(SIGABRT, signalHandler);
+  std::signal(SIGSEGV, signalHandler);
+  std::signal(SIGTERM, signalHandler);
 }
 }  // namespace
 
@@ -40,9 +72,7 @@ bool initialize(MPI_Comm comm)
     slic::initialize();
   }
 
-  std::signal(SIGINT, signalHandler);
-  std::signal(SIGABRT, signalHandler);
-  std::signal(SIGSEGV, signalHandler);
+  registerSignals();
 
   int numRanks, rank;
   MPI_Comm_size(comm, &numRanks);
