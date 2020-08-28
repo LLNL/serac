50708b75100c0105579a6ecbbde7bd8ad078aee8
Josh Essman <essman1@llnl.gov>
2020-08-28 09:52:57 -0500

    fix: switched to generic binary vector operations, removed CTAD due to 1-to-1 correspondence between functions and deductions

--- src/common/expression_templates.hpp
+++ src/common/expression_templates.hpp
@@ -3,6 +3,7 @@
 
 #include <assert.h>
 
+#include <functional>
 #include <iostream>
 #include <type_traits>
 #include <utility>
@@ -10,14 +11,12 @@
 #include "mfem.hpp"
 
 template <typename T>
-struct VectorExpr {
+class VectorExpr {
+public:
   double operator[](size_t i) const { return asDerived()[i]; }
   size_t Size() const { return asDerived().Size(); }
 
-  const T& asDerived() const { return static_cast<const T&>(*this); }
-  T&       asDerived() { return static_cast<T&>(*this); }
-
-  operator mfem::Vector () 
+  operator mfem::Vector() const
   {
     mfem::Vector result(Size());
     for (size_t i = 0; i < Size(); i++) {
@@ -25,16 +24,14 @@ struct VectorExpr {
     }
     return result;
   }
+  const T& asDerived() const { return static_cast<const T&>(*this); }
+  T&       asDerived() { return static_cast<T&>(*this); }
 };
 
 template <typename T>
-auto evaluate(const VectorExpr<T>& expr)
+mfem::Vector evaluate(const VectorExpr<T>& expr)
 {
-  mfem::Vector result(expr.Size());
-  for (size_t i = 0; i < expr.Size(); i++) {
-    result[i] = expr[i];
-  }
-  return result;
+  return expr;
 }
 
 template <typename vec, bool owns>
@@ -44,149 +41,123 @@ template <typename vec, bool owns>
 using vec_arg_t = typename std::conditional<owns, std::remove_const_t<vec>&&, const vec&>::type;
 
 template <typename vec, bool owns>
-struct UnaryNegation : VectorExpr<UnaryNegation<vec, owns>> {
-  const vec_t<vec, owns> v_;
+class UnaryNegation : public VectorExpr<UnaryNegation<vec, owns>> {
+public:
   UnaryNegation(vec_arg_t<vec, owns> v) : v_(std::forward<vec_t<vec, owns>>(v)) {}
   double operator[](size_t i) const { return -v_[i]; }
   size_t Size() const { return v_.Size(); }
-};
-template <typename T>
-UnaryNegation(const T&) -> UnaryNegation<T, false>;
 
-template <typename T>
-UnaryNegation(T &&) -> UnaryNegation<T, true>;
+private:
+  const vec_t<vec, owns> v_;
+};
 
 template <typename T>
 auto operator-(VectorExpr<T>&& u)
 {
-  return UnaryNegation(u.asDerived());
+  return UnaryNegation<T, true>(u.asDerived());
 }
 
-auto operator-(const mfem::Vector& u) { return UnaryNegation(u); }
+auto operator-(const mfem::Vector& u) { return UnaryNegation<mfem::Vector, false>(u); }
 
 template <typename vec, bool owns>
-struct ScalarMultiplication : VectorExpr<ScalarMultiplication<vec, owns>> {
-  const double           a_;
-  const vec_t<vec, owns> v_;
+class ScalarMultiplication : public VectorExpr<ScalarMultiplication<vec, owns>> {
+public:
   ScalarMultiplication(const double a, vec_arg_t<vec, owns> v) : a_(a), v_(std::forward<vec_t<vec, owns>>(v)) {}
   double operator[](size_t i) const { return a_ * v_[i]; }
   size_t Size() const { return v_.Size(); }
-};
-template <typename T>
-ScalarMultiplication(const double, const T&) -> ScalarMultiplication<T, false>;
 
-template <typename T>
-ScalarMultiplication(const double, T &&) -> ScalarMultiplication<T, true>;
+private:
+  const double           a_;
+  const vec_t<vec, owns> v_;
+};
 
 template <typename T>
 auto operator*(VectorExpr<T>&& u, const double a)
 {
-  return ScalarMultiplication(a, std::move(u.asDerived()));
+  return ScalarMultiplication<T, true>(a, std::move(u.asDerived()));
 }
 
 template <typename T>
 auto operator*(const double a, VectorExpr<T>&& u)
 {
-  return ScalarMultiplication(a, std::move(u.asDerived()));
+  return operator*(std::move(u), a);
 }
 
-auto operator*(const double a, const mfem::Vector& u) { return ScalarMultiplication(a, u); }
+auto operator*(const double a, const mfem::Vector& u) { return ScalarMultiplication<mfem::Vector, false>(a, u); }
 
-auto operator*(const mfem::Vector& u, const double a) { return ScalarMultiplication(a, u); }
+auto operator*(const mfem::Vector& u, const double a) { return operator*(a, u); }
 
-template <typename lhs, typename rhs, bool lhs_owns, bool rhs_owns>
-struct VectorAddition : VectorExpr<VectorAddition<lhs, rhs, lhs_owns, rhs_owns>> {
-  const vec_t<lhs, lhs_owns> u_;
-  const vec_t<rhs, rhs_owns> v_;
-  VectorAddition(vec_arg_t<lhs, lhs_owns> u, vec_arg_t<rhs, rhs_owns> v)
+template <typename lhs, typename rhs, bool lhs_owns, bool rhs_owns, typename BinOp>
+class BinaryVectorExpr : public VectorExpr<BinaryVectorExpr<lhs, rhs, lhs_owns, rhs_owns, BinOp>> {
+public:
+  BinaryVectorExpr(vec_arg_t<lhs, lhs_owns> u, vec_arg_t<rhs, rhs_owns> v)
       : u_(std::forward<vec_t<lhs, lhs_owns>>(u)), v_(std::forward<vec_t<rhs, rhs_owns>>(v))
   {
     assert(u_.Size() == v_.Size());
   }
-  double operator[](size_t i) const { return u_[i] + v_[i]; }
+  double operator[](size_t i) const { return BinOp{}(u_[i], v_[i]); }
   size_t Size() const { return v_.Size(); }
+
+private:
+  const vec_t<lhs, lhs_owns> u_;
+  const vec_t<rhs, rhs_owns> v_;
 };
-template <typename S, typename T>
-VectorAddition(const S&, const T&) -> VectorAddition<S, T, false, false>;
 
-template <typename S, typename T>
-VectorAddition(S&&, const T&) -> VectorAddition<S, T, true, false>;
+template <typename lhs, typename rhs, bool lhs_owns, bool rhs_owns>
+using VectorAddition = BinaryVectorExpr<lhs, rhs, lhs_owns, rhs_owns, std::plus<double>>;
 
-template <typename S, typename T>
-VectorAddition(const S&, T &&) -> VectorAddition<S, T, false, true>;
-
-template <typename S, typename T>
-VectorAddition(S&&, T &&) -> VectorAddition<S, T, true, true>;
+template <typename lhs, typename rhs, bool lhs_owns, bool rhs_owns>
+using VectorSubtraction = BinaryVectorExpr<lhs, rhs, lhs_owns, rhs_owns, std::minus<double>>;
 
 template <typename S, typename T>
 auto operator+(VectorExpr<S>&& u, VectorExpr<T>&& v)
 {
-  return VectorAddition(std::move(u.asDerived()), std::move(v.asDerived()));
+  return VectorAddition<S, T, true, true>(std::move(u.asDerived()), std::move(v.asDerived()));
 }
 
 template <typename T>
 auto operator+(const mfem::Vector& u, VectorExpr<T>&& v)
 {
-  return VectorAddition(u, std::move(v.asDerived()));
+  return VectorAddition<mfem::Vector, T, false, true>(u, std::move(v.asDerived()));
 }
 
 template <typename T>
 auto operator+(VectorExpr<T>&& u, const mfem::Vector& v)
 {
-  return VectorAddition(std::move(u.asDerived()), v);
+  return operator+(v, std::move(u));
 }
 
-auto operator+(const mfem::Vector& u, const mfem::Vector& v) { return VectorAddition(u, v); }
-
-template <typename lhs, typename rhs, bool lhs_owns, bool rhs_owns>
-struct VectorSubtraction : VectorExpr<VectorSubtraction<lhs, rhs, lhs_owns, rhs_owns>> {
-  const vec_t<lhs, lhs_owns> u_;
-  const vec_t<rhs, rhs_owns> v_;
-  VectorSubtraction(vec_arg_t<lhs, lhs_owns> u, vec_arg_t<rhs, rhs_owns> v)
-      : u_(std::forward<vec_t<lhs, lhs_owns>>(u)), v_(std::forward<vec_t<rhs, rhs_owns>>(v))
-  {
-    assert(u_.Size() == v_.Size());
-  }
-  double operator[](size_t i) const { return u_[i] - v_[i]; }
-  size_t Size() const { return v_.Size(); }
-};
-template <typename S, typename T>
-VectorSubtraction(const S&, const T&) -> VectorSubtraction<S, T, false, false>;
-
-template <typename S, typename T>
-VectorSubtraction(S&&, const T&) -> VectorSubtraction<S, T, true, false>;
-
-template <typename S, typename T>
-VectorSubtraction(const S&, T &&) -> VectorSubtraction<S, T, false, true>;
-
-template <typename S, typename T>
-VectorSubtraction(S&&, T &&) -> VectorSubtraction<S, T, true, true>;
+auto operator+(const mfem::Vector& u, const mfem::Vector& v)
+{
+  return VectorAddition<mfem::Vector, mfem::Vector, false, false>(u, v);
+}
 
 template <typename S, typename T>
 auto operator-(VectorExpr<S>&& u, VectorExpr<T>&& v)
 {
-  return VectorSubtraction(std::move(u.asDerived()), std::move(v.asDerived()));
+  return VectorSubtraction<S, T, true, true>(std::move(u.asDerived()), std::move(v.asDerived()));
 }
 
 template <typename T>
 auto operator-(const mfem::Vector& u, VectorExpr<T>&& v)
 {
-  return VectorSubtraction(u, std::move(v.asDerived()));
+  return VectorSubtraction<mfem::Vector, T, false, true>(u, std::move(v.asDerived()));
 }
 
 template <typename T>
 auto operator-(VectorExpr<T>&& u, const mfem::Vector& v)
 {
-  return VectorSubtraction(std::move(u.asDerived()), v);
+  return operator-(v, std::move(u));
 }
 
-auto operator-(const mfem::Vector& u, const mfem::Vector& v) { return VectorSubtraction(u, v); }
+auto operator-(const mfem::Vector& u, const mfem::Vector& v)
+{
+  return VectorSubtraction<mfem::Vector, mfem::Vector, false, false>(u, v);
+}
 
 template <typename vec, bool owns>
-struct Matvec : VectorExpr<Matvec<vec, owns>> {
-  const mfem::Operator&  A_;
-  const vec_t<vec, owns> v_;
-  mfem::Vector           result;
+class Matvec : public VectorExpr<Matvec<vec, owns>> {
+public:
   Matvec(const mfem::Operator& A, vec_arg_t<vec, owns> v) : A_(A), v_(std::forward<vec_t<vec, owns>>(v))
   {
     result.SetSize(A_.Height());
@@ -199,19 +170,19 @@ struct Matvec : VectorExpr<Matvec<vec, owns>> {
   }
   double operator[](size_t i) const { return result[i]; }
   size_t Size() const { return result.Size(); }
-};
-template <typename T>
-Matvec(const mfem::Operator& A, const T&) -> Matvec<T, false>;
 
-template <typename T>
-Matvec(const mfem::Operator& A, T &&) -> Matvec<T, true>;
+private:
+  const mfem::Operator&  A_;
+  const vec_t<vec, owns> v_;
+  mfem::Vector           result;
+};
 
 template <typename T>
 auto operator*(const mfem::Operator& A, VectorExpr<T>&& v)
 {
-  return Matvec(A, std::move(v.asDerived()));
+  return Matvec<T, true>(A, std::move(v.asDerived()));
 }
 
-auto operator*(const mfem::Operator& A, const mfem::Vector& v) { return Matvec(A, v); }
+auto operator*(const mfem::Operator& A, const mfem::Vector& v) { return Matvec<mfem::Vector, false>(A, v); }
 
 #endif
--- tests/expr_templates.cpp
+++ tests/expr_templates.cpp
@@ -69,17 +69,16 @@ int main()
 
   auto sum = [](const mfem::Vector& vec) {
     double sum = 0.0;
-    for (int i = 0; i < vec.Size(); i++)
-    {
+    for (int i = 0; i < vec.Size(); i++) {
       sum += vec[i];
     }
     return sum;
   };
 
-  auto g = [sum](const auto& a, const auto& b, const auto& Kc) { 
+  auto g = [sum](const auto& a, const auto& b, const auto& Kc) {
     auto b3 = b * 3.0;
-    auto s = sum(b3);
-    return -a + std::move(b3) - 0.3 * s * Kc; 
+    auto s  = sum(b3);
+    return -a + std::move(b3) - 0.3 * s * Kc;
   };
   auto result2 = evaluate(g(a, b, Kc));
   for (int i = 0; i < m; i++) {
