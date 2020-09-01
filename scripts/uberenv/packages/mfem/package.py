ccf4a2a00eb7d3f0865581c1891b48aeb138a3ad
Chris White <white238@llnl.gov>
2020-09-01 16:19:54 -0700

    Move away from hardcoded library names

--- scripts/uberenv/packages/mfem/package.py
+++ scripts/uberenv/packages/mfem/package.py
@@ -499,16 +499,16 @@ class Mfem(Package):
                 ld_flags_from_dirs([spec['gslib'].prefix.lib], ['gs'])]
 
         if '+netcdf' in spec:
-            netcdf_libdirs = [spec['netcdf-c'].prefix.lib]
-            netcdf_libs = LibraryList(['netcdf'])
-            hdf5 = spec['hdf5']
+            lib_flags = ld_flags_from_dirs([spec['netcdf-c'].prefix.lib],
+                                           ['netcdf'])
+            hdf5 = spec['hdf5:hl']
             if hdf5.satisfies('~shared'):
-                netcdf_libdirs.append(hdf5.prefix.lib)
-                netcdf_libs += ['hdf5_hl', 'hdf5', 'dl']
+                hdf5_libs = hdf5.libs
+                hdf5_libs += LibraryList(find_system_libraries('libdl'))
+                lib_flags += " " + ld_flags_from_library_list(hdf5_libs)
             options += [
                 'NETCDF_OPT=-I%s' % spec['netcdf-c'].prefix.include,
-                'NETCDF_LIB=%s' %
-                ld_flags_from_dirs(netcdf_libdirs, netcdf_libs)]
+                'NETCDF_LIB=%s' % lib_flags]
 
         if '+zlib' in spec:
             if "@:3.3.2" in spec:
