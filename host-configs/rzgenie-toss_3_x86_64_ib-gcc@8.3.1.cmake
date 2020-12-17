a8d00d5e7fa81e9d304c215f49f58068f1fd65cd
Chris White <white238@llnl.gov>
2020-12-17 11:50:13 -0800

    update rz host-configs

--- host-configs/rzgenie-toss_3_x86_64_ib-gcc@8.3.1.cmake
+++ host-configs/rzgenie-toss_3_x86_64_ib-gcc@8.3.1.cmake
@@ -35,7 +35,7 @@ set(MPI_CXX_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.3-gcc-8.3.1/bin/mpic
 #---------------------------------------
 # Library Dependencies
 #---------------------------------------
-set(TPL_ROOT "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2020_12_15_22_39_56/gcc-8.3.1" CACHE PATH "")
+set(TPL_ROOT "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2020_12_17_10_46_56/gcc-8.3.1" CACHE PATH "")
 
 set(AXOM_DIR "${TPL_ROOT}/axom-0.4.0serac" CACHE PATH "")
 
