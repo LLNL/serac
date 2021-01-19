a087ec7bb09d0fd9bd9afefe25ebc2524ea39997
Josh Essman <essman1@llnl.gov>
2021-01-19 12:01:29 -0600

    ci: update with new Docker host-configs

--- host-configs/docker/docker-linux-ubuntu18.04-x86_64-clang@10.0.0.cmake
+++ host-configs/docker/docker-linux-ubuntu18.04-x86_64-clang@10.0.0.cmake
@@ -41,25 +41,25 @@ set(MPIEXEC_EXECUTABLE "/usr/bin/mpiexec" CACHE PATH "")
 #---------------------------------------
 # Library Dependencies
 #---------------------------------------
-set(TPL_ROOT "/home/serac/serac_tpls/clang-10.0.0" CACHE PATH "")
+set(TPL_ROOT "/home/serac/serac_tpls/spack/opt/spack/linux-ubuntu18.04-ivybridge/clang-10.0.0" CACHE PATH "")
 
-set(AXOM_DIR "${TPL_ROOT}/axom-0.4.0serac" CACHE PATH "")
+set(AXOM_DIR "${TPL_ROOT}/axom-0.4.0serac-eawir7ejjglhdcxvjqh7y5rdidn77jco" CACHE PATH "")
 
-set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.5.1p1" CACHE PATH "")
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.5.1p1-ivd4ltzd2buete33sb3jx2drbhvfpzed" CACHE PATH "")
 
-set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.21" CACHE PATH "")
+set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.21-la2ye2dk36ev7tdd4kew67bjaaffzyrj" CACHE PATH "")
 
-set(HYPRE_DIR "${TPL_ROOT}/hypre-2.18.2" CACHE PATH "")
+set(HYPRE_DIR "${TPL_ROOT}/hypre-2.18.2-rbpkyifmhegomrmarn4xybx735vzbf67" CACHE PATH "")
 
-set(METIS_DIR "${TPL_ROOT}/metis-5.1.0" CACHE PATH "")
+set(METIS_DIR "${TPL_ROOT}/metis-5.1.0-4jcour4vwbrujdrbthuzl7kevjmvnw6g" CACHE PATH "")
 
-set(NETCDF_DIR "${TPL_ROOT}/netcdf-c-4.7.4" CACHE PATH "")
+set(NETCDF_DIR "${TPL_ROOT}/netcdf-c-4.7.4-wkpx6sofxrctmgkg4nlvlkzi64pq35lu" CACHE PATH "")
 
-set(PARMETIS_DIR "${TPL_ROOT}/parmetis-4.0.3" CACHE PATH "")
+set(PARMETIS_DIR "${TPL_ROOT}/parmetis-4.0.3-36uc63phhgdbydtdat3jaghjdkgd2jcd" CACHE PATH "")
 
-set(SUPERLUDIST_DIR "${TPL_ROOT}/superlu-dist-6.1.1" CACHE PATH "")
+set(SUPERLUDIST_DIR "${TPL_ROOT}/superlu-dist-6.1.1-ibz4g7qzcihl7quz2t4xjdfiisc5c7nw" CACHE PATH "")
 
-set(MFEM_DIR "${TPL_ROOT}/mfem-4.2.0" CACHE PATH "")
+set(MFEM_DIR "${TPL_ROOT}/mfem-4.2.0-e3al6azmlhnlzsgpfdoczx3wglhc3qk5" CACHE PATH "")
 
 #------------------------------------------------------------------------------
 # Devtools
