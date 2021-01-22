e25a6d0bff3f9fb95fce9dbd8da97eae6924833e
Chris White <white238@llnl.gov>
2021-01-22 10:34:57 -0800

    Bump Axom sha

--- scripts/spack/packages/axom/package.py
+++ scripts/spack/packages/axom/package.py
@@ -60,7 +60,7 @@ class Axom(CMakePackage, CudaPackage):
     version('develop', branch='develop', submodules=True)
 
     # SERAC EDIT START
-    version('0.4.0serac', commit='85a67b8489947965a32a50312c559f44c9f7c880', submodules="True")
+    version('0.4.0serac', commit='494fc6429ac217b9d6aff5c9bc967dd3ee8aa5b9', submodules="True")
     # SERAC EDIT END
 
     version('0.4.0', tag='v0.4.0', submodules=True)
