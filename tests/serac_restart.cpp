09977e6b314f7bcb79c6dcdff460f539e093d86a
Josh Essman <essman1@llnl.gov>
2021-02-17 10:22:53 -0600

    fix: UnitTestLogger -> SimpleLogger

--- tests/serac_restart.cpp
+++ tests/serac_restart.cpp
@@ -162,8 +162,7 @@ TEST(thermal_solver, dyn_imp_solve)
 }
 
 //------------------------------------------------------------------------------
-#include "axom/slic/core/UnitTestLogger.hpp"
-using axom::slic::UnitTestLogger;
+#include "axom/slic/core/SimpleLogger.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -173,8 +172,8 @@ int main(int argc, char* argv[])
 
   MPI_Init(&argc, &argv);
 
-  UnitTestLogger logger;  // create & initialize test logger, finalized when
-                          // exiting main scope
+  axom::slic::SimpleLogger logger;  // create & initialize test logger, finalized when
+                                    // exiting main scope
 
   result = RUN_ALL_TESTS();
 
