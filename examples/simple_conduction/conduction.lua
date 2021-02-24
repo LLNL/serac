44fe9396b9dccc570809125578617d2bf89c822d
Josh Essman <essman1@llnl.gov>
2021-02-24 15:49:33 -0600

    docs: more interesting boundary condition

--- examples/simple_conduction/conduction.lua
+++ examples/simple_conduction/conduction.lua
@@ -5,48 +5,54 @@ output_type = "ParaView"
 -- Mesh information
 -- _mesh_start
 main_mesh = {
-  type = "generate",
-  elements = {x = 10, y = 10}
+    type = "generate",
+    elements = {x = 10, y = 10}
 }
 -- _mesh_end
 
 -- Solver parameters
 thermal_conduction = {
   -- _solver_opts_start
-  stiffness_solver = {
-      linear = {
-          type = "iterative",
-          iterative_options = {
-              rel_tol     = 1.0e-6,
-              abs_tol     = 1.0e-12,
-              max_iter    = 200,
-              print_level = 0,
-              solver_type = "cg",
-              prec_type   = "JacobiSmoother",
-          },
-      },
+    stiffness_solver = {
+        linear = {
+            type = "iterative",
+            iterative_options = {
+                rel_tol     = 1.0e-6,
+                abs_tol     = 1.0e-12,
+                max_iter    = 200,
+                print_level = 0,
+                solver_type = "cg",
+                prec_type   = "JacobiSmoother",
+            },
+        },
 
-      nonlinear = {
-          rel_tol     = 1.0e-4,
-          abs_tol     = 1.0e-8,
-          max_iter    = 500,
-          print_level = 1,
-      },
-  },
+        nonlinear = {
+            rel_tol     = 1.0e-4,
+            abs_tol     = 1.0e-8,
+            max_iter    = 500,
+            print_level = 1,
+        },
+    },
 
-  order = 2,
-  -- _solver_opts_end
+    order = 2,
+    -- _solver_opts_end
 
-  -- _conductivity_start
-  kappa = 0.5,
-  -- _conductivity_end
+    -- _conductivity_start
+    kappa = 0.5,
+    -- _conductivity_end
 
-  -- _bc_start
-  boundary_conds = {
-      ['temperature'] = {
-          attrs = {1},
-          constant = 1.0
-      },
-  },
-  -- _bc_end
+    -- _bc_start
+    boundary_conds = {
+        ['temperature_1'] = {
+            attrs = {1},
+            constant = 1.0
+        },
+        ['temperature_2'] = {
+            attrs = {2, 3},
+            scalar_function = function(v)
+                return v.x * v.x + v.y - 1
+            end
+        }
+    },
+    -- _bc_end
 }
