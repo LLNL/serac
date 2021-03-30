bbbd90709847ac5d936f9de23d06f1d3b3d1122b
Josh Essman <essman1@llnl.gov>
2021-03-30 11:08:35 -0500

    build: switch to blt_import_library for mfem

--- cmake/thirdparty/FindMFEM.cmake
+++ cmake/thirdparty/FindMFEM.cmake
@@ -116,6 +116,12 @@ else()
         list(APPEND MFEM_LIBRARIES ${CUDA_LIBRARIES})
         list(APPEND MFEM_LIBRARIES ${CUDA_CUBLAS_LIBRARIES})
     endif()
+
+    blt_import_library(
+        NAME          mfem
+        INCLUDES      ${MFEM_INCLUDE_DIRS}
+        LIBRARIES     ${MFEM_LIBRARIES}
+        TREAT_INCLUDES_AS_SYSTEM ON)
 endif()
 
 include(FindPackageHandleStandardArgs)
@@ -131,9 +137,3 @@ endif()
 
 message(STATUS "MFEM Includes: ${MFEM_INCLUDE_DIRS}")
 message(STATUS "MFEM Libraries: ${MFEM_LIBRARIES}")
-
-blt_register_library(
-    NAME          mfem
-    INCLUDES      ${MFEM_INCLUDE_DIRS}
-    LIBRARIES     ${MFEM_LIBRARIES}
-    TREAT_INCLUDES_AS_SYSTEM ON)
