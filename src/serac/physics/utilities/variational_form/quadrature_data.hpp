2d2a2b26b33a51df75f6438a8d4dc5dff278b613
Josh Essman <essman1@llnl.gov>
2021-04-14 09:01:30 -0500

    feat: initial skeleton of QuadratureData

---
+++ src/serac/physics/utilities/variational_form/quadrature_data.hpp
@@ -0,0 +1,84 @@
+// Copyright (c) 2019-2021, Lawrence Livermore National Security, LLC and
+// other Serac Project Developers. See the top-level LICENSE file for
+// details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+/**
+ * @file quadrature_data.hpp
+ *
+ * @brief The definition of the QuadratureData class
+ */
+
+#pragma once
+
+#include "mfem.hpp"
+
+namespace serac {
+/**
+ * @brief Stores instances of user-defined type for each quadrature point in a mesh
+ * @tparam T The type of the per-qpt data
+ */
+template <typename T>
+class QuadratureData {
+public:
+  /**
+   * @brief Constructs using a mesh and polynomial order
+   * @param[in] mesh The mesh for which quadrature-point data should be stored
+   * @param[in] p The polynomial order of the associated finite elements
+   */
+  QuadratureData(mfem::Mesh& mesh, const int p);
+
+  /**
+   * @brief Retrieves the data for a given quadrature point
+   * @param[in] element_idx The index of the desired element within the mesh
+   * @param[in] q_idx The index of the desired quadrature point within the element
+   */
+  T& operator()(const int element_idx, const int q_idx);
+
+  /**
+   * @brief Assigns an item to each quadrature point
+   * @param[in] item The item to assign
+   */
+  QuadratureData& operator=(const T& item);
+
+private:
+  // FIXME: These will probably need to be MaybeOwningPointers
+  // See https://github.com/LLNL/axom/pull/433
+  /**
+   * @brief Storage layout of @p qfunc_ containing mesh and polynomial order info
+   */
+  mfem::QuadratureSpace qspace_;
+  /**
+   * @brief Per-quadrature point data, stored as array of doubles for compatibility with Sidre
+   */
+  mfem::QuadratureFunction qfunc_;
+};
+
+// Hijacks the "vdim" parameter (number of doubles per qpt) to allocate the correct amount of storage
+template <typename T>
+QuadratureData<T>::QuadratureData(mfem::Mesh& mesh, const int p)
+    : qspace_(&mesh, p), qfunc_(&qspace_, sizeof(T) / sizeof(double))
+{
+}
+
+template <typename T>
+T& QuadratureData<T>::operator()(const int element_idx, const int q_idx)
+{
+  // A view into the quadrature point data
+  mfem::Vector view;
+  qfunc_.GetElementValues(element_idx, q_idx, view);
+  return *(reinterpret_cast<T*>(view.GetData()));
+}
+
+template <typename T>
+QuadratureData<T>& QuadratureData<T>::operator=(const T& item)
+{
+  // Number of doubles divided by number of doubles per T
+  const auto n_qpts = qfunc_.Size() / (sizeof(T) / sizeof(double));
+  T*         ptr    = reinterpret_cast<T*>(qfunc_.GetData());
+  std::fill(ptr, ptr + n_qpts, item);
+  return *this;
+}
+
+}  // namespace serac
---
+++ src/serac/physics/utilities/variational_form/tests/weak_form_material_state_test.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2019-2021, Lawrence Livermore National Security, LLC and
+// other Serac Project Developers. See the top-level LICENSE file for
+// details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#include "serac/serac_config.hpp"
+
+#include "serac/numerics/mesh_utils.hpp"
+
+#include "serac/infrastructure/initialize.hpp"
+// #include "serac/infrastructure/input.hpp"
+// #include "serac/infrastructure/logger.hpp"
+#include "serac/infrastructure/terminator.hpp"
+
+#include "serac/physics/utilities/variational_form/weak_form.hpp"
+#include "serac/physics/utilities/variational_form/tensor.hpp"
+#include "serac/physics/utilities/variational_form/quadrature_data.hpp"
+
+using namespace serac;
+
+struct State {
+  double x;
+};
+
+int main(int argc, char** argv)
+{
+  serac::initialize(argc, argv);
+  constexpr auto mesh_file = SERAC_REPO_DIR "/data/meshes/star.mesh";
+  auto           mesh      = mesh::refineAndDistribute(buildMeshFromFile(mesh_file), 1, 0);
+
+  constexpr int p   = 1;
+  constexpr int dim = 2;
+
+  SLIC_ERROR_IF(dim != mesh->Dimension(), "pick a different mesh");
+
+  auto                        fec = mfem::H1_FECollection(p, mesh->Dimension());
+  mfem::ParFiniteElementSpace fespace(mesh.get(), &fec);
+
+  using test_space  = H1<p>;
+  using trial_space = H1<p>;
+
+  WeakForm<test_space(trial_space)> residual(&fespace, &fespace);
+
+  QuadratureData<State> qdata(*mesh, p);
+  State                 init{0};
+  qdata = init;
+  residual.AddSurfaceIntegral([&](auto x, auto /* u */) { return x[0]; }, *mesh);
+
+  serac::exitGracefully();
+}
