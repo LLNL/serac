2041553ed2832dfe7647e1d264d8aa43b36f1756
samuelpmishLLNL <61714427+samuelpmishLLNL@users.noreply.github.com>
2021-05-13 09:32:54 -0700

    additional comments for tuple_arithmetic.hpp

--- src/serac/physics/utilities/variational_form/tuple_arithmetic.hpp
+++ src/serac/physics/utilities/variational_form/tuple_arithmetic.hpp
@@ -140,12 +140,30 @@ auto chain_rule_tuple_scale(std::tuple<T...> df_dx, S dx)
       [&](auto... each_component_of_df_dx) { return std::tuple{chain_rule(each_component_of_df_dx, dx)...}; }, df_dx);
 }
 
+// chain rule between two tuples of values, kind of like a "dot product"
+// effectively equivalent to
+//
+// std::tuple df{
+//   chain_rule(std::get<0>(df_dx), std::get<0>(dx)),
+//   chain_rule(std::get<1>(df_dx), std::get<1>(dx)),
+//   chain_rule(std::get<2>(df_dx), std::get<2>(dx)),
+//   ...
+// }  
 template <typename... T, typename... S, int... i>
 auto chain_rule_tuple_vecvec(std::tuple<T...> df_dx, std::tuple<S...> dx, std::integer_sequence<int, i...>)
 {
   return (chain_rule(std::get<i>(df_dx), std::get<i>(dx)) + ...);
 }
 
+// chain rule between a tuple-of-tuples, and another tuple, kind of like a "matrix vector product"
+// effectively equivalent to
+//
+// std::tuple df{
+//   chain_rule(std::get<0>(std::get<0>(df_dx)), std::get<0>(dx)) + chain_rule(std::get<1>(std::get<0>(df_dx)), std::get<1>(dx)) + ... ,
+//   chain_rule(std::get<0>(std::get<1>(df_dx)), std::get<0>(dx)) + chain_rule(std::get<1>(std::get<1>(df_dx)), std::get<1>(dx)) + ... ,
+//   chain_rule(std::get<0>(std::get<2>(df_dx)), std::get<0>(dx)) + chain_rule(std::get<1>(std::get<2>(df_dx)), std::get<1>(dx)) + ... ,
+//   ...
+// } 
 template <typename... T, typename... S>
 auto chain_rule_tuple_matvec(std::tuple<T...> df_dx, std::tuple<S...> dx)
 {
@@ -160,6 +178,13 @@ auto chain_rule_tuple_matvec(std::tuple<T...> df_dx, std::tuple<S...> dx)
 
 }  // namespace detail
 
+/**
+ * @brief apply operator+ to each pair of entries in two similarly-sized tuples
+ * @param[in] A a tuple of values
+ * @param[in] B a tuple of values
+ * Note: A and B must have the same size, and the type of the ith entry of the returned tuple
+ * is given by decltype(std::get<i>(A) + std::get<i>(B))
+ */
 template <typename... S, typename... T>
 constexpr auto operator+(const std::tuple<S...>& A, const std::tuple<T...>& B)
 {
@@ -167,12 +192,25 @@ constexpr auto operator+(const std::tuple<S...>& A, const std::tuple<T...>& B)
   return detail::plus_helper(A, B, std::make_integer_sequence<int, int(sizeof...(S))>{});
 }
 
+/**
+ * @brief apply operator+= to each pair of entries in two similarly-sized and similarly-typed tuples
+ * @param[in] A a tuple of values
+ * @param[in] B a tuple of values
+ * Note: A and B must have the same size
+ */
 template <typename... S>
 constexpr auto operator+=(std::tuple<S...>& A, const std::tuple<S...>& B)
 {
   return A = A + B;
 }
 
+/**
+ * @brief apply operator- to each pair of entries in two similarly-sized tuples
+ * @param[in] A a tuple of values
+ * @param[in] B a tuple of values
+ * Note: A and B must have the same size, and the type of the ith entry of the returned tuple
+ * is given by decltype(std::get<i>(A) - std::get<i>(B))
+ */
 template <typename... S, typename... T>
 constexpr auto operator-(const std::tuple<S...>& A, const std::tuple<T...>& B)
 {
@@ -180,24 +218,52 @@ constexpr auto operator-(const std::tuple<S...>& A, const std::tuple<T...>& B)
   return detail::minus_helper(A, B, std::make_integer_sequence<int, int(sizeof...(S))>{});
 }
 
+/**
+ * @brief apply a scaling (from the right) to each entry in a tuple
+ * @param[in] A a tuple of values
+ * @param[in] scale the scaling factor
+ * Note: the type of the ith entry of the returned tuple
+ * is given by decltype(std::get<i>(A) * scale)
+ */
 template <typename... S, typename T>
 constexpr auto operator*(const std::tuple<S...>& A, T scale)
 {
   return detail::mult_helper(A, scale, std::make_integer_sequence<int, int(sizeof...(S))>{});
 }
 
+/**
+ * @brief apply a scaling (from the left) to each entry in a tuple
+ * @param[in] scale the scaling factor
+ * @param[in] A a tuple of values
+ * Note: the type of the ith entry of the returned tuple
+ * is given by decltype(scale * std::get<i>(A))
+ */
 template <typename S, typename... T>
 constexpr auto operator*(S scale, const std::tuple<T...>& A)
 {
   return detail::mult_helper(scale, A, std::make_integer_sequence<int, int(sizeof...(T))>{});
 }
 
+/**
+ * @brief divide each entry in a tuple by a denominator
+ * @param[in] numerators a tuple of numerators
+ * @param[in] denominator the denominator
+ * Note: the type of the ith entry of the returned tuple
+ * is given by decltype(std::get<i>(numerators) / denominator)
+ */
 template <typename... S, typename T>
 constexpr auto operator/(const std::tuple<S...>& numerators, T denominator)
 {
   return detail::div_helper(numerators, denominator, std::make_integer_sequence<int, int(sizeof...(S))>{});
 }
 
+/**
+ * @brief return a tuple with entries defined by dividing a numerator value by each entry of a tuple
+ * @param[in] numerator a numerator value
+ * @param[in] denominators a tuple of denominator values
+ * Note: the type of the ith entry of the returned tuple
+ * is given by decltype(numerator / std::get<i>(denominators))
+ */
 template <typename S, typename... T>
 constexpr auto operator/(S numerator, const std::tuple<T...>& denominators)
 {
@@ -258,6 +324,12 @@ auto get_gradient(std::tuple<T...> tuple_of_values)
   return std::apply([](auto... each_value) { return std::tuple{get_gradient(each_value)...}; }, tuple_of_values);
 }
 
+/**
+ * @brief entry point for combining derivatives from get_gradient(***) with the chain rule
+ * calculates df = df_dx * dx for different possible combinations of tuples-of-tuples, tuples, tensors, and scalars
+ * @param[in] df_dx the derivative of some transformation f
+ * @param[in] dx a small change in the inputs to the transformation f 
+ */
 template <typename S, typename T>
 auto chain_rule(S df_dx, T dx)
 {
