d2afafaea36cffb717dd4e0e039800dbd91cb865
Sam Mish <mish2@llnl.gov>
2021-05-24 09:52:37 -0700

    address all doxygen warnings in dual.hpp

--- src/serac/physics/utilities/variational_form/dual.hpp
+++ src/serac/physics/utilities/variational_form/dual.hpp
@@ -21,89 +21,109 @@ namespace serac {
 /**
  * @brief Dual number struct (value plus gradient)
  * @tparam gradient_type The type of the gradient (should support addition, scalar multiplication/division, and unary negation operators)
- *  
  */
 template <typename gradient_type>
 struct dual {
-  double        value;
-  gradient_type gradient;
+  double        value;    ///< the actual numerical value 
+  gradient_type gradient; ///< the partial derivatives of value w.r.t. some other quantity
 };
 
+/**
+ * @brief class template argument deduction guide for type `dual`.
+ * 
+ * @note this lets users write 
+ * \code{.cpp} dual something{my_value, my_gradient}; \endcode
+ * instead of explicitly writing the template parameter 
+ * \code{.cpp} dual< decltype(my_gradient) > something{my_value, my_gradient}; \endcode
+ */
 template <typename T>
 dual(double, T) -> dual<T>;
 
+/** @brief addition of a dual number and a non-dual number */
 template <typename gradient_type>
 constexpr auto operator+(dual<gradient_type> a, double b)
 {
   return dual{a.value + b, a.gradient};
 }
 
+/** @brief addition of a dual number and a non-dual number */
 template <typename gradient_type>
 constexpr auto operator+(double a, dual<gradient_type> b)
 {
   return dual{a + b.value, b.gradient};
 }
 
+/** @brief addition of two dual numbers */
 template <typename gradient_type_a, typename gradient_type_b>
 constexpr auto operator+(dual<gradient_type_a> a, dual<gradient_type_b> b)
 {
   return dual{a.value + b.value, a.gradient + b.gradient};
 }
 
+/** @brief unary negation of a dual number */
 template <typename gradient_type>
 constexpr auto operator-(dual<gradient_type> x)
 {
   return dual{-x.value, -x.gradient};
 }
 
+/** @brief subtraction of a non-dual number from a dual number */
 template <typename gradient_type>
 constexpr auto operator-(dual<gradient_type> a, double b)
 {
   return dual{a.value - b, a.gradient};
 }
 
+/** @brief subtraction of a dual number from a non-dual number */
 template <typename gradient_type>
 constexpr auto operator-(double a, dual<gradient_type> b)
 {
   return dual{a - b.value, -b.gradient};
 }
 
+/** @brief subtraction of two dual numbers */
 template <typename gradient_type_a, typename gradient_type_b>
 constexpr auto operator-(dual<gradient_type_a> a, dual<gradient_type_b> b)
 {
   return dual{a.value - b.value, a.gradient - b.gradient};
 }
 
+/** @brief multiplication of a dual number and a non-dual number */
 template <typename gradient_type>
 constexpr auto operator*(const dual<gradient_type>& a, double b)
 {
   return dual{a.value * b, a.gradient * b};
 }
 
+/** @brief multiplication of a dual number and a non-dual number */
 template <typename gradient_type>
 constexpr auto operator*(double a, const dual<gradient_type>& b)
 {
   return dual{a * b.value, a * b.gradient};
 }
 
+/** @brief multiplication of two dual numbers */
 template <typename gradient_type_a, typename gradient_type_b>
 constexpr auto operator*(dual<gradient_type_a> a, dual<gradient_type_b> b)
 {
   return dual{a.value * b.value, b.value * a.gradient + a.value * b.gradient};
 }
 
+/** @brief division of a dual number by a non-dual number */
 template <typename gradient_type>
 constexpr auto operator/(const dual<gradient_type>& a, double b)
 {
   return dual{a.value / b, a.gradient / b};
 }
 
+/** @brief division of a non-dual number by a dual number */
 template <typename gradient_type>
 constexpr auto operator/(double a, const dual<gradient_type>& b)
 {
   return dual{a / b.value, -(a / (b.value * b.value)) * b.gradient};
 }
 
+/** @brief division of two dual numbers */
 template <typename gradient_type_a, typename gradient_type_b>
 constexpr auto operator/(dual<gradient_type_a> a, dual<gradient_type_b> b)
 {
@@ -134,14 +154,15 @@ constexpr auto operator/(dual<gradient_type_a> a, dual<gradient_type_b> b)
     return a.value x b.value;                                   \
   };
 
-binary_comparator_overload(<);
-binary_comparator_overload(<=);
-binary_comparator_overload(==);
-binary_comparator_overload(>=);
-binary_comparator_overload(>);
+binary_comparator_overload(<);  ///< implement operator<  for dual numbers
+binary_comparator_overload(<=); ///< implement operator<= for dual numbers
+binary_comparator_overload(==); ///< implement operator== for dual numbers
+binary_comparator_overload(>=); ///< implement operator>= for dual numbers
+binary_comparator_overload(>);  ///< implement operator>  for dual numbers
 
 #undef binary_comparator_overload
 
+/** @brief compound assignment (+) for dual numbers */
 template <typename gradient_type>
 constexpr auto& operator+=(dual<gradient_type>& a, const dual<gradient_type>& b)
 {
@@ -150,6 +171,7 @@ constexpr auto& operator+=(dual<gradient_type>& a, const dual<gradient_type>& b)
   return a;
 }
 
+/** @brief compound assignment (-) for dual numbers */
 template <typename gradient_type>
 constexpr auto& operator-=(dual<gradient_type>& a, const dual<gradient_type>& b)
 {
@@ -158,6 +180,7 @@ constexpr auto& operator-=(dual<gradient_type>& a, const dual<gradient_type>& b)
   return a;
 }
 
+/** @brief compound assignment (+) for dual numbers with `double` righthand side */
 template <typename gradient_type>
 constexpr auto& operator+=(dual<gradient_type>& a, double b)
 {
@@ -165,6 +188,7 @@ constexpr auto& operator+=(dual<gradient_type>& a, double b)
   return a;
 }
 
+/** @brief compound assignment (-) for dual numbers with `double` righthand side */
 template <typename gradient_type>
 constexpr auto& operator-=(dual<gradient_type>& a, double b)
 {
@@ -172,12 +196,14 @@ constexpr auto& operator-=(dual<gradient_type>& a, double b)
   return a;
 }
 
+/** @brief implementation of absolute value function for dual numbers */
 template <typename gradient_type>
 auto abs(dual<gradient_type> x)
 {
   return (x.value > 0) ? x : -x;
 }
 
+/** @brief implementation of square root for dual numbers */
 template <typename gradient_type>
 auto sqrt(dual<gradient_type> x)
 {
@@ -185,6 +211,7 @@ auto sqrt(dual<gradient_type> x)
   return dual<gradient_type>{sqrt(x.value), x.gradient / (2.0 * sqrt(x.value))};
 }
 
+/** @brief implementation of cosine for dual numbers */
 template <typename gradient_type>
 auto cos(dual<gradient_type> a)
 {
@@ -192,6 +219,7 @@ auto cos(dual<gradient_type> a)
   return dual<gradient_type>{cos(a.value), -a.gradient * sin(a.value)};
 }
 
+/** @brief implementation of exponential function for dual numbers */
 template <typename gradient_type>
 auto exp(dual<gradient_type> a)
 {
@@ -199,6 +227,7 @@ auto exp(dual<gradient_type> a)
   return dual<gradient_type>{exp(a.value), exp(a.value)};
 }
 
+/** @brief implementation of the natural logarithm function for dual numbers */
 template <typename gradient_type>
 auto log(dual<gradient_type> a)
 {
@@ -206,6 +235,7 @@ auto log(dual<gradient_type> a)
   return dual<gradient_type>{log(a.value), a.gradient / a.value};
 }
 
+/** @brief implementation of `a` (dual) raised to the `b` (dual) power */
 template <typename gradient_type>
 auto pow(dual<gradient_type> a, dual<gradient_type> b)
 {
@@ -214,6 +244,7 @@ auto pow(dual<gradient_type> a, dual<gradient_type> b)
   return dual<gradient_type>{value, value * (a.gradient * (b.value / a.value) + b.gradient * log(a.value))};
 }
 
+/** @brief implementation of `a` (non-dual) raised to the `b` (dual) power */
 template <typename gradient_type>
 auto pow(double a, dual<gradient_type> b)
 {
@@ -222,6 +253,7 @@ auto pow(double a, dual<gradient_type> b)
   return dual<gradient_type>{value, value * b.gradient * log(a)};
 }
 
+/** @brief implementation of `a` (dual) raised to the `b` (non-dual) power */
 template <typename gradient_type>
 auto pow(dual<gradient_type> a, double b)
 {
@@ -230,6 +262,7 @@ auto pow(dual<gradient_type> a, double b)
   return dual<gradient_type>{value, value * a.gradient * b / a.value};
 }
 
+/** @brief overload of operator<< for `dual` to work with `std::cout` and other `std::ostream`s */
 template <typename T, int... n>
 auto& operator<<(std::ostream& out, dual<T> A)
 {
@@ -237,34 +270,40 @@ auto& operator<<(std::ostream& out, dual<T> A)
   return out;
 }
 
+/** @brief promote a value to a dual number of the appropriate type */
 constexpr auto make_dual(double x) { return dual{x, 1.0}; }
 
+/** @brief return the "value" part from a given type. For non-dual types, this is just the identity function */
 template <typename T>
 auto get_value(const T& arg)
 {
   return arg;
 }
 
+/** @brief return the "value" part from a dual number type */
 template <typename T>
 auto get_value(dual<T> arg)
 {
   return arg.value;
 }
 
+/** @brief return the "gradient" part from a dual number type */
 template <typename gradient_type>
 auto get_gradient(dual<gradient_type> arg)
 {
   return arg.gradient;
 }
 
+/** @brief class for checking if a type is a dual number or not */
 template <typename T>
 struct is_dual_number {
-  static constexpr bool value = false;
+  static constexpr bool value = false; ///< whether or not type T is a dual number
 };
 
+/** @brief class for checking if a type is a dual number or not */
 template <typename T>
 struct is_dual_number<dual<T> > {
-  static constexpr bool value = true;
+  static constexpr bool value = true; ///< whether or not type T is a dual number
 };
 
 }  // namespace serac
