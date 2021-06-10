bc786d20e72fd099f4d9f3ffc1cb2e9bb935efa8
Jonathan Wong <jonwong12@users.noreply.github.com>
2021-06-10 14:51:03 -0700

    fix merge

--- src/serac/infrastructure/profiling.hpp
+++ src/serac/infrastructure/profiling.hpp
@@ -130,12 +130,10 @@ inline const char* make_cstr(const char* str) { return str; }
 inline const char* make_cstr(const std::string& str) { return str.c_str(); }
 
 }  // namespace serac::profiling::detail
+/// @endcond
 
 #define SERAC_PROFILE_SCOPE(name) \
   cali::ScopeAnnotation SERAC_CONCAT(region, __LINE__)(serac::profiling::detail::make_cstr(name))
-
-}  // namespace detail
-/// @endcond
 
 /**
  * @brief The type that should be returned from the profiling wrapper lambda
