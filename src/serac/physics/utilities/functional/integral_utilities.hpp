eca1bbee82b82cb6a54fc188d531a601e0363cbf
Sam Mish <mish2@llnl.gov>
2021-06-15 08:10:58 -0700

    license + description for integral_utilities

--- src/serac/physics/utilities/functional/integral_utilities.hpp
+++ src/serac/physics/utilities/functional/integral_utilities.hpp
@@ -1,3 +1,15 @@
+// Copyright (c) 2019-2021, Lawrence Livermore National Security, LLC and
+// other Serac Project Developers. See the top-level LICENSE file for
+// details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+/**
+ * @file intergal_utilities.hpp
+ *
+ * @brief this file contains functions and tools used by both integral.hpp and boundary_integral.hpp
+ */
+
 #pragma once
 
 #include "mfem.hpp"
