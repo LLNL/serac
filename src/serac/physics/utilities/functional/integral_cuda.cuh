c7845c5a4951e4429838d638e35157d00e52b073
Jonathan Wong <jonwong12@users.noreply.github.com>
2021-06-24 19:55:27 -0700

    testing cases that work

--- src/serac/physics/utilities/functional/integral_cuda.cuh
+++ src/serac/physics/utilities/functional/integral_cuda.cuh
@@ -38,58 +38,59 @@ __host__ inline mfem::DeviceTensor<sizeof...(Dims),T> Reshape(T *ptr, Dims... di
   // 	    typename derivatives_type, typename lambda>
   // __global__ void eval_cuda(const mfem::DeviceTensor<2, const double>& u, mfem::DeviceTensor<2, double>& r, derivatives_type* derivatives_ptr,
   // 			    const mfem::DeviceTensor<4, const double>& J, const mfem::DeviceTensor<3, const double>& X, int num_elements, lambda qf) {
-  template <Geometry g, typename test, typename trial, int geometry_dim, int spatial_dim, int Q, typename derivatives_type>
-  __global__ void eval_cuda(int num_elements) {
+  template <Geometry g, typename test, typename trial, int geometry_dim, int spatial_dim, int Q, typename derivatives_type, typename lambda>
+  __global__ void eval_cuda(const mfem::DeviceTensor<2, const double>& u, mfem::DeviceTensor<2, double>& r, derivatives_type* derivatives_ptr, const mfem::DeviceTensor<4, const double>& J, const mfem::DeviceTensor<3, const double>& X, int num_elements, lambda qf) {
 
 
 
     using test_element               = finite_element<g, test>;
     using trial_element              = finite_element<g, trial>;
-    // using element_residual_type      = typename trial_element::residual_type;
+    using element_residual_type      = typename trial_element::residual_type;
     // // static constexpr int  test_ndof  = test_element::ndof;
     // // static constexpr int  trial_ndof = trial_element::ndof;
-    // static constexpr auto rule       = GaussQuadratureRule<g, Q>();
+    static constexpr auto rule       = GaussQuadratureRule<g, Q>();
 
     // for each element in the domain
     for (int e = 0; e < num_elements; e++) {
-      // // get the DOF values for this particular element
-      // tensor u_elem = detail::Load<trial_element>(u, e);
+      // get the DOF values for this particular element
+      tensor u_elem = detail::Load<trial_element>(u, e);
 
-      // // this is where we will accumulate the element residual tensor
-      // element_residual_type r_elem{};
+      // this is where we will accumulate the element residual tensor
+      element_residual_type r_elem{};
 
-      // // for each quadrature point in the element
-      // for (int q = 0; q < static_cast<int>(rule.size()); q++) {
-      // 	// get the position of this quadrature point in the parent and physical space,
-      // 	// and calculate the measure of that point in physical space.
-      // 	auto   xi  = rule.points[q];
-      // 	auto   dxi = rule.weights[q];
-      // 	auto   x_q = make_tensor<spatial_dim>([&](int i) { return X(q, i, e); });  // Physical coords of qpt
-      // 	auto   J_q = make_tensor<spatial_dim, geometry_dim>([&](int i, int j) { return J(q, i, j, e); });
-      // 	double dx  = detail::Measure(J_q) * dxi;
+      // for each quadrature point in the element
+      for (int q = 0; q < static_cast<int>(rule.size()); q++) {
+      	// get the position of this quadrature point in the parent and physical space,
+      	// and calculate the measure of that point in physical space.
+      	auto   xi  = rule.points[q];
+      	auto   dxi = rule.weights[q];
+      	auto   x_q = make_tensor<spatial_dim>([&](int i) { return X(q, i, e); });  // Physical coords of qpt
+      	auto   J_q = make_tensor<spatial_dim, geometry_dim>([&](int i, int j) { return J(q, i, j, e); });
+      	double dx  = detail::Measure(J_q) * dxi;
 
-      // 	// evaluate the value/derivatives needed for the q-function at this quadrature point
-      // 	auto arg = detail::Preprocess<trial_element>(u_elem, xi, J_q);
+      	// evaluate the value/derivatives needed for the q-function at this quadrature point
+      	auto arg = detail::Preprocess<trial_element>(u_elem, xi, J_q);
 
-      // 	// evaluate the user-specified constitutive model
-      // 	//
-      // 	// note: make_dual(arg) promotes those arguments to dual number types
-      // 	// so that qf_output will contain values and derivatives
-      // 	auto qf_output = qf(x_q, make_dual(arg));
+      	// evaluate the user-specified constitutive model
+      	//
+      	// note: make_dual(arg) promotes those arguments to dual number types
+      	// so that qf_output will contain values and derivatives
+      	auto qf_output = qf(x_q, make_dual(arg));
 
-      // 	// integrate qf_output against test space shape functions / gradients
-      // 	// to get element residual contributions
-      // 	r_elem += detail::Postprocess<test_element>(get_value(qf_output), xi, J_q) * dx;
+      	// integrate qf_output against test space shape functions / gradients
+      	// to get element residual contributions
+      	r_elem += detail::Postprocess<test_element>(get_value(qf_output), xi, J_q) * dx;
 
-      // 	// here, we store the derivative of the q-function w.r.t. its input arguments
-      // 	//
-      // 	// this will be used by other kernels to evaluate gradients / adjoints / directional derivatives
-      // 	derivatives_ptr[e * int(rule.size()) + q] = get_gradient(qf_output);
-      // }
+      	// here, we store the derivative of the q-function w.r.t. its input arguments
+      	//
+      	// this will be used by other kernels to evaluate gradients / adjoints / directional derivatives
+	// NOTE: Comment this back in later!
+	//      	derivatives_ptr[e * int(rule.size()) + q] = get_gradient(qf_output);
+      }
 
-      // // once we've finished the element integration loop, write our element residuals
-      // // out to memory, to be later assembled into global residuals by mfem
-      // detail::Add(r, r_elem, e);
+      // once we've finished the element integration loop, write our element residuals
+      // out to memory, to be later assembled into global residuals by mfem
+      detail::Add(r, r_elem, e);
 
     } // e loop
 
@@ -127,17 +128,30 @@ const mfem::Vector& J_, const mfem::Vector& X_, int num_elements, lambda qf)
   auto u = detail::Reshape<trial>(U.Read(), trial_ndof, num_elements);
   auto r = detail::Reshape<test>(R.ReadWrite(), test_ndof, num_elements);
 
+
+  uint32_t num_quadrature_points = rule.size() * num_elements;
+  // derivatives_type * qf_derivatives_temp;
+  // cudaMalloc(&qf_derivatives_temp, sizeof(derivatives_type) * num_quadrature_points);
+  int* qf_derivatives_temp;
+  cudaMalloc(&qf_derivatives_temp, sizeof(int) * num_quadrature_points);
+
   
   serac::detail::displayLastCUDAErrorMessage(std::cout, "integral_cuda.cuh before eval_cuda is fine");
+
 
   //testing <<<1,1>>> ();
   // eval_cuda<g, test, trial, geometry_dim, spatial_dim, Q> <<<1,1>>>(u, r, derivatives_ptr, J, X, num_elements, qf  );
 
-  eval_cuda<g, test, trial, geometry_dim, spatial_dim, Q, derivatives_type ><<<1,1>>>(num_elements);
+  // Note: CUDA does not seem to like derivatives_type?
+  // eval_cuda<g, test, trial, geometry_dim, spatial_dim, Q, derivatives_type, lambda ><<<1,1>>>(u, r, qf_derivatives_temp, J, X, num_elements, qf);
+
+  eval_cuda<g, test, trial, geometry_dim, spatial_dim, Q, int, lambda ><<<1,1>>>(u, r, qf_derivatives_temp, J, X, num_elements, qf);
 
 
   serac::detail::displayLastCUDAErrorMessage(std::cout, "integral_cuda.cuh after eval_cuda is fine");
   cudaDeviceSynchronize();
+
+  cudaFree(qf_derivatives_temp);
 
   // copy back to host?
   R.HostRead();
