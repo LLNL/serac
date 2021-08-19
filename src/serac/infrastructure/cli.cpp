0f089b0c512223cb13167af78fdb5a005df25286
Chris White <white238@llnl.gov>
2021-08-19 15:25:04 -0700

    switch default for output fields command line variable

--- src/serac/infrastructure/cli.cpp
+++ src/serac/infrastructure/cli.cpp
@@ -27,8 +27,8 @@ std::unordered_map<std::string, std::string> defineAndParse(int argc, char* argv
   bool create_input_file_docs{false};
   app.add_flag("-d, --create-input-file-docs", create_input_file_docs,
                "Writes Sphinx documentation for input file, then exits");
-  bool output_fields{true};
-  app.add_flag("--output-fields,!--no-output-fields", output_fields, "Writes field data to file system.");
+  bool output_fields{false};
+  app.add_flag("--output-fields", output_fields, "Writes field data to file system.");
   std::string output_directory;
   app.add_option("-o, --output_directory", output_directory, "Directory to put outputted files.");
 
