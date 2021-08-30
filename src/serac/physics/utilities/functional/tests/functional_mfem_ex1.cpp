872e74335fa304464e647e9ab21047bd951020a4
Jamie A. Bramwell <bramwell1@llnl.gov>
2021-08-30 15:00:16 -0700

    style: Put the indentation back in the test file

--- src/serac/physics/utilities/functional/tests/functional_mfem_ex1.cpp
+++ src/serac/physics/utilities/functional/tests/functional_mfem_ex1.cpp
@@ -30,69 +30,69 @@ constexpr int p   = 1;
 int main(int argc, char*argv[])
 {
 
-int num_procs, myid;
-MPI_Init(&argc, &argv);
-MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  int num_procs, myid;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
-axom::slic::SimpleLogger logger;
+  axom::slic::SimpleLogger logger;
 
-serac::profiling::initializeCaliper();
+  serac::profiling::initializeCaliper();
 
-int serial_refinement   = 1;
-int parallel_refinement = 0;
+  int serial_refinement   = 1;
+  int parallel_refinement = 0;
 
-std::string meshfile2D = SERAC_REPO_DIR "/data/meshes/star.mesh";
-auto        mesh = mesh::refineAndDistribute(buildMeshFromFile(meshfile2D), serial_refinement, parallel_refinement);
+  std::string meshfile2D = SERAC_REPO_DIR "/data/meshes/star.mesh";
+  auto        mesh = mesh::refineAndDistribute(buildMeshFromFile(meshfile2D), serial_refinement, parallel_refinement);
 
-// Create standard MFEM bilinear and linear forms on H1
-auto                        fec = mfem::H1_FECollection(p, dim);
-mfem::ParFiniteElementSpace fespace(mesh.get(), &fec);
+  // Create standard MFEM bilinear and linear forms on H1
+  auto                        fec = mfem::H1_FECollection(p, dim);
+  mfem::ParFiniteElementSpace fespace(mesh.get(), &fec);
 
-// Set a random state to evaluate the residual
-mfem::ParGridFunction u_global(&fespace);
-u_global.Randomize();
+  // Set a random state to evaluate the residual
+  mfem::ParGridFunction u_global(&fespace);
+  u_global.Randomize();
 
-mfem::Vector U(fespace.TrueVSize());
-u_global.GetTrueDofs(U);
+  mfem::Vector U(fespace.TrueVSize());
+  u_global.GetTrueDofs(U);
 
-// Define the types for the test and trial spaces using the function arguments
-using test_space  = H1<p>;
-using trial_space = H1<p>;
+  // Define the types for the test and trial spaces using the function arguments
+  using test_space  = H1<p>;
+  using trial_space = H1<p>;
 
-// Construct the new functional object using the known test and trial spaces
-Functional<test_space(trial_space)> residual(&fespace, &fespace);
+  // Construct the new functional object using the known test and trial spaces
+  Functional<test_space(trial_space)> residual(&fespace, &fespace);
 
-// Set the essential boundaries
-mfem::Array<int> ess_bdr(mesh->bdr_attributes.Max());
-ess_bdr = 1;
+  // Set the essential boundaries
+  mfem::Array<int> ess_bdr(mesh->bdr_attributes.Max());
+  ess_bdr = 1;
 
-residual.SetEssentialBC(ess_bdr);
+  residual.SetEssentialBC(ess_bdr);
 
-// Add the total domain residual term to the functional
-residual.AddDomainIntegral(
-    Dimension<dim>{},
-    [=]([[maybe_unused]] auto x, auto temperature) {
-      // get the value and the gradient from the input tuple
-      auto [u, du_dx] = temperature;
-      auto source     = -1.0;
-      auto flux       = du_dx;
-      return serac::tuple{source, flux};
-    },
-    *mesh);
+  // Add the total domain residual term to the functional
+  residual.AddDomainIntegral(
+      Dimension<dim>{},
+      [=]([[maybe_unused]] auto x, auto temperature) {
+        // get the value and the gradient from the input tuple
+        auto [u, du_dx] = temperature;
+        auto source     = -1.0;
+        auto flux       = du_dx;
+        return serac::tuple{source, flux};
+      },
+      *mesh);
 
-// Initialize the solution vector with the essential boundary values
-mfem::Vector solution(U.Size());
-solution = 1.0;
+  // Initialize the solution vector with the essential boundary values
+  mfem::Vector solution(U.Size());
+  solution = 0.001;
 
-mfem::Vector zero_vec(U.Size());
-zero_vec = 0.0;
+  mfem::Vector zero_vec(U.Size());
+  zero_vec = 0.0;
 
-mfem::Operator& grad = residual.GetGradient(solution);
+  mfem::Operator& grad = residual.GetGradient(solution);
 
-mfem::CG(grad, zero_vec, solution, 1, 50000, 1.0e-6, 1.0e-8);
+  mfem::CG(grad, zero_vec, solution, 1, 50000, 1.0e-6, 1.0e-8);
 
-serac::profiling::terminateCaliper();
+  serac::profiling::terminateCaliper();
 
-MPI_Finalize();
+  MPI_Finalize();
 }
