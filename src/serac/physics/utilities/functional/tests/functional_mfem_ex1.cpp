f366405f575925962f9c99167eb3b7ebd90bf341
Jamie A. Bramwell <bramwell1@llnl.gov>
2021-08-31 10:53:08 -0700

    style: Remove unnecessary includes

--- src/serac/physics/utilities/functional/tests/functional_mfem_ex1.cpp
+++ src/serac/physics/utilities/functional/tests/functional_mfem_ex1.cpp
@@ -4,25 +4,14 @@
 //
 // SPDX-License-Identifier: (BSD-3-Clause)
 
-#include <fstream>
-#include <iostream>
-
 #include "mfem.hpp"
 
 #include "axom/slic/core/SimpleLogger.hpp"
-#include "serac/infrastructure/input.hpp"
 #include "serac/serac_config.hpp"
-#include "serac/numerics/expr_template_ops.hpp"
 #include "serac/numerics/mesh_utils_base.hpp"
-#include "serac/physics/operators/stdfunction_operator.hpp"
 #include "serac/physics/utilities/functional/functional.hpp"
-#include "serac/physics/utilities/functional/tensor.hpp"
-#include "serac/numerics/assembled_sparse_matrix.hpp"
-#include "serac/infrastructure/profiling.hpp"
-#include "serac/physics/utilities/functional/tuple.hpp"
 
 using namespace serac;
-using namespace serac::profiling;
 
 constexpr int dim = 2;
 constexpr int p   = 1;
