ffd3eacf879a377f466129cef016ecdf8ff7089f
Chris White <white238@llnl.gov>
2021-09-03 13:26:35 -0700

    move command line to own function

--- scripts/azure-pipelines/check_for_docs_warnings.py
+++ scripts/azure-pipelines/check_for_docs_warnings.py
@@ -13,29 +13,39 @@ import sys
 def print_usage():
     print("Usage: " + sys.argv[0] + " <path to documentation build output>")
 
-def main():
-    # Check usage
+def handle_command_line():
+    # Check for help
     if len(sys.argv) > 1 and ("--help" in sys.argv or "-h" in sys.argv):
         print_usage()
-        return True
+        sys.exit(0)
 
+    # Check arg count
     if len(sys.argv) < 2:
         print("Error: Too few command line arguments given")
         print_usage()
-        return False
+        sys.exit(1)
 
     if len(sys.argv) > 2:
         print("Error: Too many command line arguments given")
         print_usage()
-        return False
+        sys.exit(1)
 
-    # Get file to be checked
+    # Check given file path
     file_path = sys.argv[1]
     if not os.path.isfile(file_path):
-        print("Log Path: {0}".format(file_path))
+        print("Error: Given path is not a file: {0}".format(file_path))
+        print_usage()
+        sys.exit(1)
+
+    print("Log Path: {0}".format(file_path))
+
+    return file_path
+
+
+def main()
+    file_path = handle_command_line()
 
     return True
-
 
 if __name__ == "__main__":
     sys.exit(main())
