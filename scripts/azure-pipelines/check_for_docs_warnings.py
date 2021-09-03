bae1251eefb39bdd6d5d25313eda9b9e3498cf85
Chris White <white238@llnl.gov>
2021-09-03 14:10:45 -0700

    convert to argparse because i should have done it to begin with

--- scripts/azure-pipelines/check_for_docs_warnings.py
+++ scripts/azure-pipelines/check_for_docs_warnings.py
@@ -7,45 +7,28 @@
 # SPDX-License-Identifier: (BSD-3-Clause)
 ##############################################################################
 
+import argparse
 import os
 import sys
 
-def print_usage():
-    print("Usage: " + sys.argv[0] + " <path to documentation build output>")
+def parse_args():
+    parser = argparse.ArgumentParser(description="Checks log for warnings and errors")
 
-def handle_command_line():
-    # Check for help
-    if len(sys.argv) > 1 and ("--help" in sys.argv or "-h" in sys.argv):
-        print_usage()
-        sys.exit(0)
+    parser.add_argument("-l","--log", type=str, required=True, help="Path to log file to be checked") 
 
-    # Check arg count
-    if len(sys.argv) < 2:
-        print("Error: Too few command line arguments given")
-        print_usage()
-        sys.exit(1)
+    args = parser.parse_args()
 
-    if len(sys.argv) > 2:
-        print("Error: Too many command line arguments given")
-        print_usage()
-        sys.exit(1)
+    print("~~~~~~~ Given Command line Arguments ~~~~~~~")
+    print("Log Path: {0}".format(args.log))
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 
-    # Check given file path
-    log_path = sys.argv[1]
-    if not os.path.isfile(log_path):
-        print("Error: Given path is not a file: {0}".format(log_path))
-        print_usage()
-        sys.exit(1)
-
-    print("Log Path: {0}".format(log_path))
-
-    return log_path
+    return args
 
 
 def main():
-    log_path = handle_command_line()
+    args = parse_args()
 
-    with open(log_path, "r") as f:
+    with open(args.log, "r") as f:
         log_lines = f.readlines()
 
     # Get warnings/errors out of log file
