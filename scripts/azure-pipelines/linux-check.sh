8f105d85cb2e2b4112465c77d5284195574d9134
Chris White <white238@llnl.gov>
2021-09-09 19:29:20 -0700

    use new docker images, add ignore

--- scripts/azure-pipelines/linux-check.sh
+++ scripts/azure-pipelines/linux-check.sh
@@ -69,7 +69,7 @@ fi
 
 if [[ "$DO_DOCS_CHECK" == "yes" ]] ; then
     or_die make VERBOSE=1 docs 2>&1 | tee docs_output
-    or_die ../scripts/check_log.py -l docs_output
+    or_die ../scripts/check_log.py -l docs_output -i ../scripts/azure-pipelines/docs_ignore_regexs.txt
 fi
 
 if [[ "$DO_STYLE_CHECK" == "yes" ]] ; then
