befe198e7ed02d6742cb0ce29e37a5e1d9776d22
Sam Mish <mish2@llnl.gov>
2021-09-30 12:30:59 -0700

    update type and member naming in dof_numbering.hpp

--- src/serac/physics/utilities/functional/dof_numbering.hpp
+++ src/serac/physics/utilities/functional/dof_numbering.hpp
@@ -6,52 +6,51 @@
 
 namespace serac {
 
-struct elem_info {
-  int  global_row;
-  int  global_col;
-  int  local_row;
-  int  local_col;
-  int  element_id;
-  int  sign;
-  bool on_boundary;
+struct ElemInfo {
+  int  global_row_;
+  int  global_col_;
+  int  local_row_;
+  int  local_col_;
+  int  element_id_;
+  int  sign_;
+  bool on_boundary_;
 };
 
 // for sorting lexicographically by {global_row, global_col}
-bool operator<(const elem_info& x, const elem_info& y)
+bool operator<(const ElemInfo & x, const ElemInfo & y)
 {
-  return (x.global_row < y.global_row) || (x.global_row == y.global_row && x.global_col < y.global_col);
+  return (x.global_row_ < y.global_row_) || (x.global_row_ == y.global_row_ && x.global_col_ < y.global_col_);
 }
 
-bool operator!=(const elem_info& x, const elem_info& y)
+bool operator!=(const ElemInfo & x, const ElemInfo & y)
 {
-  return (x.global_row != y.global_row) || (x.global_col != y.global_col);
+  return (x.global_row_ != y.global_row_) || (x.global_col_ != y.global_col_);
 }
 
-auto& operator<<(std::ostream& out, elem_info e)
+auto& operator<<(std::ostream& out, ElemInfo e)
 {
-  out << e.global_row << ", ";
-  out << e.global_col << ", ";
-  out << e.local_row << ", ";
-  out << e.local_col << ", ";
-  out << e.element_id << ", ";
-  out << e.on_boundary;
+  out << e.global_row_ << ", ";
+  out << e.global_col_ << ", ";
+  out << e.local_row_ << ", ";
+  out << e.local_col_ << ", ";
+  out << e.element_id_ << ", ";
+  out << e.on_boundary_;
   return out;
 }
 
 int mfem_sign(int i) { return (i >= 0) ? 1 : -1; }
 int mfem_index(int i) { return (i >= 0) ? i : -1 - i; }
 
-struct signed_index {
-  int index;
-  int sign;
-
-  operator int() { return index; }
+struct SignedIndex {
+  int index_;
+  int sign_;
+  operator int() { return index_; }
 };
 
 struct DofNumbering {
   DofNumbering(const mfem::ParFiniteElementSpace& fespace)
-      : element_dofs(fespace.GetNE(), fespace.GetFE(0)->GetDof() * fespace.GetVDim()),
-        boundary_element_dofs(fespace.GetNFbyType(mfem::FaceType::Boundary),
+      : element_dofs_(fespace.GetNE(), fespace.GetFE(0)->GetDof() * fespace.GetVDim()),
+        boundary_element_dofs_(fespace.GetNFbyType(mfem::FaceType::Boundary),
                               fespace.GetBE(0)->GetDof() * fespace.GetVDim())
   {
     {
@@ -67,10 +66,10 @@ struct DofNumbering {
       elem_restriction->Mult(iota, dof_ids);
       const double * dof_ids_h = dof_ids.HostRead();
 
-      for (size_t e = 0; e < element_dofs.size(0); e++) {
-        for (size_t i = 0; i < element_dofs.size(1); i++) {
-          int mfem_id        = static_cast<int>(dof_ids_h[element_dofs.index(e, i)]);
-          element_dofs(e, i) = signed_index{mfem_index(mfem_id), mfem_sign(mfem_id)};
+      for (size_t e = 0; e < element_dofs_.size(0); e++) {
+        for (size_t i = 0; i < element_dofs_.size(1); i++) {
+          int mfem_id        = static_cast<int>(dof_ids_h[element_dofs_.index(e, i)]);
+          element_dofs_(e, i) = SignedIndex{mfem_index(mfem_id), mfem_sign(mfem_id)};
         }
       }
     }
@@ -88,17 +87,17 @@ struct DofNumbering {
       face_restriction->Mult(iota, dof_ids);
       const double * dof_ids_h = dof_ids.HostRead();
 
-      for (size_t e = 0; e < boundary_element_dofs.size(0); e++) {
-        for (size_t i = 0; i < boundary_element_dofs.size(1); i++) {
-          int mfem_id                 = static_cast<int>(dof_ids_h[boundary_element_dofs.index(e, i)]);
-          boundary_element_dofs(e, i) = signed_index{mfem_index(mfem_id), mfem_sign(mfem_id)};
+      for (size_t e = 0; e < boundary_element_dofs_.size(0); e++) {
+        for (size_t i = 0; i < boundary_element_dofs_.size(1); i++) {
+          int mfem_id                 = static_cast<int>(dof_ids_h[boundary_element_dofs_.index(e, i)]);
+          boundary_element_dofs_(e, i) = SignedIndex{mfem_index(mfem_id), mfem_sign(mfem_id)};
         }
       }
     }
   }
 
-  serac::CPUArray<signed_index, 2> element_dofs;
-  serac::CPUArray<signed_index, 2> boundary_element_dofs;
+  serac::CPUArray<SignedIndex, 2> element_dofs_;
+  serac::CPUArray<SignedIndex, 2> boundary_element_dofs_;
 };
 
 struct GradientAssemblyLookupTables {
@@ -109,22 +108,22 @@ struct GradientAssemblyLookupTables {
                                      test_fespace.GetBE(0)->GetDof() * test_fespace.GetVDim(),
                                      trial_fespace.GetBE(0)->GetDof() * trial_fespace.GetVDim())
   {
-    DofNumbering test_dofs_(test_fespace);
-    DofNumbering trial_dofs_(trial_fespace);
+    DofNumbering test_dofs(test_fespace);
+    DofNumbering trial_dofs(trial_fespace);
 
     int num_elements          = trial_fespace.GetNE();
     int num_boundary_elements = trial_fespace.GetNFbyType(mfem::FaceType::Boundary);
 
-    std::vector<elem_info> infos;
+    std::vector<ElemInfo> infos;
 
     bool on_boundary = false;
     for (int e = 0; e < num_elements; e++) {
-      for (int i = 0; i < int(test_dofs_.element_dofs.size(1)); i++) {
-        auto test_dof = test_dofs_.element_dofs(e, i);
-        for (int j = 0; j < int(trial_dofs_.element_dofs.size(1)); j++) {
-          auto trial_dof = trial_dofs_.element_dofs(e, j);
+      for (int i = 0; i < int(test_dofs.element_dofs_.size(1)); i++) {
+        auto test_dof = test_dofs.element_dofs_(e, i);
+        for (int j = 0; j < int(trial_dofs.element_dofs_.size(1)); j++) {
+          auto trial_dof = trial_dofs.element_dofs_(e, j);
           infos.push_back(
-              elem_info{test_dof, trial_dof, i, j, e, mfem_sign(test_dof) * mfem_sign(trial_dof), on_boundary});
+              ElemInfo{test_dof, trial_dof, i, j, e, mfem_sign(test_dof) * mfem_sign(trial_dof), on_boundary});
         }
       }
     }
@@ -134,12 +133,12 @@ struct GradientAssemblyLookupTables {
     // an empty 2D array, so these loops will not do anything
     on_boundary = true;
     for (int e = 0; e < num_boundary_elements; e++) {
-      for (int i = 0; i < int(test_dofs_.boundary_element_dofs.size(1)); i++) {
-        auto test_dof = test_dofs_.boundary_element_dofs(e, i);
-        for (int j = 0; j < int(trial_dofs_.boundary_element_dofs.size(1)); j++) {
-          auto trial_dof = trial_dofs_.boundary_element_dofs(e, j);
+      for (int i = 0; i < int(test_dofs.boundary_element_dofs_.size(1)); i++) {
+        auto test_dof = test_dofs.boundary_element_dofs_(e, i);
+        for (int j = 0; j < int(trial_dofs.boundary_element_dofs_.size(1)); j++) {
+          auto trial_dof = trial_dofs.boundary_element_dofs_(e, j);
           infos.push_back(
-              elem_info{test_dof, trial_dof, i, j, e, mfem_sign(test_dof) * mfem_sign(trial_dof), on_boundary});
+              ElemInfo{test_dof, trial_dof, i, j, e, mfem_sign(test_dof) * mfem_sign(trial_dof), on_boundary});
         }
       }
     }
@@ -148,24 +147,24 @@ struct GradientAssemblyLookupTables {
 
     row_ptr.resize(test_fespace.GetNDofs() * test_fespace.GetVDim() + 1);
 
-    std::vector<signed_index> nonzero_ids(infos.size());
+    std::vector<SignedIndex> nonzero_ids(infos.size());
 
     nnz        = 0;
     row_ptr[0] = 0;
-    col_ind.push_back(infos[0].global_col);
-    nonzero_ids[0] = {0, infos[0].sign};
+    col_ind.push_back(infos[0].global_col_);
+    nonzero_ids[0] = {0, infos[0].sign_};
 
     for (size_t i = 1; i < infos.size(); i++) {
       // increment the nonzero count every time we find a new (i,j) pair
       nnz += (infos[i - 1] != infos[i]);
 
-      nonzero_ids[i] = signed_index{nnz, infos[i].sign};
+      nonzero_ids[i] = SignedIndex{nnz, infos[i].sign_};
 
       if (infos[i - 1] != infos[i]) {
-        col_ind.push_back(infos[i].global_col);
+        col_ind.push_back(infos[i].global_col_);
       }
 
-      for (int j = infos[i - 1].global_row; j < infos[i].global_row; j++) {
+      for (int j = infos[i - 1].global_row_; j < infos[i].global_row_; j++) {
         row_ptr[j + 1] = nonzero_ids[i];
       }
     }
@@ -186,8 +185,8 @@ struct GradientAssemblyLookupTables {
   std::vector<int> row_ptr;
   std::vector<int> col_ind;
 
-  serac::CPUArray<signed_index, 3> element_nonzero_LUT;
-  serac::CPUArray<signed_index, 3> boundary_element_nonzero_LUT;
+  serac::CPUArray<SignedIndex, 3> element_nonzero_LUT;
+  serac::CPUArray<SignedIndex, 3> boundary_element_nonzero_LUT;
 };
 
 }  // namespace serac
