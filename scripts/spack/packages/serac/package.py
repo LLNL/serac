445aafd4a7228609adaac80ad38703c88fa2add9
Josh Essman <essman1@llnl.gov>
2021-10-19 12:10:20 -0500

    build: update chai/raja/umpire version

--- scripts/spack/packages/serac/package.py
+++ scripts/spack/packages/serac/package.py
@@ -111,12 +111,11 @@ class Serac(CachedCMakePackage, CudaPackage):
     depends_on("axom~raja", when="~raja")
     depends_on("axom~umpire", when="~umpire")
     depends_on("camp@0.1.0serac", when="+raja")
-    depends_on("raja@0.13.1serac~openmp~shared", when="+raja")
-    depends_on("umpire@5.0.1~shared", when="+umpire")
+    depends_on("raja@0.14.0~openmp~shared", when="+raja")
+    depends_on("umpire@6.0.0~shared", when="+umpire")
     # Lump in CHAI with Umpire for now
-    depends_on("chai@2.3.1serac~shared", when="+umpire")
-    # Our RAJA version is too new for CHAI
-    # depends_on("chai+raja", when="+umpire+raja")
+    depends_on("chai@2.4.0serac~shared+raja", when="+cuda+umpire")
+    depends_on("chai+raja", when="+cuda+raja")
 
     # Libraries that support "build_type=RelWithDebInfo|Debug|Release|MinSizeRel"
     # "build_type=RelWithDebInfo|Debug|Release|MinSizeRel"
