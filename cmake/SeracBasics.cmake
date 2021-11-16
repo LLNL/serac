59af0f368cd6abe665abafb714adcb354a2c135c
Chris White <white238@llnl.gov>
2021-11-16 13:27:14 -0800

    remove global includes

--- cmake/SeracBasics.cmake
+++ cmake/SeracBasics.cmake
@@ -37,10 +37,3 @@ if(GLVIS_EXECUTABLE)
                       COMMAND ${CMAKE_COMMAND} 
                       -E create_symlink ${GLVIS_EXECUTABLE} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/glvis)
 endif()
-
-
-#------------------------------------------------------------------------------
-# Global includes (restrict these as much as possible)
-#------------------------------------------------------------------------------
-include_directories(${CMAKE_BINARY_DIR}/include)
-include_directories(${PROJECT_SOURCE_DIR})
