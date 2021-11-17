5725b2de8a8261f9a58b9afac8386a77287e9fc7
Josh Essman <essman1@llnl.gov>
2021-11-17 16:52:44 -0600

    refactor: add support for multiple datacollections

--- src/serac/physics/state/state_manager.cpp
+++ src/serac/physics/state/state_manager.cpp
@@ -21,90 +21,115 @@ QuadratureData<void> dummy_qdata;
 QuadratureDataView<void> dummy_qdata_view;
 
 // Initialize StateManager's static members - both of these will be fully initialized in StateManager::initialize
-std::optional<axom::sidre::MFEMSidreDataCollection> StateManager::datacoll_;
-bool                                                StateManager::is_restart_      = false;
-std::string                                         StateManager::collection_name_ = "";
-std::vector<std::unique_ptr<SyncableData>>          StateManager::syncable_data_;
+std::unordered_map<std::string, axom::sidre::MFEMSidreDataCollection> StateManager::datacolls_;
+bool                                                                  StateManager::is_restart_      = false;
+std::string                                                           StateManager::collection_name_ = "";
+std::vector<std::unique_ptr<SyncableData>>                            StateManager::syncable_data_;
+axom::sidre::DataStore*                                               StateManager::ds_         = nullptr;
+std::string                                                           StateManager::output_dir_ = "";
+std::optional<int>                                                    StateManager::cycle_to_load_;
+const std::string                                                     StateManager::primary_mesh_name_ = "primary";
 
-void StateManager::initialize(axom::sidre::DataStore& ds, const std::string& collection_name_prefix,
-                              const std::string output_directory, const std::optional<int> cycle_to_load)
+void StateManager::newDataCollection(const std::string& name)
 {
-  // If the global object has already been initialized, clear it out
-  if (datacoll_) {
-    reset();
-  }
-
-  collection_name_ = collection_name_prefix + "_datacoll";
-
-  auto global_grp   = ds.getRoot()->createGroup(collection_name_ + "_global");
-  auto bp_index_grp = global_grp->createGroup("blueprint_index/" + collection_name_);
-  auto domain_grp   = ds.getRoot()->createGroup(collection_name_);
+  SLIC_ERROR_ROOT_IF(!ds_, "Cannot construct a DataCollection without a DataStore");
+  std::string coll_name    = name + "_datacoll";
+  auto        global_grp   = ds_->getRoot()->createGroup(coll_name + "_global");
+  auto        bp_index_grp = global_grp->createGroup("blueprint_index/" + coll_name);
+  auto        domain_grp   = ds_->getRoot()->createGroup(coll_name);
 
   // Needs to be configured to own the mesh data so all mesh data is saved to datastore/output file
-  const bool owns_mesh_data = true;
-  datacoll_.emplace(collection_name_, bp_index_grp, domain_grp, owns_mesh_data);
-  datacoll_->SetComm(MPI_COMM_WORLD);
+  constexpr bool owns_mesh_data = true;
+  auto [iter, _]                = datacolls_.emplace(std::piecewise_construct, std::forward_as_tuple(name),
+                                      std::forward_as_tuple(coll_name, bp_index_grp, domain_grp, owns_mesh_data));
+  auto& datacoll                = iter->second;
+  datacoll.SetComm(MPI_COMM_WORLD);
 
-  if (output_directory.empty()) {
-    SLIC_ERROR_ROOT(
-        "DataCollection output directory cannot be empty - this will result in problems if executables are run in "
-        "parallel");
-  } else {
-    datacoll_->SetPrefixPath(output_directory);
-  }
+  datacoll.SetPrefixPath(output_dir_);
 
-  if (cycle_to_load) {
-    is_restart_ = true;
+  if (is_restart_) {
     // NOTE: Load invalidates previous Sidre pointers
-    datacoll_->Load(*cycle_to_load);
-    datacoll_->SetGroupPointers(
-        ds.getRoot()->getGroup(collection_name_ + "_global/blueprint_index/" + collection_name_),
-        ds.getRoot()->getGroup(collection_name_));
-    SLIC_ERROR_ROOT_IF(datacoll_->GetBPGroup()->getNumGroups() == 0,
+    datacoll.Load(*cycle_to_load_);
+    datacoll.SetGroupPointers(ds_->getRoot()->getGroup(coll_name + "_global/blueprint_index/" + coll_name),
+                              ds_->getRoot()->getGroup(coll_name));
+    SLIC_ERROR_ROOT_IF(datacoll.GetBPGroup()->getNumGroups() == 0,
                        "Loaded datastore is empty, was the datastore created on a "
                        "different number of nodes?");
 
-    datacoll_->UpdateStateFromDS();
-    datacoll_->UpdateMeshAndFieldsFromDS();
+    datacoll.UpdateStateFromDS();
+    datacoll.UpdateMeshAndFieldsFromDS();
 
     // Functional needs the nodal grid function and neighbor data in the mesh
     mesh().EnsureNodes();
     mesh().ExchangeFaceNbrData();
 
   } else {
-    datacoll_->SetCycle(0);   // Iteration counter
-    datacoll_->SetTime(0.0);  // Simulation time
+    datacoll.SetCycle(0);   // Iteration counter
+    datacoll.SetTime(0.0);  // Simulation time
   }
 }
 
-FiniteElementState StateManager::newState(FiniteElementVector::Options&& options)
+void StateManager::initialize(axom::sidre::DataStore& ds, const std::string&, const std::string output_directory,
+                              const std::optional<int> cycle_to_load)
 {
-  SLIC_ERROR_ROOT_IF(!datacoll_, "Serac's datacollection was not initialized - call StateManager::initialize first");
-  const std::string name = options.name;
+  // If the global object has already been initialized, clear it out
+  if (ds_) {
+    reset();
+  }
+  ds_         = &ds;
+  output_dir_ = output_directory;
+  if (output_directory.empty()) {
+    SLIC_ERROR_ROOT(
+        "DataCollection output directory cannot be empty - this will result in problems if executables are run in "
+        "parallel");
+  }
+  // else {
+  //   datacoll_->SetPrefixPath(output_directory);
+  // }
+  cycle_to_load_ = cycle_to_load;
+
+  if (cycle_to_load) {
+    is_restart_ = true;
+    // FIXME FIXME THIS IS VERY WRONG
+    // Each datacollection corresponds to its own set of files right now
+    // but we need to keep track of what datacollections were created
+    // or should we try to stuff them all into one save file?
+    // this will break if a) someone makes a non-primary datacollection
+    // or if someone doesn't make a primary datacollection
+    newDataCollection(primary_mesh_name_);
+  }
+}
+
+FiniteElementState StateManager::newState(FiniteElementVector::Options&& options, const std::string& tag)
+{
+  SLIC_ERROR_ROOT_IF(!ds_, "Serac's datacollection was not initialized - call StateManager::initialize first");
+  auto&             datacoll = datacolls_.at(tag);
+  const std::string name     = options.name;
   if (is_restart_) {
-    auto field = datacoll_->GetParField(name);
+    auto field = datacoll.GetParField(name);
     return {mesh(), *field, name};
   } else {
-    SLIC_ERROR_ROOT_IF(datacoll_->HasField(name),
+    SLIC_ERROR_ROOT_IF(datacoll.HasField(name),
                        axom::fmt::format("Serac's datacollection was already given a field named '{0}'", name));
     options.managed_by_sidre = true;
     FiniteElementState state(mesh(), std::move(options));
-    datacoll_->RegisterField(name, &(state.gridFunc()));
+    datacoll.RegisterField(name, &(state.gridFunc()));
     // Now that it's been allocated, we can set it to zero
     state.gridFunc() = 0.0;
     return state;
   }
 }
 
-FiniteElementDual StateManager::newDual(FiniteElementVector::Options&& options)
+FiniteElementDual StateManager::newDual(FiniteElementVector::Options&& options, const std::string& tag)
 {
-  SLIC_ERROR_ROOT_IF(!datacoll_, "Serac's datacollection was not initialized - call StateManager::initialize first");
-  const std::string name = options.name;
+  SLIC_ERROR_ROOT_IF(!ds_, "Serac's datacollection was not initialized - call StateManager::initialize first");
+  auto&             datacoll = datacolls_.at(tag);
+  const std::string name     = options.name;
   if (is_restart_) {
-    auto field = datacoll_->GetParField(name);
+    auto field = datacoll.GetParField(name);
     return {mesh(), *field, name};
   } else {
-    SLIC_ERROR_ROOT_IF(datacoll_->HasField(name),
+    SLIC_ERROR_ROOT_IF(datacoll.HasField(name),
                        axom::fmt::format("Serac's datacollection was already given a field named '{0}'", name));
     options.managed_by_sidre = true;
     FiniteElementDual dual(mesh(), std::move(options));
@@ -115,43 +140,45 @@ FiniteElementDual StateManager::newDual(FiniteElementVector::Options&& options)
     // are inappropriate for dual vectors.
     auto gf_view_of_local_dual_vector = static_cast<mfem::ParGridFunction*>(&dual.localVec());
 
-    datacoll_->RegisterField(name, gf_view_of_local_dual_vector);
+    datacoll.RegisterField(name, gf_view_of_local_dual_vector);
     // Now that it's been allocated, we can set it to zero
     dual.localVec() = 0.0;
     return dual;
   }
 }
 
-void StateManager::save(const double t, const int cycle)
+void StateManager::save(const double t, const int cycle, const std::string& tag)
 {
-  SLIC_ERROR_ROOT_IF(!datacoll_, "Serac's datacollection was not initialized - call StateManager::initialize first");
-
-  std::string file_path =
-      axom::utilities::filesystem::joinPath(datacoll_->GetPrefixPath(), datacoll_->GetCollectionName());
+  SLIC_ERROR_ROOT_IF(!ds_, "Serac's datacollection was not initialized - call StateManager::initialize first");
+  auto&       datacoll  = datacolls_.at(tag);
+  std::string file_path = axom::utilities::filesystem::joinPath(datacoll.GetPrefixPath(), datacoll.GetCollectionName());
   SLIC_INFO_ROOT(axom::fmt::format("Saving data collection at time: {} to path: {}", t, file_path));
 
   for (const auto& data : syncable_data_) {
     data->sync();
   }
-  datacoll_->SetTime(t);
-  datacoll_->SetCycle(cycle);
-  datacoll_->Save();
+  datacoll.SetTime(t);
+  datacoll.SetCycle(cycle);
+  datacoll.Save();
 }
 
-void StateManager::setMesh(std::unique_ptr<mfem::ParMesh> pmesh)
+void StateManager::setMesh(std::unique_ptr<mfem::ParMesh> pmesh, const std::string& tag)
 {
-  datacoll_->SetMesh(pmesh.release());
-  datacoll_->SetOwnData(true);
+  newDataCollection(tag);
+  auto& datacoll = datacolls_.at(tag);
+  datacoll.SetMesh(pmesh.release());
+  datacoll.SetOwnData(true);
 
   // Functional needs the nodal grid function and neighbor data in the mesh
-  StateManager::mesh().EnsureNodes();
-  StateManager::mesh().ExchangeFaceNbrData();
+  auto& new_pmesh = mesh(tag);
+  new_pmesh.EnsureNodes();
+  new_pmesh.ExchangeFaceNbrData();
 }
 
-mfem::ParMesh& StateManager::mesh()
+mfem::ParMesh& StateManager::mesh(const std::string& tag)
 {
-  auto mesh = datacoll_->GetMesh();
-  SLIC_ERROR_ROOT_IF(!mesh, "The datastore does not contain a mesh object");
+  auto mesh = datacolls_.at(tag).GetMesh();
+  SLIC_ERROR_ROOT_IF(!mesh, "The datacollection does not contain a mesh object");
   return static_cast<mfem::ParMesh&>(*mesh);
 }
 
--- src/serac/physics/state/state_manager.hpp
+++ src/serac/physics/state/state_manager.hpp
@@ -13,6 +13,7 @@
 #pragma once
 
 #include <optional>
+#include <unordered_map>
 
 #include "mfem.hpp"
 #include "axom/sidre/core/MFEMSidreDataCollection.hpp"
@@ -34,11 +35,13 @@ public:
    * @brief Initializes the StateManager with a sidre DataStore (into which state will be written/read)
    * @param[in] ds The DataStore to use
    * @param[in] collection_name_prefix The prefix for the name of the Sidre DataCollection to be created
+   * FIXME: Remove collection_name_prefix - No longer needed since we associate a mesh/datacoll with a string elsewhere
    * @param[in] output_directory The directory to output files to - cannot be empty
    * @param[in] cycle_to_load The cycle to load - required for restarts
+   * FIXME: Would we want to have different meshes at different cycles? Maybe need a method @p restart(name,cycle)
    */
-  static void initialize(axom::sidre::DataStore& ds, const std::string& collection_name_prefix,
-                         const std::string output_directory, const std::optional<int> cycle_to_load = {});
+  static void initialize(axom::sidre::DataStore& ds, const std::string output_directory,
+                         const std::optional<int> cycle_to_load = {});
 
   /**
    * @brief Factory method for creating a new FEState object, signature is identical to FEState constructor
@@ -46,7 +49,8 @@ public:
    * @see FiniteElementState::FiniteElementState
    * @note If this is a restart then the options (except for the name) will be ignored
    */
-  static FiniteElementState newState(FiniteElementVector::Options&& options = {});
+  static FiniteElementState newState(FiniteElementVector::Options&& options = {},
+                                     const std::string&             tag     = primary_mesh_name_);
 
   /**
    * @brief Factory method for creating a new FEDual object, signature is identical to FEDual constructor
@@ -54,7 +58,8 @@ public:
    * @see FiniteElementDual::FiniteElementDual
    * @note If this is a restart then the options (except for the name) will be ignored
    */
-  static FiniteElementDual newDual(FiniteElementVector::Options&& options = {});
+  static FiniteElementDual newDual(FiniteElementVector::Options&& options = {},
+                                   const std::string&             tag     = primary_mesh_name_);
 
   /**
    * @brief Factory method for creating a new QuadratureData object
@@ -64,19 +69,21 @@ public:
    * @see QuadratureData::QuadratureData
    */
   template <typename T>
-  static QuadratureData<T>& newQuadratureData(const std::string& name, const int p)
+  static QuadratureData<T>& newQuadratureData(const std::string& name, const int p,
+                                              const std::string& tag = primary_mesh_name_)
   {
+    auto& datacoll = datacolls_.at(tag);
     if (is_restart_) {
-      auto field = datacoll_->GetQField(name);
+      auto field = datacoll.GetQField(name);
       syncable_data_.push_back(std::make_unique<QuadratureData<T>>(*field));
       return static_cast<QuadratureData<T>&>(*syncable_data_.back());
     } else {
-      SLIC_ERROR_ROOT_IF(datacoll_->HasQField(name),
+      SLIC_ERROR_ROOT_IF(datacoll.HasQField(name),
                          axom::fmt::format("Serac's datacollection was already given a qfield named '{0}'", name));
       syncable_data_.push_back(std::make_unique<QuadratureData<T>>(mesh(), p, false));
       // The static_cast is safe here because we "know" what we just inserted into the vector
       auto& qdata = static_cast<QuadratureData<T>&>(*syncable_data_.back());
-      datacoll_->RegisterQField(name, &(qdata.QFunc()));
+      datacoll.RegisterQField(name, &(qdata.QFunc()));
       return qdata;
     }
   }
@@ -86,27 +93,28 @@ public:
    * @param[in] t The current sim time
    * @param[in] cycle The current iteration number of the simulation
    */
-  static void save(const double t, const int cycle);
+  static void save(const double t, const int cycle, const std::string& tag = primary_mesh_name_);
 
   /**
    * @brief Resets the underlying global datacollection object
    */
   static void reset()
   {
-    datacoll_.reset();
+    datacolls_.clear();
     is_restart_ = false;
     syncable_data_.clear();
+    ds_ = nullptr;
   };
 
   /**
    * @brief Gives ownership of mesh to StateManager
    */
-  static void setMesh(std::unique_ptr<mfem::ParMesh> pmesh);
+  static void setMesh(std::unique_ptr<mfem::ParMesh> pmesh, const std::string& tag = primary_mesh_name_);
 
   /**
    * @brief Returns a non-owning reference to mesh held by StateManager
    */
-  static mfem::ParMesh& mesh();
+  static mfem::ParMesh& mesh(const std::string& tag = primary_mesh_name_);
 
   /**
    * @brief Returns the Sidre DataCollection name
@@ -115,12 +123,15 @@ public:
 
 private:
   /**
-   * @brief The datacollection instance
-   *
-   * The std::optional is used here to allow for deferred construction on the stack.
+   * @brief Creates a new datacollection based on a registered mesh
+   */
+  static void newDataCollection(const std::string& name);
+
+  /**
+   * @brief The datacollection instances
    * The object is constructed when the user calls StateManager::initialize.
    */
-  static std::optional<axom::sidre::MFEMSidreDataCollection> datacoll_;
+  static std::unordered_map<std::string, axom::sidre::MFEMSidreDataCollection> datacolls_;
   /**
    * @brief Whether this simulation has been restarted from another simulation
    */
@@ -131,8 +142,18 @@ private:
   static std::string collection_name_;
   /**
    * @brief A set of @p QuadratureData<T> objects that need to be synchronized before saving to disk
+   * FIXME Need one set per datacoll since we can save at different times?
    */
   static std::vector<std::unique_ptr<SyncableData>> syncable_data_;
+
+  /// @brief Pointer (non-owning) to the datastore
+  static axom::sidre::DataStore* ds_;
+  /// @brief Output directory to which all datacollections are saved
+  static std::string output_dir_;
+  /// @brief Cycle to load - for restarts - FIXME can this be different between datacollections?
+  static std::optional<int> cycle_to_load_;
+  /// @brief Default name for the mesh - mostly for backwards compatibility
+  const static std::string primary_mesh_name_;
 };
 
 }  // namespace serac
