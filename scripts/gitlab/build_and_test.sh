9cd8a84da66dfad80aa1494416a9803d3d5dc19e
Chris White <white238@llnl.gov>
2022-01-18 19:28:03 -0800

    pull multiple commands into a script

---
+++ scripts/gitlab/build_and_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "DO_INTEGRATION_TESTS=${DO_INTEGRATION_TESTS}"
+echo "EXTRA_BUILD_OPTIONS=${EXTRA_BUILD_OPTIONS}"
+echo "HOST_CONFIG=${HOST_CONFIG}"
+
+# Build source, run unit tests, and test install examples
+python3 scripts/llnl/build_src.py -v --host-config ${HOST_CONFIG} ${EXTRA_BUILD_OPTIONS}
+
+# Run integration tests
+if [[ "$DO_INTEGRATION_TESTS" == "yes" ]] ; then
+    cd _serac_build_and_test_*/build-*
+    ./ats.sh
+fi
