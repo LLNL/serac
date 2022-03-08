e822df835e4f7fde03cbbf5d6cc2e6be01a3c4e5
Jamie A. Bramwell <bramwell1@llnl.gov>
2022-03-07 16:45:47 -0800

    doc: Add documentation to the parameterized material class

--- src/serac/physics/materials/parameterized_thermal_functional_material.hpp
+++ src/serac/physics/materials/parameterized_thermal_functional_material.hpp
@@ -17,37 +17,72 @@
 /// ThermalConductionFunctional helper structs
 namespace serac::Thermal {
 
+/// Linear isotropic conductor with a parameterized conductivity
 class ParameterizedLinearIsotropicConductor {
 public:
+
+  /**
+   * @brief Construct a new Parameterized Linear Isotropic Conductor object
+   * 
+   * @param density Density of the material (mass/volume)
+   * @param specific_heat_capacity Specific heat capacity of the material (energy / (mass * temp))
+   * @param conductivity_offset Thermal conductivity offset of the material (power / (length * temp)). This is 
+   * added to the parameter value to get the total conductivity.
+   */
   ParameterizedLinearIsotropicConductor(double density = 1.0, double specific_heat_capacity = 1.0,
-                                        double conductivity = 1.0)
-      : density_(density), specific_heat_capacity_(specific_heat_capacity), conductivity_(conductivity)
+                                        double conductivity_offset = 1.0)
+      : density_(density), specific_heat_capacity_(specific_heat_capacity), conductivity_offset_(conductivity_offset)
   {
-    assert(conductivity > 0.0);
-    assert(density > 0.0);
-    assert(specific_heat_capacity > 0.0);
+    SLIC_ERROR_ROOT_IF(conductivity_offset_ < 0.0,
+                       "Conductivity must be positive in the linear isotropic conductor material model.");
+
+    SLIC_ERROR_ROOT_IF(density_ < 0.0, "Density must be positive in the linear isotropic conductor material model.");
+
+    SLIC_ERROR_ROOT_IF(specific_heat_capacity_ < 0.0,
+                       "Specific heat capacity must be positive in the linear isotropic conductor material model.");
   }
 
+  /**
+   * @brief Thermal material response operator
+   * 
+   * @tparam T1 Spatial position type
+   * @tparam T2 Temperature type
+   * @tparam T3 Temperature gradient type
+   * @tparam T4 Parameter type
+   * @param temperature_gradient The spatial gradient of the temperature (d temperature dx)
+   * @param parameter The user-defined parameter used to compute the conductivity (total conductivity = conductivity offset + parameter)
+   * @return The density, specific heat capacity, and heat flux of the material.
+   */
   template <typename T1, typename T2, typename T3, typename T4>
-  SERAC_HOST_DEVICE auto operator()(const T1&, const T2&, const T3& temperature_gradient, const T4& parameter) const
+  SERAC_HOST_DEVICE auto operator()(const T1& /* x */, const T2& /* temperature */, const T3& temperature_gradient, const T4& parameter) const
   {
     return MaterialResponse{.density                = density_,
                             .specific_heat_capacity = specific_heat_capacity_,
-                            .heat_flux              = -1.0 * (conductivity_ + parameter) * temperature_gradient};
+                            .heat_flux              = -1.0 * (conductivity_offset_ + parameter) * temperature_gradient};
   }
 
+  /**
+   * @brief The number of parameters in the model
+   * 
+   * @return The number of parameters in the model
+   */
   static constexpr int numParameters() { return 1; }
 
 private:
+  /// Density
   double density_;
+
+  /// Specific heat capacity
   double specific_heat_capacity_;
-  double conductivity_;
+
+  /// Conductivity offset
+  double conductivity_offset_;
 };
 
 /// Parameterized thermal source model
 struct ParameterizedSource {
-  /// The constant source
-  double source_ = 0.0;
+  /// The constant source offset
+  double source_offset_ = 0.0;
 
   /**
    * @brief Evaluation function for the constant thermal source model
@@ -55,23 +90,29 @@ struct ParameterizedSource {
    * @tparam T1 type of the position vector
    * @tparam T2 type of the temperature
    * @tparam T3 type of the temperature gradient
-   * @tparam dim The dimension of the problem
+   * @tparam T4 type of the parameter
+   * @param parameter user-defined parameter
    * @return The thermal source value
    */
   template <typename T1, typename T2, typename T3, typename T4>
   SERAC_HOST_DEVICE auto operator()(const T1& /* x */, const double /* t */, const T2& /* u */, const T3& /* du_dx */,
                                     const T4& parameter) const
   {
-    return source_ + parameter;
+    return source_offset_ + parameter;
   }
 
+  /**
+   * @brief The number of parameters in the model
+   * 
+   * @return The number of parameters in the model
+   */
   static constexpr int numParameters() { return 1; }
 };
 
 /// Constant thermal flux boundary model
 struct ParameterizedFlux {
   /// The constant flux applied to the boundary
-  double flux_ = 0.0;
+  double flux_offset_ = 0.0;
 
   /**
    * @brief Evaluation function for the thermal flux on a boundary
@@ -79,15 +120,21 @@ struct ParameterizedFlux {
    * @tparam T1 Type of the position vector
    * @tparam T2 Type of the normal vector
    * @tparam T3 Type of the temperature on the boundary
-   * @tparam dim The dimension of the problem
+   * @tparam T4 Type of the parameter
+   * @param parameter The user-defined parameter value
    * @return The flux applied to the boundary
    */
   template <typename T1, typename T2, typename T3, typename T4>
   SERAC_HOST_DEVICE auto operator()(const T1& /* x */, const T2& /* n */, const T3& /* u */, const T4& parameter) const
   {
-    return flux_ + parameter;
+    return flux_offset_ + parameter;
   }
 
+  /**
+   * @brief The number of parameters in the model
+   * 
+   * @return The number of parameters in the model
+   */
   static constexpr int numParameters() { return 1; }
 };
 
