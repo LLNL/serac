a6fb8516a2e63e39042f8139118e81e946b604eb
Alex Tyler Chapman <chapman39@llnl.gov>
2022-04-01 16:15:13 -0700

    cleaned

--- scripts/llnl/get_specs_test.py
+++ scripts/llnl/get_specs_test.py
@@ -8,7 +8,7 @@ def get_specs_for(machine_name, using_yaml):
             try:
                 specs = yaml.safe_load(stream)[machine_name]
                 for i, val in enumerate(specs):
-                    specs[i] = "%" + "".join(val)
+                    specs[i] = "%" + " ".join(val)
             except yaml.YAMLError as exc:
                 print(exc)
     else:
@@ -19,7 +19,24 @@ def get_specs_for(machine_name, using_yaml):
     return specs
 
 if __name__ == "__main__":
-    machine_name = "toss_4_x86_64_ib"
+    machine_names = [
+        "toss_3_x86_64_ib",
+        "toss_4_x86_64_ib",
+        "toss_4_x86_64_ib_cray",
+        "blueos_3_ppc64le_ib_p9",
+        "darwin-x86_64"
+    ]
 
-    for spec in get_specs_for(machine_name, True):
-        print(spec)
+    for machine in machine_names:
+        print(machine + ":")
+        for spec in get_specs_for(machine, True):
+            print(spec)
+        print()
+
+    print("========================")
+
+    for machine in machine_names:
+        print(machine + ":")
+        for spec in get_specs_for(machine, False):
+            print(spec)
+        print()
