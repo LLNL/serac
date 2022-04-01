0dc9f3b13e005ad4bc11d85753d5cf0fc5936348
Sam Mish <mish2@llnl.gov>
2022-04-01 15:29:26 -0700

    first working impl of sum factorization E->Q for hcurl

--- src/serac/numerics/functional/tests/performance/batched_hcurl.cpp
+++ src/serac/numerics/functional/tests/performance/batched_hcurl.cpp
@@ -9,7 +9,21 @@
 using namespace serac;
 
 template < int q, int p >
-auto batched_hcurl_interpolation(tensor< double, 3, p, p + 1, p + 1 > element_values) {
+auto batched_hcurl_interpolation(tensor< double, 3, p + 1, p + 1, p > element_values) {
+
+  // clang-format off
+  union {
+    tensor< double, 3, p + 1, p + 1, p > copy;
+    struct {
+      tensor< double, p + 1, p + 1, p     > element_values_x;
+      tensor< double, p + 1, p    , p + 1 > element_values_y;
+      tensor< double, p    , p + 1, p + 1 > element_values_z;
+    };
+  } data;
+  // clang-format on
+
+  data.copy = element_values;
+
   tensor< double, 3, q, q, q > value_q{};
   tensor< double, 3, q, q, q > curl_q{};
 
@@ -25,19 +39,23 @@ auto batched_hcurl_interpolation(tensor< double, 3, p, p + 1, p + 1 > element_va
   }
 
   tensor< double, p + 1, p + 1, q > A1; 
+  tensor< double, 2, p + 1, q, q > A2; 
+
+  /////////////////////////////////
+  ////////// Y-component //////////
+  /////////////////////////////////
   for (int k = 0; k < p + 1; k++) {
     for (int j = 0; j < p + 1; j++) {
       for (int qx = 0; qx < q; qx++) {
         double sum = 0.0;
         for (int i = 0; i < p; i++) {
-          sum += B1(qx, i) * element_values(0, i, j, k);
+          sum += B1(qx, i) * data.element_values_x(k, j, i);
         }
         A1(k, j, qx) = sum;
       }
     }
   }
 
-  tensor< double, 2, p + 1, q, q > A2; 
   for (int k = 0; k < p + 1; k++) {
     for (int qy = 0; qy < q; qy++) {
       for (int qx = 0; qx < q; qx++) {
@@ -59,25 +77,104 @@ auto batched_hcurl_interpolation(tensor< double, 3, p, p + 1, p + 1 > element_va
         for (int k = 0; k < (p + 1); k++) {
           sum[0] += B2(qz, k) * A2(0, k, qy, qx);
           sum[1] += G2(qz, k) * A2(0, k, qy, qx);
-          sum[1] += B2(qz, k) * A2(1, k, qy, qx);
+          sum[2] += B2(qz, k) * A2(1, k, qy, qx);
         }
         value_q(0, qz, qy, qx) += sum[0];
-        curl_q(1, qy, qy, qx)  += sum[1];
-        curl_q(2, qy, qy, qx)  -= sum[2];
+        curl_q(1, qz, qy, qx)  += sum[1];
+        curl_q(2, qz, qy, qx)  -= sum[2];
       }
     }
   }
 
-#if 0
+  /////////////////////////////////
+  ////////// Y-component //////////
+  /////////////////////////////////
   for (int k = 0; k < p + 1; k++) {
-    for (int j = 0; j < p + 1; j++) {
-      for (int i = 0; i < p; i++) {
-        N[count++] = {f[0][i] * g[1][j] * g[2][k], 0.0, 0.0};
-        curl[count++] = {0.0, f[0][i] * g[1][j] * dg[2][k], -f[0][i] * dg[1][j] * g[2][k]};
+    for (int i = 0; i < p + 1; i++) {
+      for (int qy = 0; qy < q; qy++) {
+        double sum = 0.0;
+        for (int j = 0; j < p; j++) {
+          sum += B1(qy, j) * data.element_values_y(k, j, i);
+        }
+        A1(k, i, qy) = sum;
       }
     }
   }
-#endif
+
+  for (int k = 0; k < p + 1; k++) {
+    for (int qy = 0; qy < q; qy++) {
+      for (int qx = 0; qx < q; qx++) {
+        double sum[2]{};
+        for (int i = 0; i < (p + 1); i++) {
+          sum[0] += B2(qx, i) * A1(k, i, qy);
+          sum[1] += G2(qx, i) * A1(k, i, qy);
+        }
+        A2(0, k, qy, qx) = sum[0];
+        A2(1, k, qy, qx) = sum[1];
+      }
+    }
+  }
+
+  for (int qz = 0; qz < q; qz++) {
+    for (int qy = 0; qy < q; qy++) {
+      for (int qx = 0; qx < q; qx++) {
+        double sum[3]{};
+        for (int k = 0; k < (p + 1); k++) {
+          sum[0] += B2(qz, k) * A2(0, k, qy, qx);
+          sum[1] += G2(qz, k) * A2(0, k, qy, qx);
+          sum[2] += B2(qz, k) * A2(1, k, qy, qx);
+        }
+        value_q(1, qz, qy, qx) += sum[0];
+        curl_q(2, qz, qy, qx)  += sum[2];
+        curl_q(0, qz, qy, qx)  -= sum[1];
+      }
+    }
+  }
+
+  /////////////////////////////////
+  ////////// Z-component //////////
+  /////////////////////////////////
+  for (int j = 0; j < p + 1; j++) {
+    for (int i = 0; i < p + 1; i++) {
+      for (int qz = 0; qz < q; qz++) {
+        double sum = 0.0;
+        for (int k = 0; k < p; k++) {
+          sum += B1(qz, k) * data.element_values_z(k, j, i);
+        }
+        A1(j, i, qz) = sum;
+      }
+    }
+  }
+
+  for (int j = 0; j < p + 1; j++) {
+    for (int qz = 0; qz < q; qz++) {
+      for (int qx = 0; qx < q; qx++) {
+        double sum[2]{};
+        for (int i = 0; i < (p + 1); i++) {
+          sum[0] += B2(qx, i) * A1(j, i, qz);
+          sum[1] += G2(qx, i) * A1(j, i, qz);
+        }
+        A2(0, j, qz, qx) = sum[0];
+        A2(1, j, qz, qx) = sum[1];
+      }
+    }
+  }
+
+  for (int qz = 0; qz < q; qz++) {
+    for (int qy = 0; qy < q; qy++) {
+      for (int qx = 0; qx < q; qx++) {
+        double sum[3]{};
+        for (int j = 0; j < (p + 1); j++) {
+          sum[0] += B2(qy, j) * A2(0, j, qz, qx);
+          sum[1] += G2(qy, j) * A2(0, j, qz, qx);
+          sum[2] += B2(qy, j) * A2(1, j, qz, qx);
+        }
+        value_q(2, qz, qy, qx) += sum[0];
+        curl_q(0, qz, qy, qx)  += sum[1];
+        curl_q(1, qz, qy, qx)  -= sum[2];
+      }
+    }
+  }
 
   return tuple{value_q, curl_q};
 }
@@ -91,12 +188,12 @@ int main() {
   using element_type = finite_element< Geometry::Hexahedron, Hcurl<p> >;
 
   union {
-    tensor< double, 3, p, n, n > element_values;
-    tensor< double, 3 * p * n * n > element_values_1D;
+    tensor< double, 3, n, n, p > element_values;
+    tensor< double, 3 * n * n * p > element_values_1D;
   };
 
-  element_values = make_tensor< 3, p, n, n >([](int c, int i, int j, int k) {
-    return sin(c + i - j + 3 * k);
+  element_values = make_tensor< 3, n, n, p >([](int c, int i, int j, int k) {
+    return (c == 1) * sin(c + i - j + 3 * k);
   });
 
   // values of the interpolated field and its curl, at each quadrature point
@@ -105,16 +202,16 @@ int main() {
   
   auto x1D = GaussLegendreNodes<q>();
   
-  for (int i = 0; i < q; i++) {
+  for (int k = 0; k < q; k++) {
     for (int j = 0; j < q; j++) {
-      for (int k = 0; k < q; k++) {
+      for (int i = 0; i < q; i++) {
         tensor xi = {{x1D[i], x1D[j], x1D[k]}};
         auto value = dot(element_values_1D, element_type::shape_functions(xi));
         auto curl = dot(element_values_1D, element_type::shape_function_curl(xi));
 
         for (int c = 0; c < 3; c++) {
-          value_q(c, i, j, k) = value[c];
-          curl_q(c, i, j, k) = curl[c];
+          value_q(c, k, j, i) = value[c];
+          curl_q(c, k, j, i) = curl[c];
         }
       }
     }
@@ -122,7 +219,16 @@ int main() {
 
   auto [batched_value_q, batched_curl_q] = batched_hcurl_interpolation<q>(element_values);
 
-  std::cout << value_q << std::endl;
-  std::cout << batched_value_q << std::endl;
-  //std::cout << norm(curl_q - batched_curl_q) << std::endl;
+  //std::cout << value_q[0] << std::endl;
+  //std::cout << batched_value_q[0] << std::endl;
+  std::cout << "errors in value: " << std::endl;
+  std::cout << norm(value_q[0] - batched_value_q[0]) << std::endl;
+  std::cout << norm(value_q[1] - batched_value_q[1]) << std::endl;
+  std::cout << norm(value_q[2] - batched_value_q[2]) << std::endl;
+
+  std::cout << "errors in curl: " << std::endl;
+  std::cout << norm(curl_q[0] - batched_curl_q[0]) << std::endl;
+  std::cout << norm(curl_q[1] - batched_curl_q[1]) << std::endl;
+  std::cout << norm(curl_q[2] - batched_curl_q[2]) << std::endl;
+
 }
