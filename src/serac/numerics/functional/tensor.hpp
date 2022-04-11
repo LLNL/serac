71133d92f928bf8fb899aea190d4b2ae019ff2c1
Sam Mish <mish2@llnl.gov>
2022-04-11 16:28:06 -0700

    found some useful old code worth adding to the tensor class

--- src/serac/numerics/functional/tensor.hpp
+++ src/serac/numerics/functional/tensor.hpp
@@ -1696,4 +1696,244 @@ SERAC_HOST_DEVICE auto chain_rule(const tensor<double, m, n, p...>& df_dx, const
 
 }  // namespace serac
 
+// todo: port to current tensor class:
+#if 0
+// eigendecomposition for symmetric A
+//
+// based on "A robust algorithm for finding the eigenvalues and
+// eigenvectors of 3x3 symmetric matrices", by Scherzinger & Dohrmann
+__host__ __device__
+inline void eig(const r2tensor < 3, 3 > & A,
+                      r1tensor < 3 >    & eta,
+                      r2tensor < 3, 3 > & Q) {
+
+  for (int i = 0; i < 3; i++) {
+    eta(i) = 1.0;
+    for (int j = 0; j < 3; j++) {
+      Q(i,j) = (i == j);
+    }
+  }
+
+  auto A_dev = dev(A);
+
+  double J2 = I2(A_dev);
+  double J3 = I3(A_dev);
+
+  if (J2 > 0.0) {
+
+    // angle used to find eigenvalues
+    double tmp = (0.5 * J3) * pow(3.0 / J2, 1.5);
+    double alpha = acos(fmin(fmax(tmp, -1.0), 1.0)) / 3.0;
+
+    // consider the most distinct eigenvalue first
+    if (6.0 * alpha < M_PI) {
+      eta(0) = 2 * sqrt(J2 / 3.0) * cos(alpha);
+    } else {
+      eta(0) = 2 * sqrt(J2 / 3.0) * cos(alpha + 2.0 * M_PI / 3.0);
+    }
+
+    // find the eigenvector for that eigenvalue
+    r1tensor < 3 > r[3];
+
+    int imax = -1;
+    double norm_max = -1.0;
+
+    for (int i = 0; i < 3; i++) {
+
+      for (int j = 0; j < 3; j++) {
+        r[i](j) = A_dev(j,i) - (i == j) * eta(0);
+      }
+
+      double norm_r = norm(r[i]);
+      if (norm_max < norm_r) {
+        imax = i;
+        norm_max = norm_r;
+      }
+
+    }
+
+    r1tensor < 3 > s0, s1, t1, t2, v0, v1, v2, w;
+
+    s0 = normalize(r[imax]);
+    t1 = r[(imax+1)%3] - dot(r[(imax+1)%3], s0) * s0;
+    t2 = r[(imax+2)%3] - dot(r[(imax+2)%3], s0) * s0;
+    s1 = normalize((norm(t1) > norm(t2)) ? t1 : t2);
+
+    // record the first eigenvector
+    v0 = cross(s0, s1);
+    for (int i = 0; i < 3; i++) {
+      Q(i,0) = v0(i);
+    }
+
+    // get the other two eigenvalues by solving the
+    // remaining quadratic characteristic polynomial
+    auto A_dev_s0 = dot(A_dev, s0);
+    auto A_dev_s1 = dot(A_dev, s1);
+
+    double A11 = dot(s0, A_dev_s0);
+    double A12 = dot(s0, A_dev_s1);
+    double A21 = dot(s1, A_dev_s0);
+    double A22 = dot(s1, A_dev_s1);
+
+    double delta = 0.5 * signum(A11-A22) * sqrt((A11-A22)*(A11-A22) + 4*A12*A21);
+
+    eta(1) = 0.5 * (A11 + A22) - delta;
+    eta(2) = 0.5 * (A11 + A22) + delta;
+
+    // if the remaining eigenvalues are exactly the same
+    // then just use the basis for the orthogonal complement
+    // found earlier
+    if (fabs(delta) <= 1.0e-15) {
+      
+      for (int i = 0; i < 3; i++){
+        Q(i,1) = s0(i);
+        Q(i,2) = s1(i);
+      } 
+      
+    // otherwise compute the remaining eigenvectors
+    } else {
+
+      t1 = A_dev_s0 - eta(1) * s0;
+      t2 = A_dev_s1 - eta(1) * s1;
+
+      w = normalize((norm(t1) > norm(t2)) ? t1 : t2);
+
+      v1 = normalize(cross(w, v0));
+      for (int i = 0; i < 3; i++) Q(i,1) = v1(i);
+
+      // define the last eigenvector as
+      // the direction perpendicular to the
+      // first two directions
+      v2 = normalize(cross(v0, v1));
+      for (int i = 0; i < 3; i++) Q(i,2) = v2(i);
+
+    }
+
+    // eta are actually eigenvalues of A_dev, so
+    // shift them to get eigenvalues of A
+    eta += tr(A) / 3.0;
+
+  }
+
+}
+
+inline float angle_between(const vec < 2 > & a, const vec < 2 > & b) {
+  return acos(clip(dot(normalize(a), normalize(b)), -1.0f, 1.0f));
+}
+
+inline float angle_between(const vec < 3 > & a, const vec < 3 > & b) {
+  return acos(clip(dot(normalize(a), normalize(b)), -1.0f, 1.0f));
+}
+
+// angle between proper orthogonal matrices
+inline float angle_between(const mat < 3, 3 > & U, const mat < 3, 3 > & V) {
+  return acos(0.5f * (tr(dot(U, transpose(V))) - 1.0f));
+}
+
+inline mat < 2, 2 > rotation(const float theta) {
+  return mat< 2, 2 >{
+    {cos(theta), -sin(theta)},
+    { sin(theta), cos(theta) }
+  };
+}
+
+inline mat < 3, 3 > axis_to_rotation(const vec < 3 > & omega) {
+
+  float norm_omega = norm(omega);
+
+  if (fabs(norm_omega) < 0.000001f) {
+
+    return eye< 3 >();
+
+  } else {
+
+    vec3 u = omega / norm_omega;
+
+    float c = cos(norm_omega);
+    float s = sin(norm_omega);
+
+    return mat < 3, 3 >{
+      {
+        u[0]*u[0]*(1.0f - c) + c,
+        u[0]*u[1]*(1.0f - c) - u[2]*s,
+        u[0]*u[2]*(1.0f - c) + u[1]*s
+      },{
+        u[1]*u[0]*(1.0f - c) + u[2]*s,
+        u[1]*u[1]*(1.0f - c) + c,
+        u[1]*u[2]*(1.0f - c) - u[0]*s
+      },{
+        u[2]*u[0]*(1.0f - c) - u[1]*s,
+        u[2]*u[1]*(1.0f - c) + u[0]*s,
+        u[2]*u[2]*(1.0f - c) + c
+      }
+    };
+
+  }
+
+}
+
+// assumes R is a proper-orthogonal matrix
+inline vec < 3 > rotation_to_axis(const mat < 3, 3 > & R) {
+
+  float theta = acos(clip(0.5f * (tr(R) - 1.0f), -1.0f, 1.0f));
+
+  float scale;
+
+  // for small angles, prefer series expansion to division by sin(theta) ~ 0
+  if (fabs(theta) < 0.00001f) {
+    scale = 0.5f + theta * theta / 12.0f;
+  }
+  else {
+    scale = 0.5f * theta / sin(theta);
+  }
+
+  return vec3{ R(2,1) - R(1,2), R(0,2) - R(2,0), R(1,0) - R(0,1) } *scale;
+
+}
+
+inline mat < 3, 3 > look_at(const vec < 3 > & direction, const vec < 3 > & up = vec3{ 0.0f, 0.0f, 1.0f }) {
+  vec3 f = normalize(direction);
+  vec3 u = normalize(cross(f, cross(up, f)));
+  vec3 l = normalize(cross(u, f));
+
+  return mat3{
+    {f[0], l[0], u[0]},
+    {f[1], l[1], u[1]},
+    {f[2], l[2], u[2]}
+  };
+}
+
+inline mat < 2, 2 > look_at(const vec < 2 > & direction) {
+  vec2 f = normalize(direction);
+  vec2 l = cross(f);
+
+  return mat2{
+    {f[0], l[0]},
+    {f[1], l[1]},
+  };
+}
+
+inline mat < 3, 3 > R3_basis(const vec3 & n) {
+  float sign = (n[2] >= 0.0f) ? 1.0f : -1.0f;
+  float a = -1.0f / (sign + n[2]); 
+  float b = n[0] * n[1] * a;
+
+  return mat < 3, 3 >{
+    {
+      1.0f + sign * n[0] * n[0] * a,
+      b,
+      n[0],
+    },{
+      sign * b,
+      sign + n[1] * n[1] * a,
+      n[1]
+    },{
+      -sign * n[0],
+      -n[1],
+      n[2]
+    }
+  };
+}
+#endif
+
 #include "serac/numerics/functional/isotropic_tensor.hpp"
