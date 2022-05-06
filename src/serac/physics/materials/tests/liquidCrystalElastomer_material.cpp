ed9db80b9c925a91f527f52758537b9b3cf01292
Jorge Luis <barreracruz1@llnl.gov>
2022-05-05 18:04:18 -0700

    Added LCE model operations to test

--- src/serac/physics/materials/tests/liquidCrystalElastomer_material.cpp
+++ src/serac/physics/materials/tests/liquidCrystalElastomer_material.cpp
@@ -22,11 +22,6 @@ namespace serac {
 
 static constexpr auto I = Identity<3>();
 
-// An arbitrary rotation tensor for frame indifference tests
-static const tensor<double, 3, 3> Q{{{0.852932108456964, 0.405577416697288, -0.328654495524269},
-                                     {-0.51876824611259, 0.728725283562492, -0.44703351990878},
-                                     {0.058192140263313, 0.551784758906877, 0.831953877717782}}};
-
 /**
  * @brief Compute Green's strain from the displacement gradient
  */
@@ -38,12 +33,21 @@ auto greenStrain(const tensor<T, 3, 3>& grad_u)
 
 /// @brief Green-Saint Venant isotropic thermoelastic model
 struct LCEMaterialProperties {
-  double E;          ///< Young's modulus
-  double nu;         ///< Poisson's ratio
-  double C;          ///< volumetric heat capacity
-  double alpha;      ///< thermal expansion coefficient
-  double theta_ref;  ///< datum temperature
-  double k;          ///< thermal conductivity
+
+double E;          ///< Young's modulus
+double nu;         ///< Poisson's ratio
+double C;          ///< volumetric heat capacity
+double alpha;      ///< thermal expansion coefficient
+double theta_ref;  ///< datum temperature
+double k;          ///< thermal conductivity
+
+  // double G;          ///< shear modulus (=ca*KB*T) [Pa]
+  // double kB;         ///< Boltzmann constant [J/K]
+  // double ca;         ///< number of mechanically active chains [1/m^3]
+  // double N_seg;      ///< rigid (Khun’s) segments in chain
+  // double b;          ///< segment length
+  // double T_ni;       ///< nematic-isotropic transition temperature
+  // double c;          ///< material-specific constant parameter
 
   struct State { /* this material has no internal variables */
   };
@@ -63,19 +67,71 @@ struct LCEMaterialProperties {
    * @param[out] q0 Piola heat flux
    */
   template <typename T1, typename T2, typename T3>
-  auto calculateConstitutiveOutputs(const tensor<T1, 3, 3>& grad_u, T2 theta, const tensor<T3, 3>& grad_theta,
-                                    State& /*state*/, const tensor<double, 3, 3>& grad_u_old, double /*theta_old*/,
-                                    double dt)
+  auto calculateConstitutiveOutputs(
+    const tensor<T1, 3, 3>& grad_u, 
+    T2 theta, 
+    const tensor<T3, 3>& grad_theta, 
+    State& /*state*/, 
+    const tensor<double, 3, 3>& grad_u_old, 
+    double theta_old,
+    double dt)
   {
-    const double K    = E / (3.0 * (1.0 - 2.0 * nu));
-    const double G    = 0.5 * E / (1.0 + nu);
-    auto         F    = grad_u + I;
-    const auto   Eg   = greenStrain(grad_u);
-    const auto   trEg = tr(Eg);
+
+const double K    = E / (3.0 * (1.0 - 2.0 * nu));
+const double G    = 0.5 * E / (1.0 + nu);
+const auto   Eg   = greenStrain(grad_u);
+const auto   trEg = tr(Eg);
+
+    // const double ca    = 3.2183e+24;   // number of mechanically active chains [1/m^3]
+    // const double kB    = 1.380649e-23; // Boltzmann constant [J/K]
+    const double Gshear     = 13.33e3;      // shear modulus (=ca*KB*T) [Pa]
+    const double N_seg = 1.0;          // rigid (Khun’s) segments in chain
+    const double b     = 1.0;          // segment length
+    const double T_ni  = 273+92;       // nematic-isotropic transition temperature
+    const double c     = 10;           // material-specific constant parameter
+    const double q0    = 0.46;         // initial nematic order parameter
+    const double p     = 30;           // hydrostatic pressure
+    tensor<double, 1, 3> normal = {2/std::sqrt(3), -1/std::sqrt(3), 3/std::sqrt(3)};
+
+    // Deformation gradients
+    auto         F     = grad_u + I;
+    auto         F_old = grad_u_old + I;
+    auto         F_hat = F * inv(F_old);
+
+    // Polar decomposition of deformation gradient based on F_hat
+    auto U_hat = transpose(F_hat) * F_hat; // need to do pow(.,0.5) still
+    auto R_hat = F_hat * inv (U_hat);
+
+    // Velocity gradient using Hughes-Winget approx
+    // auto L = 2.0 / dt * (F_hat - I) * inv(F_hat + I);
+
+    // Spin tensor
+    // auto W = 0.5 * (L - transpose(L));
+
+    // Determinant of deformation gradient
+    auto J = det(F_hat);
+
+    // Nematic order scalar
+    double q_old = q0 / (1 + std::exp((theta_old - T_ni)/c));
+    double q     = q0 / (1 + std::exp((theta - T_ni)/c));
+
+    // Nematic order tensor
+    auto Q_old = q_old/2 * (3 * transpose(normal) * normal - I);
+    auto Q     = q/2 * (3 * transpose(normal) * normal - I);
+
+    // Distribution tensor (using 'Strang Splitting' approach)
+    auto mu0_a = I - Q_old ;
+    auto mu0_b = 3 * Q_old * (transpose(normal) * normal);
+    auto mu_0 = N_seg*std::pow(b,2/3) * (mu0_a + mu0_b);
+
+    auto mu_a = F_hat * ( mu_0 + 2*N_seg*std::pow(b,2/3)* (Q - Q_old)) * transpose(F_hat);
+    auto mu_b =  2*N_seg*std::pow(b,2/3) * (Q - R_hat * Q * transpose(R_hat));
+    auto mu = mu_a + mu_b;
 
     // stress
-    const auto S = 2.0 * G * dev(Eg) + K * (trEg - 3.0 * alpha * (theta - theta_ref)) * I;
-    const auto P = F * S;
+    const auto S = I + 0.0 * G * dev(Eg) + K * (trEg - 3.0 * alpha * (theta - theta_ref)) * I;
+    // const auto P = J * mu * W * R_hat * S;
+    const auto P =  S * J * ( (3*Gshear/(N_seg*b*b)) * (mu - mu_0) + p*I ) * inv(transpose(F_hat));
 
     // internal heat source
     // use backward difference to estimate rate of green strain
@@ -84,9 +140,9 @@ struct LCEMaterialProperties {
     const double s0     = -3 * K * alpha * theta * egdot;
 
     // heat flux
-    const auto q0 = -k * grad_theta;
+    const auto q0_ = -k * grad_theta;
 
-    return serac::tuple{P, C, s0, q0};
+    return serac::tuple{P, C, s0, q0_};
   }
 
   /**
@@ -193,101 +249,6 @@ TEST(LiqCrystElastMaterial, FreeEnergyAndStressAgree)
                                                                 displacement_grad_old, temperature_old, dt);
   auto error  = stress - get_gradient(energy_and_stress);
   EXPECT_NEAR(norm(error), 0.0, 1e-12);
-}
-
-TEST(LiqCrystElastMaterial, SatisfiesDissipationInequality)
-{
-  LCEMaterialProperties material{.E = 100.0, .nu = 0.25, .C = 1.0, .alpha = 1.0e-3, .theta_ref = 300.0, .k = 1.0};
-  // clang-format off
-  tensor<double, 3, 3>  displacement_grad{{{0.35490513, 0.60419905, 0.4275843},
-                                           {0.23061597, 0.6735498,  0.43953657},
-                                           {0.25099766, 0.27730572, 0.7678207}}};
-  // clang-format on
-  double                       temperature = 290.0;
-  tensor<double, 3>            temperature_grad{0.87241435, 0.11105156, -0.27708054};
-  double                       temperature_old = temperature;
-  LCEMaterialProperties::State state{};
-  tensor<double, 3, 3>         displacement_grad_old{};
-  double                       dt                 = 1.0;
-  auto                         generalized_fluxes = material.calculateThermalConstitutiveOutputs(
-      displacement_grad, temperature, temperature_grad, state, displacement_grad_old, temperature_old, dt);
-  auto [heat_capacity, source, heat_flux] = generalized_fluxes;
-  auto dissipation                        = -dot(heat_flux, temperature_grad) / temperature;
-  EXPECT_TRUE(dissipation >= 0.0);
-}
-
-TEST(LiqCrystElastMaterial, IsFrameIndifferent)
-{
-  LCEMaterialProperties material{.E = 100.0, .nu = 0.25, .C = 1.0, .alpha = 1.0e-3, .theta_ref = 300.0, .k = 1.0};
-  // clang-format off
-  tensor<double, 3, 3>  displacement_grad{{{0.35490513, 0.60419905, 0.4275843},
-                                           {0.23061597, 0.6735498,  0.43953657},
-                                           {0.25099766, 0.27730572, 0.7678207}}};
-  // clang-format on
-  double                       temperature = 290.0;
-  tensor<double, 3>            temperature_grad{0.87241435, 0.11105156, -0.27708054};
-  double                       temperature_old = 300;
-  LCEMaterialProperties::State state{};
-  tensor<double, 3, 3>         displacement_grad_old{};
-  double                       dt = 1.0;
-
-  auto generalized_fluxes = material.calculateConstitutiveOutputs(displacement_grad, temperature, temperature_grad,
-                                                                  state, displacement_grad_old, temperature_old, dt);
-
-  auto displacement_grad_transformed     = Q * (displacement_grad + I) - I;
-  auto displacement_grad_old_transformed = Q * (displacement_grad_old + I) - I;
-  auto generalized_fluxes_2 =
-      material.calculateConstitutiveOutputs(displacement_grad_transformed, temperature, temperature_grad, state,
-                                            displacement_grad_old_transformed, temperature_old, dt);
-
-  auto [piola_stress, heat_capacity, internal_source, heat_flux]         = generalized_fluxes;
-  auto [piola_stress_2, heat_capacity_2, internal_source_2, heat_flux_2] = generalized_fluxes_2;
-  EXPECT_LT(norm(piola_stress - transpose(Q) * piola_stress_2), 1e-12);
-  EXPECT_NEAR(heat_capacity, heat_capacity_2, 1e-12);
-  EXPECT_NEAR(internal_source, internal_source_2, 1e-12);
-  EXPECT_LT(norm(heat_flux - heat_flux_2), 1e-12);
-}
-
-TEST(LiqCrystElastMaterial, InternalSourceHasCorrectSign)
-{
-  LCEMaterialProperties material{.E = 100.0, .nu = 0.25, .C = 1.0, .alpha = 1.0e-3, .theta_ref = 300.0, .k = 1.0};
-  // clang-format off
-  tensor<double, 3, 3>  displacement_grad{{{0.35490513, 0.60419905, 0.4275843},
-                                           {0.23061597, 0.6735498,  0.43953657},
-                                           {0.25099766, 0.27730572, 0.7678207}}};
-  // clang-format on
-  double                       temperature_old = 290.0;
-  tensor<double, 3>            temperature_grad{0.87241435, 0.11105156, -0.27708054};
-  LCEMaterialProperties::State state{};
-  tensor<double, 3, 3>         displacement_grad_old{};
-  double                       temperature         = temperature_old;
-  double                       dt                  = 1.0;
-  auto [heat_capacity, internal_source, heat_flux] = material.calculateThermalConstitutiveOutputs(
-      displacement_grad, temperature, temperature_grad, state, displacement_grad_old, temperature_old, dt);
-  // should have same sign as sgn(alpha*trEdot), here negative
-  EXPECT_LT(internal_source, 0.0);
-}
-
-TEST(LiqCrystElastMaterial, StressHasCorrectSymmetry)
-{
-  LCEMaterialProperties material{.E = 100.0, .nu = 0.25, .C = 1.0, .alpha = 1.0e-3, .theta_ref = 300.0, .k = 1.0};
-  // clang-format off
-  tensor<double, 3, 3>  displacement_grad{{{0.35490513, 0.60419905, 0.4275843},
-                                           {0.23061597, 0.6735498,  0.43953657},
-                                           {0.25099766, 0.27730572, 0.7678207}}};
-  // clang-format on
-  double                       temperature_old = 290.0;
-  tensor<double, 3>            temperature_grad{0.87241435, 0.11105156, -0.27708054};
-  LCEMaterialProperties::State state{};
-  tensor<double, 3, 3>         displacement_grad_old{};
-  double                       temperature  = temperature_old;
-  double                       dt           = 1.0;
-  auto                         piola_stress = material.calculateMechanicalConstitutiveOutputs(
-      displacement_grad, temperature, temperature_grad, state, displacement_grad_old, temperature_old, dt);
-  auto   deformation_grad = displacement_grad + I;
-  auto   kirchhoff_stress = piola_stress * transpose(deformation_grad);
-  double tol              = 1e-10;
-  EXPECT_TRUE(is_symmetric(kirchhoff_stress, tol));
 }
 
 }  // namespace serac
