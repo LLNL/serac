869a69344371d84de77c3d662728c97cf5a531cf
Brandon Talamini <talamini1@llnl.gov>
2022-06-02 21:05:58 -0700

    Implement crude iteration to uniaxial stress state
    Not checking any tolerance yet.

--- src/serac/physics/materials/material_driver.hpp
+++ src/serac/physics/materials/material_driver.hpp
@@ -13,39 +13,101 @@
 #pragma once
 
 #include <iostream>
+#include <functional>
 
 #include "serac/physics/materials/solid_functional_material.hpp"
 #include "serac/numerics/functional/tensor.hpp"
+#include "serac/numerics/functional/dual.hpp"
+#include "serac/numerics/functional/tuple.hpp"
 
 namespace serac::solid_util {
+
+// tensor<double, 2> my_newton_solve(const std::function<tensor<dual<double>, 2>(tensor<dual<double>, 2>)>& f,
+//                                   const tensor<double, 2>& x0)
+// {
+//   auto& value_and_jacobian = f(make_dual(x0));
+//   const auto& r0 = get_value(value_and_jacobian);
+//   auto J = get_gradient(value_and_jacobian);
+//   constexpr double tol = 1e-10;
+//   auto r = r0;
+//   double resnorm = norm(r);
+//   auto x = x0;
+//   const double resnorm 0 = resnorm;
+//   const int MAXITERS = 10;
+//   for (int i = 0; i < MAXITERS; i++) {
+//     auto dx = linear_solve(J, r);
+//     x -= dx;
+//     auto& r_and_jac = f(x);
+//     r = get_value(r_and_jac);
+//     J = get_gradient(r_and_jac);
+//     resnorm = norm(r);
+//     if (renorm < tol*resnorm0) break;
+//   }
+//   return x;
+// }
 
 template <typename T>
 class MaterialDriver {
  public:
-
+  
   MaterialDriver(const T& material):
       material_(material)
   {
     // empty
   };
 
-  void run_uniaxial(double maxEngineeringStrain, int nsteps)
+  /**
+   * @brief Drive the material model thorugh a uniaxial tension experiment
+   *
+   * Currently only implemented for isotropic materials.
+   *
+   * @param maxEngineeringStrain Maximum engineering strain to apply
+   * @param nsteps The number of discrete strain steps to apply
+   */
+  std::vector<tuple<double, double>> runUniaxial(double maxEngineeringStrain, unsigned int nsteps)
   {
     const double strain_increment = maxEngineeringStrain/nsteps;
-    const tensor<double, 3> X{};
+    const tensor<double, 3> x{};
     const tensor<double, 3> u{};
     tensor<double, 3, 3> dudx{};
+
+    // for output
+    std::vector<tuple<double, double>> stress_strain_history;
+
+    //constexpr double tol = 1e-10;
     
-    for (int i = 0; i < nsteps; i++) {
+    for (unsigned int i = 0; i < nsteps; i++) {
       dudx[0][0] += strain_increment;
-      dudx[1][1] -= 0.25*strain_increment;
-      dudx[2][2] -= 0.25*strain_increment;
-      auto [density, stress] = material_(X, u, dudx);
-      std::cout << "step " << i << " stress " << stress[0][0] << std::endl;
+      double resnorm = 1e20;
+      tensor<double, 3, 3> stress{};
+      for (int j = 0; j < 3; j++) {
+        auto response = material_(x, u, make_dual(dudx));
+        auto& sigma = response.stress;
+        auto r = makeUnknownVector(get_value(sigma));
+        auto J = makeJacobianMatrix(get_gradient(sigma));
+        auto corr = linear_solve(J, -r);
+        resnorm = norm(r);
+        dudx[1][1] += corr[0];
+        dudx[2][2] += corr[1];
+        stress = get_value(sigma);
+      }
+      //std::cout << "out of plane stress " << stress[1][1] << std::endl;
+      stress_strain_history.push_back(tuple{dudx[0][0], stress[0][0]});
     }
+    return stress_strain_history;
+  }
+  
+ private:
+  tensor<double, 2> makeUnknownVector(const tensor<double, 3, 3>& H)
+  {
+    return {{H[1][1], H[2][2]}};
   }
 
- private:
+  tensor<double, 2, 2> makeJacobianMatrix(const tensor<double, 3, 3, 3, 3>& A)
+  {
+    return {{{A[1][1][1][1], A[1][1][2][2]}, {A[2][2][1][1], A[2][2][2][2]}}};
+  }
+      
   const T& material_;
 };
 
--- src/serac/physics/materials/tests/test_material_driver.cpp
+++ src/serac/physics/materials/tests/test_material_driver.cpp
@@ -11,13 +11,14 @@
  */
 
 #include <gtest/gtest.h>
+#include <fstream>
 
 #include "serac/physics/materials/material_driver.hpp"
 #include "serac/physics/materials/solid_functional_material.hpp"
 
 namespace serac {
 
-TEST(MaterialDriver, testUniaxialTensionCaseAgainstClosedFormSolution)
+TEST(MaterialDriver, testUniaxialTensionOnLinearMaterial)
 {
   double density = 1.0;
   double E = 1.0;
@@ -27,8 +28,36 @@ TEST(MaterialDriver, testUniaxialTensionCaseAgainstClosedFormSolution)
   solid_util::LinearIsotropicSolid<3> material(density, G, K);
   solid_util::MaterialDriver material_driver(material);
   double max_strain = 1.0;
-  int steps = 10;
-  material_driver.run_uniaxial(max_strain, steps);
+  unsigned int steps = 10;
+  auto response_history = material_driver.runUniaxial(max_strain, steps);
+  
+  for (unsigned int i = 0; i < response_history.size(); i++) {
+    double computed_stress = get<1>(response_history[i]);
+    double strain = get<0>(response_history[i]);
+    double expected_stress = E*strain;
+    //std::cout << strain << " " << computed_stress << std::endl;
+    EXPECT_NEAR(computed_stress, expected_stress, 1e-8);
+  }
+}
+
+TEST(MaterialDriver, testUniaxialTensionOnNonLinearMaterial)
+{
+  double density = 1.0;
+  double E = 1.0;
+  double nu = 0.25;
+  double G = 0.5*E/(1.0 + nu);
+  double K = E/3.0/(1.0 - 2.0*nu);
+  solid_util::NeoHookeanSolid<3> material(density, G, K);
+  solid_util::MaterialDriver material_driver(material);
+  double max_strain = 1.0;
+  unsigned int steps = 10;
+  auto response_history = material_driver.runUniaxial(max_strain, steps);
+  
+  for (unsigned int i = 0; i < response_history.size(); i++) {
+    double computed_stress = get<1>(response_history[i]);
+    double strain = get<0>(response_history[i]);
+    //std::cout << strain << " " << computed_stress << std::endl;
+  }
 }
 
 } // namespace serac
