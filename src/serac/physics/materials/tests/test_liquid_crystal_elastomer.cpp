b74d23c67ebeedb479bd9ed157f38457fa951285
Brandon Talamini <talamini1@llnl.gov>
2022-06-20 15:01:46 -0700

    Put test conditions in to replace eyeball norm

--- src/serac/physics/materials/tests/test_liquid_crystal_elastomer.cpp
+++ src/serac/physics/materials/tests/test_liquid_crystal_elastomer.cpp
@@ -19,7 +19,7 @@
 
 namespace serac {
 
-TEST(TestLiquidCrystalMaterial, agreesWithNeoHookeanInHighTemperatureLimit)
+TEST(TestLiquidCrystalMaterial, AgreesWithNeoHookeanInHighTemperatureLimit)
 {
   double density = 1.0;
   double E = 1.0;
@@ -35,28 +35,31 @@ TEST(TestLiquidCrystalMaterial, agreesWithNeoHookeanInHighTemperatureLimit)
   LiquidCrystalElastomer material(density, shear_modulus, bulk_modulus, order_constant,
                                order_parameter, transition_temperature, normal, Nb2);
 
+  // test conditions
   const tensor<double, 3> x{};
   const tensor<double, 3> u{};
-  tensor<double, 3, 3> H{{{0.5625, 0.0, 0.0}, // defines a unimodular F
-                          {0.0, -0.2, 0.0},
-                          {0.0, 0.0, -0.2}}};
-  double theta = 300.0;
+  tensor<double, 3, 3> H{{{-0.474440607694436,  0.109876281988692, -0.752574057841232},
+                          {-0.890004651428391, -1.254064550255045, -0.742440671831607},
+                          {-0.310665550306666,  0.90643674423369 , -1.090724491652343}}};
+  double theta = 300.0; // far above transition temperature
 
+  // liquid crystal elastomer model response
   auto F_old = DenseIdentity<3>();
   double theta_old = 300.0;
   tensor<double, 3, 3> mu_old = LiquidCrystalElastomer::calculateInitialDistributionTensor(normal, order_parameter, Nb2);
   LiquidCrystalElastomer::State state{F_old, mu_old, theta_old};
-
   auto response = material(x, u, H, state, theta);
-  std::cout << response.stress << std::endl;
 
+  // neo-hookean for comparison
   solid_util::NeoHookeanSolid<3> nh_material(density, shear_modulus, bulk_modulus);
   solid_util::NeoHookeanSolid<3>::State nh_state{};
   auto nh_response = nh_material(x, u, H, nh_state);
-  std::cout << nh_response.stress << std::endl;
+
+  auto stress_difference = response.stress - nh_response.stress;
+  EXPECT_LT(norm(stress_difference), 1e-8);
 }
 
-TEST(TestLiquidCrystalMaterial, generatesStressHistory)
+TEST(TestLiquidCrystalMaterial, agreesWithNeoHookeanInHighTemperatureLimitOverEntireUniaxialTest)
 {
   double density = 1.0;
   double E = 1.0;
@@ -64,13 +67,13 @@ TEST(TestLiquidCrystalMaterial, generatesStressHistory)
   double shear_modulus = 0.5*E/(1.0 + nu);
   double bulk_modulus = E / 3.0 / (1.0 - 2.0*nu);
   double order_constant = 1.0;
-  double order_parameter = 0.0;
+  double order_parameter = 1.0e-10;
   double transition_temperature = 1.0;
   tensor<double, 3> normal{{0.0, 1.0, 0.0}};
   double Nb2 = 1.0;
   
   LiquidCrystalElastomer material(density, shear_modulus, bulk_modulus, order_constant, order_parameter, transition_temperature, normal, Nb2);
-  double temperature = 50.0;
+  double temperature = 300.0; // far above transition temperature
 
   auto initial_distribution = LiquidCrystalElastomer::calculateInitialDistributionTensor(normal, order_parameter, Nb2);
   decltype(material)::State initial_state{DenseIdentity<3>(), initial_distribution, temperature};
@@ -81,15 +84,15 @@ TEST(TestLiquidCrystalMaterial, generatesStressHistory)
   std::function<double(double)> constant_temperature = [temperature](double){ return temperature; };
   auto response_history = uniaxial_stress_test(max_time, steps, material, initial_state, constant_strain_rate, constant_temperature);
 
-  for (auto const& [t, strain, stress, state] : response_history) {
-    std::cout<< strain[0][0] << " " << stress << std::endl;
-  }
-
   solid_util::NeoHookeanSolid<3> nh_material(density, shear_modulus, bulk_modulus);
   solid_util::NeoHookeanSolid<3>::State nh_state{};
   auto nh_response_history = uniaxial_stress_test(max_time, steps, nh_material, nh_state, constant_strain_rate);
-  for (auto const& [t, strain, stress, state] : nh_response_history) {
-    std::cout<< strain[0][0] << " " << stress << std::endl;
+
+  for (size_t i = 0; i < steps; i++) {
+    auto [t, strain, stress, state] = response_history[i];
+    auto [nh_t, nh_strain, nh_stress, nh_state] = nh_response_history[i];
+    double difference = std::abs(stress[0][0] - nh_stress[0][0]);
+    EXPECT_LT(difference, 1e-8);
   }
 }
 
