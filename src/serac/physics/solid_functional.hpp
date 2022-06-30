783a96bec69565ca645e685a0c02b23374b6d93c
Jamie A. Bramwell <bramwell1@llnl.gov>
2022-06-30 09:31:27 -0700

    feature: More work toward the slated wall boundary condition

--- src/serac/physics/solid_functional.hpp
+++ src/serac/physics/solid_functional.hpp
@@ -206,13 +206,80 @@ public:
    * @param[in] disp The vector function containing the set displacement values
    * @param[in] component The component to set the displacment on
    */
-  void setDisplacementBCs(const std::set<int>& disp_bdr, std::function<double(const mfem::Vector& x)> disp,
-                          int component)
+  void setSlideWallBCs(const std::set<int>& disp_bdr)
   {
-    // Project the coefficient onto the grid function
-    component_disp_bdr_coef_ = std::make_shared<mfem::FunctionCoefficient>(disp);
+    SLIC_ERROR_ROOT_IF(disp_bdr.size() > dim,
+                       "Number of sliding wall attribute boundaries must be less than the dimension.");
 
-    bcs_.addEssential(disp_bdr, component_disp_bdr_coef_, displacement_, component);
+    auto& space = displacement_.space();
+
+    std::unordered_map<int, mfem::Vector> attribute_normals;
+
+    for (int i = 0; i < space.GetNBE(); ++i) {
+      int att = space.GetBdrAttribute(i);
+
+      auto found = disp_bdr.find(att);
+      if (found != disp_bdr.end()) {
+        // compute the boundary element normal
+        mfem::ElementTransformation* Tr    = space.GetBdrElementTransformation(i);
+        const mfem::FiniteElement*   fe    = space.GetBE(i);
+        const mfem::IntegrationRule& nodes = fe->GetNodes();
+
+        mfem::Array<int> dofs;
+        space.GetBdrElementDofs(i, dofs);
+        SLIC_ERROR_IF(dofs.Size() != nodes.Size(), "Something wrong in finite element space!");
+
+        for (int j = 0; j < dofs.Size(); ++j) {
+          Tr->SetIntPoint(&nodes[j]);
+
+          mfem::Vector normal(dim);
+
+          // the normal returned in the next line is scaled by h. We should normalize it to 1
+          // to check for consistency along the boundary
+          CalcOrtho(Tr->Jacobian(), normal);
+
+          double norm_l2 = normal.Norml2();
+          normal *= norm_l2;
+
+          auto found_attr = attribute_normals.find(att);
+
+          if (found_attr == attribute_normals.end()) {
+            // if the normal is not already found, add it to the list
+            attribute_normals.emplace(att, normal);
+          } else {
+            // otherwise check that it is consistent with previously found normals
+            auto& other_normal = found_attr->second;
+            for (int comp = 0; comp < normal.Size(); ++comp) {
+              SLIC_ERROR_IF(std::abs(std::abs(other_normal(comp)) - std::abs(normal(comp))) > 1.0e-7,
+                            "The normal vectors on an attribute surface are not constant in the sliding wall boundary "
+                            "condition.");
+            }
+          }
+        }
+      }
+    }
+
+    // TODO implement for more than one sliding wall
+    SLIC_ERROR_ROOT_IF(attribute_normals.size() > 1, "Sliding wall not implemented for more than one wall.");
+
+    for (auto& normal : attribute_normals) {
+      if constexpr (dim == 2) {
+        double angle              = std::acos(normal.second(0));
+        coordinate_transform_     = {{{std::cos(angle), -1.0 * std::sin(angle)}, {std::sin(angle), std::cos(angle)}}};
+        inv_coordinate_transform_ = inv(coordinate_transform_);
+      }
+
+      if constexpr (dim == 3) {
+        SLIC_ERROR_ROOT("Sliding wall not implemented for 3D problems.");
+      }
+
+      // Add the appropriate component-based boundary condition
+      // Project the coefficient onto the grid function
+      disp_bdr_coef_ = std::make_shared<mfem::VectorFunctionCoefficient>(
+          dim, [](const mfem::Vector&, mfem::Vector& vec) { vec = 0.0; });
+
+      bcs_.addEssential({normal.first}, disp_bdr_coef_, displacement_, 0);
+    }
   }
 
   /// @brief Solve the Quasi-static Newton system
@@ -234,6 +301,8 @@ public:
       mesh_.NewNodes(*reference_nodes_);
     }
 
+    rotate();
+
     bcs_.setTime(time_);
 
     if (is_quasistatic_) {
@@ -243,6 +312,8 @@ public:
     } else {
       ode2_.Step(displacement_, velocity_, time_, dt);
     }
+
+    undoRotate();
 
     if (geom_nonlin_ == GeometricNonlinearities::On) {
       // Update the mesh with the new deformed nodes
@@ -671,6 +742,82 @@ public:
   }
 
 protected:
+  void rotate()
+  {
+    if (coordinate_transform_) {
+      // Rotate the mesh nodes
+      auto* nodes = mesh_.GetNodes();
+      rotateGridFunction(coordinate_transform_, *nodes);
+
+      // Get the displacement grid function from the state
+      auto& displacement_gf = displacement_.gridFunction();
+
+      // Rotate the displacement
+      rotateGridFunction(coordinate_transform_, displacement_gf);
+
+      // Set the underlying true vector from the rotated grid function
+      displacement_.setFromGridFunction(displacement_gf);
+
+      // Get the velocity grid function
+      auto& velocity_gf = velocity_.gridFunction();
+      rotateGridFunction(coordinate_transform_, velocity_gf);
+      velocity_.setFromGridFunction(velocity_gf);
+
+      auto& adjoint_disp = adjoint_displacement_.gridFunction();
+      rotateGridFunction(coordinate_transform_, adjoint_disp);
+      adjoint_displacement_.setFromGridFunction(adjoint_disp);
+    }
+  }
+
+  void undoRotate()
+  {
+    if (coordinate_transform_) {
+      // Rotate the mesh nodes
+      auto* nodes = mesh_.GetNodes();
+      rotateGridFunction(inv_coordinate_transform_, *nodes);
+
+      // Get the displacement grid function from the state
+      auto& displacement_gf = displacement_.gridFunction();
+
+      // Rotate the displacement
+      rotateGridFunction(inv_coordinate_transform_, displacement_gf);
+
+      // Set the underlying true vector from the rotated grid function
+      displacement_.setFromGridFunction(displacement_gf);
+
+      // Get the velocity grid function
+      auto& velocity_gf = velocity_.gridFunction();
+      rotateGridFunction(inv_coordinate_transform_, velocity_gf);
+      velocity_.setFromGridFunction(velocity_gf);
+
+      auto& adjoint_disp = adjoint_displacement_.gridFunction();
+      rotateGridFunction(inv_coordinate_transform_, adjoint_disp);
+      adjoint_displacement_.setFromGridFunction(adjoint_disp);
+    }
+  }
+
+  void rotateGridFunction(const tensor<double, dim, dim>& rotation, mfem::ParGridFunction& grid_function)
+  {
+    tensor<double, dim> unrotated_vector;
+    tensor<double, dim> rotated_vector;
+
+    // For each degree of freedom, determine the appropriate vector degree of freedom indices
+
+    for (int dof = 0; dof < grid_function.FESpace()->GetNDofs(); ++dof) {
+      for (int component = 0; component < grid_function.FESpace()->GetVDim(); ++component) {
+        int vector_dof_index        = grid_function.FESpace()->DofToVDof(dof, component);
+        unrotated_vector[component] = grid_fuction(vector_dof_index);
+      }
+
+      rotated_vector = dot(rotation, unrotated_vector);
+
+      for (int component = 0; component < grid_function.FESpace()->GetVDim(); ++component) {
+        int vector_dof_index           = grid_function.FESpace()->DofToVDof(dof, component);
+        grid_fuction(vector_dof_index) = rotated_vector[component];
+      }
+    }
+  }
+
   /// The compile-time finite element trial space for displacement and velocity (H1 of order p)
   using trial = H1<order, dim>;
 
@@ -758,6 +905,10 @@ protected:
 
   /// @brief Auxilliary identity rank 2 tensor
   const isotropic_tensor<double, dim, dim> I_ = Identity<dim>();
+
+  std::optional<tensor<double, dim, dim>> coordinate_transform_ = {};
+
+  std::optional<tensor<double, dim, dim>> inv_coordinate_transform_ = {};
 };
 
 }  // namespace serac
---
+++ src/serac/physics/tests/serac_solid_functional_slide_wall.cpp
@@ -0,0 +1,105 @@
+// Copyright (c) 2019-2022, Lawrence Livermore National Security, LLC and
+// other Serac Project Developers. See the top-level LICENSE file for
+// details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#include <fstream>
+
+#include "axom/slic/core/SimpleLogger.hpp"
+#include <gtest/gtest.h>
+#include "mfem.hpp"
+
+#include "serac/serac_config.hpp"
+#include "serac/mesh/mesh_utils.hpp"
+#include "serac/physics/state/state_manager.hpp"
+#include "serac/physics/solid_functional.hpp"
+#include "serac/physics/materials/solid_functional_material.hpp"
+#include "serac/physics/materials/parameterized_solid_functional_material.hpp"
+
+namespace serac {
+
+TEST(SolidFunctional, SlideWall)
+{
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  int serial_refinement   = 0;
+  int parallel_refinement = 0;
+
+  constexpr int p   = 1;
+  constexpr int dim = 2;
+
+  // Create DataStore
+  axom::sidre::DataStore datastore;
+  serac::StateManager::initialize(datastore, "solid_functional_static_solve");
+
+  // Construct the appropriate dimension mesh and give it to the data store
+  std::string filename = SERAC_REPO_DIR "/data/meshes/beam-quad.mesh";
+
+  auto mesh = mesh::refineAndDistribute(buildMeshFromFile(filename), serial_refinement, parallel_refinement);
+  serac::StateManager::setMesh(std::move(mesh));
+
+  // Define a boundary attribute set
+  std::set<int> ess_bdr = {1};
+
+  // define the solver configurations
+  const IterativeSolverOptions default_linear_options = {.rel_tol     = 1.0e-6,
+                                                         .abs_tol     = 1.0e-10,
+                                                         .print_level = 0,
+                                                         .max_iter    = 500,
+                                                         .lin_solver  = LinearSolver::GMRES,
+                                                         .prec        = HypreBoomerAMGPrec{}};
+
+  const NonlinearSolverOptions default_nonlinear_options = {
+      .rel_tol = 1.0e-4, .abs_tol = 1.0e-8, .max_iter = 10, .print_level = 1};
+
+  const typename solid_util::SolverOptions default_static = {default_linear_options, default_nonlinear_options};
+
+  // Construct a functional-based solid mechanics solver
+  SolidFunctional<p, dim> solid_solver(default_static, GeometricNonlinearities::On, FinalMeshOption::Reference,
+                                       "solid_functional");
+
+  solid_util::NeoHookeanSolid<dim> mat(1.0, 1.0, 1.0);
+  solid_solver.setMaterial(mat);
+
+  // Define the function for the initial displacement and boundary condition
+  auto bc = [](const mfem::Vector&, mfem::Vector& bc_vec) -> void { bc_vec = 0.0; };
+
+  // Set the initial displacement and boundary condition
+  solid_solver.setDisplacementBCs(ess_bdr, bc);
+  solid_solver.setDisplacement(bc);
+  solid_solver.setSlideWallBCs({2});
+
+  tensor<double, dim> constant_force;
+
+  constant_force[0] = 0.0;
+  constant_force[1] = 5.0e-4;
+
+  solid_util::ConstantBodyForce<dim> force{constant_force};
+  solid_solver.addBodyForce(force);
+
+  // Finalize the data structures
+  solid_solver.completeSetup();
+
+  // Perform the quasi-static solve
+  double dt = 1.0;
+  solid_solver.advanceTimestep(dt);
+
+  // Output the sidre-based plot files
+  solid_solver.outputState();
+}
+
+}  // namespace serac
+
+int main(int argc, char* argv[])
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  MPI_Init(&argc, &argv);
+
+  axom::slic::SimpleLogger logger;
+
+  int result = RUN_ALL_TESTS();
+  MPI_Finalize();
+
+  return result;
+}
