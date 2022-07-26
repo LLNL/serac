f56415409f87e7ecf0bc3130f2529e3e91afdfd5
Chris White <white238@llnl.gov>
2022-07-26 16:08:52 -0700

    point at a spack that includes the flags in the cached cmake package

--- scripts/spack/packages/serac/package.py
+++ scripts/spack/packages/serac/package.py
@@ -193,6 +193,7 @@ class Serac(CachedCMakePackage, CudaPackage):
             sys_type = env["SYS_TYPE"]
         return sys_type
 
+
     @property
     def cache_name(self):
         hostname = socket.gethostname()
@@ -205,34 +206,6 @@ class Serac(CachedCMakePackage, CudaPackage):
             self.spec.compiler.name,
             self.spec.compiler.version
         )
-
-    def initconfig_compiler_entries(self):
-        spec = self.spec
-        entries = super(Serac, self).initconfig_compiler_entries()
-
-        # NOTE: this is adapted from <spack>/lib/spack/spack/build_systems/cmake.py
-        # and should be moved into <spack>/lib/spack/spack/build_systems/cached_cmake.py
-
-        flags = spec.compiler_flags
-
-        if flags['ldflags']:
-            ld_flags = ' '.join(flags['ldflags'])
-            ld_format_string = 'CMAKE_{0}_LINKER_FLAGS'
-            # CMake has separate linker arguments for types of builds.
-            for ld_type in ['EXE', 'MODULE', 'SHARED', 'STATIC']:
-                ld_string = ld_format_string.format(ld_type)
-                entries.append(cmake_cache_string(ld_string, ld_flags))
-
-        # CMake has libs options separated by language. Apply ours to each.
-        if flags['ldlibs']:
-            libs_flags = ' '.join(flags['ldlibs'])
-            libs_format_string = 'CMAKE_{0}_STANDARD_LIBRARIES'
-            langs = ['C', 'CXX', 'Fortran']
-            for lang in langs:
-                libs_string = libs_format_string.format(lang)
-                entries.append(cmake_cache_string(libs_string, libs_flags))
-
-        return entries
 
 
     def initconfig_hardware_entries(self):
