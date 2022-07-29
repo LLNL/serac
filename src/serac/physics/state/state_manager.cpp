eceda7226d27400227b99b5f4903fe0a15815cf5
Sam Mish <mish2@llnl.gov>
2022-07-29 14:59:42 -0700

    update expected norms on a few tests affected by body force assumption change

--- src/serac/physics/state/state_manager.cpp
+++ src/serac/physics/state/state_manager.cpp
@@ -13,7 +13,6 @@ namespace serac {
 // Initialize StateManager's static members - these will be fully initialized in StateManager::initialize
 std::unordered_map<std::string, axom::sidre::MFEMSidreDataCollection> StateManager::datacolls_;
 bool                                                                  StateManager::is_restart_ = false;
-// std::vector<std::unique_ptr<SyncableData>>                            StateManager::syncable_data_;
 axom::sidre::DataStore* StateManager::ds_                = nullptr;
 std::string             StateManager::output_dir_        = "";
 const std::string       StateManager::default_mesh_name_ = "default";
@@ -129,10 +128,6 @@ void StateManager::save(const double t, const int cycle, const std::string& mesh
   std::string file_path = axom::utilities::filesystem::joinPath(datacoll.GetPrefixPath(), datacoll.GetCollectionName());
   SLIC_INFO_ROOT(axom::fmt::format("Saving data collection at time: {} to path: {}", t, file_path));
 
-  // sam: ?
-  // for (const auto& data : syncable_data_) {
-  //  data->sync();
-  //}
   datacoll.SetTime(t);
   datacoll.SetCycle(cycle);
   datacoll.Save();
--- src/serac/physics/tests/serac_solid_functional.cpp
+++ src/serac/physics/tests/serac_solid_functional.cpp
@@ -372,24 +372,23 @@ void functional_parameterized_solid_test(double expected_disp_norm)
   EXPECT_NEAR(expected_disp_norm, norm(solid_solver.displacement()), 1.0e-6);
 }
 
-//TEST(SolidFunctional, 2DLinearStatic) { functional_solid_test_static<1, 2>(1.511052595); }
-//TEST(SolidFunctional, 2DQuadStatic) { functional_solid_test_static<2, 2>(2.18604855); }
-//TEST(SolidFunctional, 2DQuadParameterizedStatic) { functional_parameterized_solid_test<2, 2>(2.18604855); }
-//
-//TEST(SolidFunctional, 3DLinearStatic) { functional_solid_test_static<1, 3>(1.3708454313665728); }
-//TEST(SolidFunctional, 3DQuadStatic) { functional_solid_test_static<2, 3>(1.949532747); }
+TEST(SolidFunctional, 2DLinearStatic) { functional_solid_test_static<1, 2>(1.5110771219497878); }
+TEST(SolidFunctional, 2DQuadStatic) { functional_solid_test_static<2, 2>(2.1864070695817928); }
+TEST(SolidFunctional, 2DQuadParameterizedStatic) { functional_parameterized_solid_test<2, 2>(2.1864070695817928); }
+
+TEST(SolidFunctional, 3DLinearStatic) { functional_solid_test_static<1, 3>(1.3708698270682129); }
+TEST(SolidFunctional, 3DQuadStatic) { functional_solid_test_static<2, 3>(1.9497254957351946); }
 
 TEST(SolidFunctional, 3DQuadStaticJ2) { functional_solid_test_static_J2(); }
 
-//TEST(SolidFunctional, 2DLinearDynamic) { functional_solid_test_dynamic<1, 2>(1.52116682); }
-//TEST(SolidFunctional, 2DQuadDynamic) { functional_solid_test_dynamic<2, 2>(1.52777214); }
-//
-//TEST(SolidFunctional, 3DLinearDynamic) { functional_solid_test_dynamic<1, 3>(1.520679017); }
-//TEST(SolidFunctional, 3DQuadDynamic) { functional_solid_test_dynamic<2, 3>(1.527009514); }
-//
-//TEST(SolidFunctional, 2DLinearPressure) { functional_solid_test_boundary<1, 2>(0.065326222, TestType::Pressure); }
-//TEST(SolidFunctional, 2DLinearTraction) { functional_solid_test_boundary<1, 2>(0.12659525750241674,
-// TestType::Traction); }
+TEST(SolidFunctional, 2DLinearDynamic) { functional_solid_test_dynamic<1, 2>(1.52116682); }
+TEST(SolidFunctional, 2DQuadDynamic) { functional_solid_test_dynamic<2, 2>(1.52777214); }
+
+TEST(SolidFunctional, 3DLinearDynamic) { functional_solid_test_dynamic<1, 3>(1.520679017); }
+TEST(SolidFunctional, 3DQuadDynamic) { functional_solid_test_dynamic<2, 3>(1.527009514); }
+
+TEST(SolidFunctional, 2DLinearPressure) { functional_solid_test_boundary<1, 2>(0.065326222, TestType::Pressure); }
+TEST(SolidFunctional, 2DLinearTraction) { functional_solid_test_boundary<1, 2>(0.12659525750241674, TestType::Traction); }
 
 }  // namespace serac
 
