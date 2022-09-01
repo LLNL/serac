31ea2b622b6545be4c19bbf09894666371ef97f0
Jorge Luis <barreracruz1@llnl.gov>
2022-09-01 13:15:15 -0700

    Added free swelling test file

---
+++ src/serac/physics/tests/liquid_crystal_elastomer_free_swelling_test.cpp
@@ -0,0 +1,217 @@
+// Copyright (c) 2019-2022, Lawrence Livermore National Security, LLC and
+// other Serac Project Developers. See the top-level LICENSE file for
+// details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#include <fstream>
+
+#include "axom/slic/core/SimpleLogger.hpp"
+#include <gtest/gtest.h>
+#include "mfem.hpp"
+
+#include "serac/serac_config.hpp"
+#include "serac/mesh/mesh_utils.hpp"
+#include "serac/physics/state/state_manager.hpp"
+#include "serac/physics/solid_functional.hpp"
+#include "serac/physics/materials/liquid_crystal_elastomer.hpp"
+
+using namespace serac;
+
+using serac::solid_mechanics::default_static_options;
+
+int main(int argc, char* argv[]) {
+
+  MPI_Init(&argc, &argv);
+
+  int rank = -1;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  axom::slic::SimpleLogger logger;
+
+  constexpr int p = 1;
+  constexpr int dim = 3;
+  
+  int num_steps = 11;
+
+  int serial_refinement   = 0;
+  int parallel_refinement = 0;
+
+  // Create DataStore
+  axom::sidre::DataStore datastore;
+  serac::StateManager::initialize(datastore, "LCE_free_swelling_test");
+
+  // Construct the appropriate dimension mesh and give it to the data store
+  std::string filename = SERAC_REPO_DIR "/data/meshes/LCE_freeSwelling_nonDim_rect.g";
+
+  auto mesh = mesh::refineAndDistribute(buildMeshFromFile(filename), serial_refinement, parallel_refinement);
+  serac::StateManager::setMesh(std::move(mesh));
+
+  double initial_temperature = 270.0; //300.0;
+  double final_temperature =  380.0; // 430.0;
+  FiniteElementState temperature(
+      StateManager::newState(FiniteElementState::Options{.order = p, .name = "temperature"}));
+
+  temperature = initial_temperature;
+
+  auto fec = std::unique_ptr< mfem::FiniteElementCollection >(new mfem::L2_FECollection(p, dim));
+
+  FiniteElementState gamma(
+      StateManager::newState(FiniteElementState::Options{.order = p, .coll = std::move(fec), .name = "gamma"}));
+
+  // orient fibers vary based on provided function:
+  //
+  //      ━━━━━━━━━━━━━━━━━━━━━━━━━━
+  // y   /                         /┃
+  // ^  /                      4  / ┃
+  // | /                       | /  ┃
+  // ┏━━━━━━━━━━━━━━━━━━━━━━━━━┓-- 4┃
+  // ┃ | | | | | | | | | | | | ┃    ┃
+  // ┃ | | | | | | | | | | | | ┃    ┃
+  // ┃ | | | | | | | | | | | | ┃    ┃
+  // ┃ | | | | | | | | | | | | ┃    ┃
+  // ┃ | | | | | | | | | | | | ┃    ┃
+  // ┃ - - - - - - - - - - - - ┃    ┃
+  // ┃ - - - - - - - - - - - - ┃    ┃
+  // ┃ - - - - - - - - - - - - ┃   /
+  // ┃ - - - - - - - - - - - - ┃  /
+  // ┃ - - - - - - - - - - - - ┃ /
+  // ┗━━━━━━━━━━━━━━━━━━━━━━━━━┛--> x
+
+  int lceArrangementTag = 1;
+  auto gamma_func = [lceArrangementTag](const mfem::Vector& x, double) -> double 
+  {
+    if (lceArrangementTag==1)
+    {
+      return M_PI_2;
+    }
+    else if (lceArrangementTag==2)
+    {
+      // Gyroid
+      double a = 4;
+      double LSF = sin(2*M_PI/a*x[0])*cos(2*M_PI/a*x[1]) 
+                + sin(2*M_PI/a*x[1])*cos(2*M_PI/a*x[2]) 
+                + sin(2*M_PI/a*x[2])*cos(2*M_PI/a*x[0]);
+
+      return (LSF > 0.0) ? 0.667*M_PI_2 : 0.333*M_PI_2; 
+    }
+    else if (lceArrangementTag==3)
+    {
+      // Straight rods
+      double rad = 0.5;
+      double LSF_rod_1 = std::pow(x[0]-4.0, 2) + std::pow(x[1]-4.0, 2) - std::pow(rad, 2);
+      double LSF_rod_2 = std::pow(x[2]-4.0, 2) + std::pow(x[1]-4.0, 2) - std::pow(rad, 2);
+      double LSF_rod_3 = std::pow(x[0]-4.0, 2) + std::pow(x[2]-4.0, 2) - std::pow(rad, 2);
+
+      // Inclined rod
+      // double rotAngle =  M_PI_2/2.0; // 0.785398; // 0.6; //
+
+      // double xp = x[0]; //  x[0]*cos(rotAngle) - x[1]*sin(rotAngle);
+      // double yp = x[1]*cos(-rotAngle) - x[2]*sin(-rotAngle); // x[0]*sin(rotAngle) + x[1]*cos(rotAngle);
+      // double zp = x[1]*sin(-rotAngle) + x[2]*cos(-rotAngle); // x[2];
+
+      // double xpp =  xp*cos(rotAngle) - yp*sin(rotAngle); // xp;
+      // // double ypp = xp*sin(rotAngle) + yp*cos(rotAngle); // yp*cos(-rotAngle) - zp*sin(-rotAngle);
+      // double zpp = zp; // yp*sin(-rotAngle) + zp*cos(-rotAngle);
+      
+      // double LSF_rod_4 = std::pow(xpp, 2) + std::pow(zpp, 2) - std::pow(rad, 2);
+      
+      double LSF_rod_4 = std::pow(x[0]-x[1], 2) + std::pow(x[1]-x[2], 2)+ std::pow(x[2]-x[0], 2) - 3*std::pow(rad, 2);
+
+      // Sphere
+      double LSF_sph = std::pow(x[0], 2) + std::pow(x[1], 2) + std::pow(x[2], 2) - std::pow(2.75*rad, 2);
+
+      // Combine LSFs4d
+      double final_LSF = std::min(std::min(std::min(std::min(LSF_rod_1, LSF_rod_2),LSF_rod_3),LSF_rod_4),LSF_sph);
+
+      return (final_LSF > 0.0) ? 1.0*M_PI_2 : 0.0*M_PI_2; 
+    }
+    else
+    {
+      // Spheres (not ready yet)
+      double rad = 0.65;
+      return ( 
+        std::pow(x[0]-3.0, 2) + std::pow(x[1]-3.0, 2) - std::pow(rad, 2) < 0.0 ||  
+        std::pow(x[0]-1.0, 2) + std::pow(x[1]-3.0, 2) - std::pow(rad, 2) < 0.0 ||  
+        std::pow(x[0]-3.0, 2) + std::pow(x[1]-1.0, 2) - std::pow(rad, 2) < 0.0 ||  
+        std::pow(x[0]-1.0, 2) + std::pow(x[1]-1.0, 2) - std::pow(rad, 2) < 0.0
+        )? 0.333*M_PI_2 : 0.667*M_PI_2; 
+    }
+  };
+
+  mfem::FunctionCoefficient coef(gamma_func);
+  gamma.project(coef);
+
+  // Construct a functional-based solid mechanics solver
+  SolidFunctional<p, dim, Parameters< H1<p>, L2<p> > > solid_solver(default_static_options, GeometricNonlinearities::Off, FinalMeshOption::Reference,
+                                       "solid_functional", {temperature, gamma});
+
+  double density = 1.0;
+  double E = 1.0e-2;
+  double nu = 0.48; // 0.49
+  double shear_modulus = 0.5*E/(1.0 + nu);
+  double bulk_modulus = E / 3.0 / (1.0 - 2.0*nu);
+  double order_constant = 10; // 6.0;
+  double order_parameter = 0.7;
+  double transition_temperature = 370.0;
+  double Nb2 = 1.0;
+  
+  LiquidCrystalElastomer mat(density, shear_modulus, bulk_modulus, order_constant, order_parameter, transition_temperature, Nb2);
+
+  LiquidCrystalElastomer::State initial_state{};
+
+  auto qdata = solid_solver.createQuadratureDataBuffer(initial_state);
+
+  solid_solver.setMaterial(mat, qdata);
+
+  // prescribe symmetry conditions
+  auto zeroFunc = [](const mfem::Vector /*x*/){ return 0.0;};
+  solid_solver.setDisplacementBCs({1}, zeroFunc, 1); // bottom face y-dir disp = 0
+  solid_solver.setDisplacementBCs({2}, zeroFunc, 0); // left face x-dir disp = 0
+  solid_solver.setDisplacementBCs({3}, zeroFunc, 2); // back face z-dir disp = 0
+
+  auto zero_displacement = [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.0; };
+  solid_solver.setDisplacement(zero_displacement);
+
+  // Finalize the data structures
+  solid_solver.completeSetup();
+
+  // Perform the quasi-static solve
+  std::string output_filename = "LCE_free_swelling_test_paraview_90d";
+  solid_solver.outputState(output_filename); 
+
+
+  double t = 0.0;
+  double tmax = 1.0;
+  double dt = tmax / num_steps;
+  for (int i = 0; i < num_steps; i++) 
+  {
+    solid_solver.advanceTimestep(dt);
+    solid_solver.outputState(output_filename);
+
+    FiniteElementState displacement = solid_solver.displacement();
+    displacement.Print();
+    if(rank==0)
+    {
+      std::cout 
+      << "\n... Entering time step: "<< i + 1
+      << "\n... At time: "<< t
+      << "\n... And with uniform temperature of: " << initial_temperature * (1.0 - (t / tmax)) + final_temperature * (t / tmax) 
+      <<"\n... Max displacement: " << displacement.Min()
+      << std::endl;
+    }
+
+    t += dt;
+    temperature = initial_temperature * (1.0 - (t / tmax)) + final_temperature * (t / tmax);
+    
+    
+    // if(rank==0)
+    // {
+    //   std::cout<<"... Max displacement = " << displacement.Max()<<std::endl;
+    // }
+    // if(i>0){exit(0);}
+  }
+
+  MPI_Finalize();
+
+}
