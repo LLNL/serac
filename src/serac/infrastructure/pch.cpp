19d1c0beb20bf42a53f1c2ed8aa4115fbd7f6768
Sam Mish <mish2@llnl.gov>
2022-09-02 11:06:33 -0700

    revert some pch-related changes

--- src/serac/infrastructure/pch.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2019-2022, Lawrence Livermore National Security, LLC and
-// other Serac Project Developers. See the top-level LICENSE file for
-// details.
-//
-// SPDX-License-Identifier: (BSD-3-Clause)
-
-// Dummy file so CMake thinks serac_precompiled_headers is a real library
