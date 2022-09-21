b152636366bff083c613e0f8ea1ec5ad735d0f16
Sam Mish <mish2@llnl.gov>
2022-09-21 09:14:14 -0700

    add missing 2D comparisons w/ mfem

--- src/serac/numerics/functional/tests/performance/thermal_comparison.cpp
+++ src/serac/numerics/functional/tests/performance/thermal_comparison.cpp
@@ -64,6 +64,150 @@ struct GaussLegendreRule<Geometry::Hexahedron, Q> {
 }
 
 template <int p, int q>
+void h1_h1_test_2D(size_t num_elements, size_t num_runs)
+{
+  using serac::Geometry;
+  using serac::H1;
+
+  constexpr int n   = p + 1;
+  constexpr int dim = 2;
+
+  const double num_runs_d = static_cast<double>(num_runs);
+
+  double rho = 1.0;
+  double k   = 1.0;
+
+  using test  = H1<p>;
+  using trial = H1<p>;
+
+  // a mass + diffusion qfunction
+  auto qf = [=](auto /*x*/, auto temperature) {
+    auto [u, du_dx] = temperature;
+    auto source     = rho * u;
+    auto flux       = k * du_dx;
+    return serac::tuple{source, flux};
+  };
+
+  std::default_random_engine             generator;
+  std::uniform_real_distribution<double> distribution(-1.0, 1.0);
+
+  mfem::Vector U1D(static_cast<int>(num_elements * n * n));
+  mfem::Vector R1D(static_cast<int>(num_elements * n * n));
+  mfem::Vector J1D(static_cast<int>(num_elements * dim * dim * q * q));
+  mfem::Vector rho_dv_1D(static_cast<int>(num_elements * q * q));
+  mfem::Vector k_invJ_invJT_dv_1D(static_cast<int>(num_elements * dim * dim * q * q));
+
+  auto U               = mfem::Reshape(U1D.ReadWrite(), n, n, num_elements);
+  auto J               = mfem::Reshape(J1D.ReadWrite(), q * q, dim, dim, num_elements);
+  auto rho_dv          = mfem::Reshape(rho_dv_1D.ReadWrite(), q * q, num_elements);
+  auto k_invJ_invJT_dv = mfem::Reshape(k_invJ_invJT_dv_1D.ReadWrite(), q * q, dim, dim, num_elements);
+
+  serac::GaussLegendreRule<Geometry::Quadrilateral, q> rule;
+
+  for (size_t e = 0; e < num_elements; e++) {
+    for (int ix = 0; ix < n; ix++) {
+      for (int iy = 0; iy < n; iy++) {
+        U(iy, ix, e) = 0.1 * distribution(generator);
+      }
+    }
+
+    for (int i = 0; i < q * q; i++) {
+      serac::tensor<double, dim, dim> J_q{};
+
+      for (int r = 0; r < dim; r++) {
+        for (int c = 0; c < dim; c++) {
+          J(i, r, c, e) = J_q[r][c] = (r == c) + 0.1 * distribution(generator);
+        }
+      }
+
+      int qx = i % q;
+      int qy = i / q;
+
+      double qweight    = rule.weight(qx, qy);
+      auto   invJ_invJT = dot(inv(J_q), transpose(inv(J_q)));
+      double dv         = det(J_q) * qweight;
+
+      rho_dv(i, e) = rho * dv;
+      for (int r = 0; r < dim; r++) {
+        for (int c = 0; c < dim; c++) {
+          k_invJ_invJT_dv(i, r, c, e) = k * invJ_invJT[r][c] * dv;
+        }
+      }
+    }
+  }
+
+  {
+    R1D            = 0.0;
+
+    auto X1D = U1D;
+
+    serac::domain_integral::KernelConfig<q, Geometry::Quadrilateral, test, trial> eval_config;
+
+    serac::domain_integral::EvaluationKernel element_residual{eval_config, J1D, X1D, num_elements, qf, serac::NoQData};
+
+    // unused anyway, since there is no material state
+    bool update_state = false; 
+
+    double runtime = time([&]() {
+      for (size_t i = 0; i < num_runs; i++) {
+        element_residual({&U1D}, R1D, update_state);
+        compiler::please_do_not_optimize_away(&R1D);
+      }
+    });
+    std::cout << "average reference kernel time: " << runtime / num_runs_d << std::endl;
+  }
+  auto answer_reference = R1D;
+
+  {
+    R1D                           = 0.0;
+    bool                symmetric = false;
+    mfem::Array<double> b_(n * q);
+    mfem::Array<double> bt_(n * q);
+    mfem::Array<double> g_(n * q);
+    mfem::Array<double> gt_(n * q);
+    auto                B  = mfem::Reshape(b_.ReadWrite(), q, n);
+    auto                Bt = mfem::Reshape(bt_.ReadWrite(), n, q);
+
+    auto G  = mfem::Reshape(g_.ReadWrite(), q, n);
+    auto Gt = mfem::Reshape(gt_.ReadWrite(), n, q);
+
+    for (int i = 0; i < q; i++) {
+      auto value      = serac::GaussLobattoInterpolation<n>(rule.points_1D[i]);
+      auto derivative = serac::GaussLobattoInterpolationDerivative<n>(rule.points_1D[i]);
+
+      for (int j = 0; j < n; j++) {
+        Bt(j, i) = B(i, j) = value[j];
+        Gt(j, i) = G(i, j) = derivative[j];
+      }
+    }
+
+    double mass_runtime = time([&]() {
+                            for (size_t i = 0; i < num_runs; i++) {
+                              mfem::SmemPAMassApply2D<n, q>(static_cast<int>(num_elements), b_, bt_, rho_dv_1D, U1D, R1D);
+                              compiler::please_do_not_optimize_away(&R1D);
+                            }
+                          });
+    std::cout << "average mfem mass kernel time: " << mass_runtime / num_runs_d << std::endl;
+
+    double diffusion_runtime =
+        time([&]() {
+          for (size_t i = 0; i < num_runs; i++) {
+            mfem::SmemPADiffusionApply2D<n, q>(static_cast<int>(num_elements), symmetric = false, b_, g_, k_invJ_invJT_dv_1D, U1D, R1D);
+            compiler::please_do_not_optimize_away(&R1D);
+          }
+        });
+    std::cout << "average mfem diffusion kernel time: " << diffusion_runtime / num_runs_d << std::endl;
+
+    std::cout << "average mfem combined kernel time: " << (mass_runtime + diffusion_runtime) / num_runs_d << std::endl;
+  }
+  auto answer_mfem = R1D;
+  auto error            = answer_reference;
+  error -= answer_mfem;
+  auto relative_error = error.Norml2() / answer_reference.Norml2();
+  std::cout << "error: " << relative_error << std::endl;
+}
+
+template <int p, int q>
 void h1_h1_test_3D(size_t num_elements, size_t num_runs)
 {
   using serac::Geometry;
@@ -158,7 +302,7 @@ void h1_h1_test_3D(size_t num_elements, size_t num_runs)
         compiler::please_do_not_optimize_away(&R1D);
       }
     });
-    std::cout << "average reference kernel time: " << runtime / static_cast< double >(num_runs) << std::endl;
+    std::cout << "average reference kernel time: " << runtime / num_runs_d << std::endl;
   }
   auto answer_reference = R1D;
 
@@ -215,7 +359,11 @@ int main()
   size_t num_runs     = 10;
   size_t num_elements = 10000;
 
-  h1_h1_test_3D<1 /* polynomial order */, 2 /* quadrature points / dim */>(num_elements, num_runs);
-  h1_h1_test_3D<2 /* polynomial order */, 3 /* quadrature points / dim */>(num_elements, num_runs);
-  h1_h1_test_3D<3 /* polynomial order */, 4 /* quadrature points / dim */>(num_elements, num_runs);
+  h1_h1_test_2D<1 /* polynomial order */, 2 /* quadrature points / dim */>(8 * num_elements, num_runs);
+  h1_h1_test_2D<2 /* polynomial order */, 3 /* quadrature points / dim */>(4 * num_elements, num_runs);
+  h1_h1_test_2D<3 /* polynomial order */, 4 /* quadrature points / dim */>(1 * num_elements, num_runs);
+
+  h1_h1_test_3D<1 /* polynomial order */, 2 /* quadrature points / dim */>(8 * num_elements, num_runs);
+  h1_h1_test_3D<2 /* polynomial order */, 3 /* quadrature points / dim */>(4 * num_elements, num_runs);
+  h1_h1_test_3D<3 /* polynomial order */, 4 /* quadrature points / dim */>(1 * num_elements, num_runs);
 }
