85a501cf5286839b35748fb62423184dbcad1ee6
Jorge Luis <barreracruz1@llnl.gov>
2022-11-01 16:03:07 -0700

    Added flag to switch between temperature and load driven loop

--- src/serac/physics/tests/liquid_crystal_elastomer_compression_test.cpp
+++ src/serac/physics/tests/liquid_crystal_elastomer_compression_test.cpp
@@ -15,7 +15,9 @@
 #include "serac/physics/state/state_manager.hpp"
 #include "serac/physics/solid_mechanics.hpp"
 #include "serac/physics/materials/liquid_crystal_elastomer.hpp"
-// #include "serac/physics/materials/liquid_crystal_elastomer_material.hpp"
+
+#define CONST_TEMP
+// #undef CONST_TEMP
 
 using namespace serac;
 
@@ -40,7 +42,11 @@ int main(int argc, char* argv[]) {
 
   // Create DataStore
   axom::sidre::DataStore datastore;
-  serac::StateManager::initialize(datastore, "lce_compression_test");
+#ifdef CONST_TEMP
+  serac::StateManager::initialize(datastore, "lce_compression_test_load");
+#else
+  serac::StateManager::initialize(datastore, "lce_compression_test_temp");
+#endif
 
   // Construct the appropriate dimension mesh and give it to the data store
   // std::string filename = SERAC_REPO_DIR "/data/meshes/beam-hex-flat.mesh";
@@ -57,7 +63,7 @@ int main(int argc, char* argv[]) {
   FiniteElementState temperature(
       StateManager::newState(FiniteElementState::Options{.order = p, .name = "temperature"}));
 
-  temperature = initial_temperature;
+  temperature = initial_temperature + 0.0*final_temperature;
 
   auto fec = std::unique_ptr< mfem::FiniteElementCollection >(new mfem::L2_FECollection(p, dim));
 
@@ -113,8 +119,19 @@ int main(int argc, char* argv[]) {
   mfem::FunctionCoefficient coef(gamma_func);
   gamma.project(coef);
 
+  IterativeSolverOptions default_linear_options = {.rel_tol     = 1.0e-6,
+                                                       .abs_tol     = 1.0e-16,
+                                                       .print_level = 0,
+                                                       .max_iter    = 500,
+                                                       .lin_solver  = LinearSolver::GMRES,
+                                                       .prec        = HypreBoomerAMGPrec{}};
+  NonlinearSolverOptions default_nonlinear_options = {
+    .rel_tol = 1.0e-4, .abs_tol = 1.0e-7, .max_iter = 5, .print_level = 1};
+
   // Construct a functional-based solid mechanics solver
-  SolidMechanics<p, dim, Parameters< H1<p>, L2<p> > > solid_solver(solid_mechanics::default_static_options, GeometricNonlinearities::Off,
+  // SolidMechanics<p, dim, Parameters< H1<p>, L2<p> > > solid_solver(solid_mechanics::default_static_options, GeometricNonlinearities::Off,
+  //                                      "lce_solid_functional");
+  SolidMechanics<p, dim, Parameters< H1<p>, L2<p> > > solid_solver({default_linear_options, default_nonlinear_options}, GeometricNonlinearities::Off,
                                        "lce_solid_functional");
 
   constexpr int TEMPERATURE_INDEX = 0;
@@ -147,26 +164,23 @@ int main(int argc, char* argv[]) {
   solid_solver.setDisplacementBCs({2}, zeroFunc, 1); // left face x-dir disp = 0
   solid_solver.setDisplacementBCs({5}, zeroFunc, 0); // back face z-dir disp = 0
 
-  // solid_solver.setDisplacementBCs({1}, [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.1; });
-  // solid_solver.setDisplacementBCs({2}, [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.2; });
-  // solid_solver.setDisplacementBCs({3}, [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.3; });
-  // solid_solver.setDisplacementBCs({4}, [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.4; });
-  // solid_solver.setDisplacementBCs({5}, [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.5; });
-  // solid_solver.setDisplacementBCs({6}, [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.6; });
+  auto ini_displacement = [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.001; };
+  solid_solver.setDisplacement(ini_displacement);
 
-  auto zero_displacement = [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.0; };
-  solid_solver.setDisplacement(zero_displacement);
-
-  solid_solver.setPiolaTraction([](auto x, auto /*n*/, auto /*t*/){
-    return tensor<double, 3>{0, 0, -5.0e-8 * (x[2]>(2.16/2))};
+  double loadVal = -5.0e-8;
+  solid_solver.setPiolaTraction([loadVal](auto x, auto /*n*/, auto /*t*/){
+    return tensor<double, 3>{0, 0, loadVal * (x[2]>(2.16/2))};
   });
 
   // Finalize the data structures
   solid_solver.completeSetup();
 
   // Perform the quasi-static solve
-  // std::string output_filename = "LCE_compression_test_paraview_rad_0p65_4_30d_60d";
-  std::string output_filename = "sol_lce_compression";
+#ifdef CONST_TEMP
+  std::string output_filename = "sol_lce_compression_load";
+#else
+  std::string output_filename = "sol_lce_compression_temp";
+#endif
   solid_solver.outputState(output_filename); 
 
   double t = 0.0;
@@ -174,20 +188,30 @@ int main(int argc, char* argv[]) {
   double dt = tmax / num_steps;
   for (int i = 0; i < num_steps; i++) 
   {
+
     if(rank==0)
     {
       std::cout 
       << "\n... Entering time step: "<< i + 1
       << "\n... At time: "<< t
+#ifdef CONST_TEMP
+      << "\n... And with a compression load of: " << loadVal
+#else
       << "\n... And with uniform temperature of: " << initial_temperature * (1.0 - (t / tmax)) + final_temperature * (t / tmax) 
+#endif
       << std::endl;
     }
-    
+
     solid_solver.advanceTimestep(dt);
     solid_solver.outputState(output_filename);
 
     t += dt;
+
+#ifdef CONST_TEMP
+    loadVal = loadVal * (1 + 50 * t / tmax);
+#else
     temperature = initial_temperature * (1.0 - (t / tmax)) + final_temperature * (t / tmax);
+#endif
   }
 
   MPI_Finalize();
