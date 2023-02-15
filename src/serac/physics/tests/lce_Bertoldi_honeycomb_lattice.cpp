086b72a86c5b540e0042f71002062c2eb3ba758d
Jorge Luis <barreracruz1@llnl.gov>
2023-02-15 12:33:06 -0800

    Updated re-entrant honeycomb problem setup and added it's mesh

--- src/serac/physics/tests/lce_Bertoldi_honeycomb_lattice.cpp
+++ src/serac/physics/tests/lce_Bertoldi_honeycomb_lattice.cpp
@@ -18,10 +18,10 @@
 
 using namespace serac;
 
-#define PERIODIC_MESH
-// #undef PERIODIC_MESH
+// #define PERIODIC_MESH
+#undef PERIODIC_MESH
 
-const static int problemID = 1;
+const static int problemID = 2;
 
 using serac::solid_mechanics::default_static_options;
 
@@ -45,22 +45,21 @@ int main(int argc, char* argv[])
   serac::StateManager::initialize(datastore, "solid_lce_functional");
 
   // Construct the appropriate dimension mesh and give it to the data store
-  std::string filename = SERAC_REPO_DIR "/data/meshes/rectangularTruss.g";
+  // int nElem = 2;
+  // double lx = 3.0e-3, ly = 3.0e-3, lz = 0.25e-3;
+  // auto initial_mesh = mfem::Mesh(mfem::Mesh::MakeCartesian3D(4*nElem, 4*nElem, nElem, mfem::Element::HEXAHEDRON, lx, ly, lz));
+  std::string filename = SERAC_REPO_DIR "/data/meshes/reEntrantHoneyComb_coarse.g";
   auto initial_mesh = buildMeshFromFile(filename);
 
 #ifdef PERIODIC_MESH
 
   // Create translation vectors defining the periodicity
-  double lx = 0.18;
-  double ly = lx;
-  // double lz = 0.1;
   mfem::Vector x_translation({lx, 0.0, 0.0});
-  mfem::Vector y_translation({0.0, ly, 0.0});
- 
-  // std::vector<mfem::Vector> translations = {x_translation};
-  std::vector<mfem::Vector> translations = {x_translation, y_translation};
+  // mfem::Vector y_translation({0.0, ly, 0.0});
+  // std::vector<mfem::Vector> translations = {x_translation, y_translation};
+  std::vector<mfem::Vector> translations = {x_translation};
+  double tol = 1e-6;
 
-  double tol = 1e-4;
   std::vector<int> periodicMap = initial_mesh.CreatePeriodicVertexMapping(translations, tol);
 
   // Create the periodic mesh using the vertex mapping defined by the translation vectors
@@ -75,17 +74,6 @@ int main(int argc, char* argv[])
 
   serac::StateManager::setMesh(std::move(mesh));
 
-  // orient fibers in the beam like below (horizontal when y < 0.5, vertical when y > 0.5):
-  //
-  // y
-  //
-  // ^                                             8
-  // |                                             |
-  // ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓-- 1
-  // ┃ | | | | | | | | | | | | | | | | | | | | | | ┃
-  // ┃ - - - - - - - - - - - - - - - - - - - - - - ┃
-  // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛--> x
-
   // Construct a functional-based solid mechanics solver
   IterativeSolverOptions default_linear_options = {.rel_tol     = 1.0e-6,
                                                        .abs_tol     = 1.0e-16,
@@ -97,15 +85,13 @@ int main(int argc, char* argv[])
     .rel_tol = 1.0e-6, .abs_tol = 1.0e-10, .max_iter = 6, .print_level = 1};
   SolidMechanics<p, dim, Parameters< H1<p>, L2<p>, L2<p> > > solid_solver({default_linear_options, default_nonlinear_options}, GeometricNonlinearities::Off,
                                        "lce_solid_functional");
-  // SolidMechanics<p, dim, Parameters<H1<p>, L2<p>, L2<p>>> solid_solver(default_static_options, GeometricNonlinearities::Off,
-  //                                                                 "lce_solid_functional");
 
   // Material properties
   double density = 1.0;
-  double young_modulus = 0.4;
-  double possion_ratio = 0.49;
-  double beta_param = 0.041;
-  double max_order_param = 0.1;
+  double young_modulus = 5.0e6; // 0.4;
+  double possion_ratio = 0.48;
+  double beta_param = 0.041; // 4.0e4; // 0.041;
+  double max_order_param = 0.45; // 0.1;
   double gamma_angle = 0.0;
   double eta_angle = 0.0;
 
@@ -120,16 +106,8 @@ int main(int argc, char* argv[])
       eta_angle = 0.0;
       break;
     case 2:
-      gamma_angle = 0.0;
-      eta_angle = M_PI_2;
-      break;
-    case 3:
-      gamma_angle = 0.5 * M_PI_2;
+      gamma_angle = M_PI_2;
       eta_angle = 0.0;
-      break;
-    case 4:
-      gamma_angle = 0.5 * M_PI_2;
-      eta_angle = M_PI_2;
       break;
     default:
       std::cout << "...... Wrong problem ID ......" << std::endl;
@@ -141,13 +119,63 @@ int main(int argc, char* argv[])
   orderParam = max_order_param;
 
   // Parameter 2
-  FiniteElementState gammaParam(StateManager::newState(FiniteElementState::Options{.order = p, .vector_dim = 1, .element_type = ElementType::L2, .name = "gammaParam"}));
-  auto gammaFunc = [gamma_angle](const mfem::Vector& /*x*/, double) -> double { return gamma_angle; }; // (x[1] > 0.5) ? M_PI_2 : 0.5*M_PI_2; };
+  FiniteElementState gammaParam(StateManager::newState(FiniteElementState::Options{.order = p, .element_type = ElementType::L2, .name = "gammaParam"}));
+  bool heterogeneousGammaField = problemID==2? true : false;
+  auto gammaFunc = [heterogeneousGammaField, gamma_angle](const mfem::Vector& x, double) -> double 
+  {
+      if(heterogeneousGammaField)
+      {
+        double Hmax = 15.0;
+        double d = 5.0;
+        double t = 0.5;
+        // double 
+        if(x[1] >= Hmax)
+        {
+          return 0.0;
+        }
+        else if((x[0] >= t/2) && (x[0] <= d-t/2))
+        {
+          if(x[1]<d || x[1]>2*d)
+          {
+            return -0.1;
+          }
+          else
+          {
+            return 0.1;
+          }
+        }
+        else if((x[0] >= d + t/2) && (x[0] <= 2*d - t/2))
+        {
+          if(x[1]<d || x[1]>2*d)
+          {
+            return 0.1920;
+          }
+          else
+          {
+            return -0.1920;
+          }
+        }
+        else if((x[0] >= 2*d + t/2) && (x[0] <= 3*d - t/2))
+        {
+          if(x[1]<d || x[1]>2*d)
+          {
+            return -0.1;
+          }
+          else
+          {
+            return 0.1;
+          }
+        }
+        
+        return M_PI_2; 
+      }
+     return gamma_angle; 
+     };
   mfem::FunctionCoefficient gammaCoef(gammaFunc);
   gammaParam.project(gammaCoef);
 
   // Paremetr 3
-  FiniteElementState etaParam(StateManager::newState(FiniteElementState::Options{.order = p, .vector_dim = 1, .element_type = ElementType::L2, .name = "etaParam"}));
+  FiniteElementState etaParam(StateManager::newState(FiniteElementState::Options{.order = p, .element_type = ElementType::L2, .name = "etaParam"}));
   auto etaFunc = [eta_angle](const mfem::Vector& /*x*/, double) -> double { return eta_angle; };
   mfem::FunctionCoefficient etaCoef(etaFunc);
   etaParam.project(etaCoef);
@@ -170,16 +198,20 @@ int main(int argc, char* argv[])
 
   // Boundary conditions:
   // Prescribe zero displacement at the supported end of the beam
-  std::set<int> support           = {1};
-  auto          zero_displacement = [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.0; };
-  solid_solver.setDisplacementBCs(support, zero_displacement);
+  // std::set<int> support           = {2};
+  // auto          zero_displacement = [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.0; };
+  // solid_solver.setDisplacementBCs(support, zero_displacement);
 
-  double iniDispVal =  0.000005;
+  auto zeroFunc = []( const mfem::Vector /*x*/){ return 0.0;};
+  solid_solver.setDisplacementBCs({1}, zeroFunc, 0); // bottom face y-dir disp = 0
+  solid_solver.setDisplacementBCs({2}, zeroFunc, 1); // left face x-dir disp = 0
+  solid_solver.setDisplacementBCs({3}, zeroFunc, 2); // back face z-dir disp = 0
+
+  double iniDispVal =  5.0e-6;
   if (problemID==4)
   {
-    iniDispVal =  0.00000005;
+    iniDispVal =  5.0e-8;
   }
-
   auto ini_displacement = [iniDispVal](const mfem::Vector&, mfem::Vector& u) -> void { u = iniDispVal; };
   solid_solver.setDisplacement(ini_displacement);
 
@@ -187,25 +219,19 @@ int main(int argc, char* argv[])
   solid_solver.completeSetup();
 
   // Perform the quasi-static solve
-  int num_steps = 30;
+  int num_steps = 20;
 
   std::string outputFilename;
   switch (problemID)
   {
     case 0:
-      outputFilename = "sol_lce_bertoldi_rect_lattice_gamma_00_eta_00";
+      outputFilename = "sol_lce_bertoldi_honeycomb_gamma_00_eta_00";
       break;
     case 1:
-      outputFilename = "sol_lce_bertoldi_rect_lattice_gamma_90_eta_00";
+      outputFilename = "sol_lce_bertoldi_honeycomb_gamma_90_eta_00";
       break;
     case 2:
-      outputFilename = "sol_lce_bertoldi_rect_lattice_gamma_00_eta_90";
-      break;
-    case 3:
-      outputFilename = "sol_lce_bertoldi_rect_lattice_gamma_45_eta_00";
-      break;
-    case 4:
-      outputFilename = "sol_lce_bertoldi_rect_lattice_gamma_45_eta_90";
+      outputFilename = "sol_lce_bertoldi_honeycomb_varying_angle";
       break;
     default:
       std::cout << "...... Wrong problem ID ......" << std::endl;
@@ -216,6 +242,8 @@ int main(int argc, char* argv[])
   double t    = 0.0;
   double tmax = 1.0;
   double dt   = tmax / num_steps;
+double gblDispYmin;
+
   for (int i = 0; i < num_steps; i++) 
   {
     if(rank==0)
@@ -229,10 +257,30 @@ int main(int argc, char* argv[])
       << std::endl;
     }
 
-    t += dt;
     solid_solver.advanceTimestep(dt);
     solid_solver.outputState(outputFilename);
 
+    // FiniteElementState &displacement = solid_solver.displacement();
+    auto &fes = solid_solver.displacement().space();
+    mfem::ParGridFunction displacement_gf = solid_solver.displacement().gridFunction();
+    mfem::Vector dispVecY(fes.GetNDofs()); dispVecY = 0.0;
+
+    for (int k = 0; k < fes.GetNDofs(); k++) 
+    {
+      dispVecY(k) = displacement_gf(3*k+1);
+    }
+
+    double lclDispYmin = dispVecY.Min();
+    MPI_Allreduce(&lclDispYmin, &gblDispYmin, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+    if(rank==0)
+    {
+      std::cout 
+      <<"... Min Y displacement: " << gblDispYmin
+      << std::endl;
+    }
+
+    t += dt;
     orderParam = max_order_param * (tmax - t) / tmax;
   }
 
