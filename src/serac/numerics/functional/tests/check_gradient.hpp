5a924ad3df17cfb71fccef773a4dcf7e6a1c0be7
Sam Mish <mish2@llnl.gov>
2023-04-26 15:13:37 -0700

    add primitive convergence check for finite difference stencils

--- src/serac/numerics/functional/tests/check_gradient.hpp
+++ src/serac/numerics/functional/tests/check_gradient.hpp
@@ -13,45 +13,66 @@
 #include "serac/numerics/functional/functional.hpp"
 
 template <typename T>
-void check_gradient(serac::Functional<T>& f, mfem::Vector& U)
+void check_gradient(serac::Functional<T>& f, mfem::Vector& U, double epsilon=1.0e-4)
 {
   int seed = 42;
 
   mfem::Vector dU(U.Size());
   dU.Randomize(seed);
 
-  double epsilon = 1.0e-8;
-
-  auto U_plus = U;
-  U_plus.Add(epsilon, dU);
-
-  auto U_minus = U;
-  U_minus.Add(-epsilon, dU);
-
-  mfem::Vector df1 = f(U_plus);
-  df1 -= f(U_minus);
-  df1 /= (2 * epsilon);
-
   auto [value, dfdU] = f(serac::differentiate_wrt(U));
-  mfem::Vector df2   = dfdU(dU);
-
-  double relative_error1 = df1.DistanceTo(df2.GetData()) / df1.Norml2();
-  EXPECT_NEAR(0., relative_error1, 5.e-6);
+  mfem::Vector df_jvp1   = dfdU(dU);
 
   std::unique_ptr<mfem::HypreParMatrix> dfdU_matrix = assemble(dfdU);
-  mfem::Vector df3 = (*dfdU_matrix) * dU;
+  mfem::Vector df_jvp2 = (*dfdU_matrix) * dU;
 
-  double relative_error2 = df1.DistanceTo(df3.GetData()) / df1.Norml2();
-  EXPECT_NEAR(0., relative_error2, 5.e-6);
+  double relative_error = df_jvp1.DistanceTo(df_jvp2.GetData()) / df_jvp1.Norml2();
+  EXPECT_NEAR(0., relative_error, 5.e-6);
 
-  std::cout << relative_error1 << " " << relative_error2 << std::endl;
+  mfem::Vector f_values[5];
+  for (int i = 0; i < 5; i++) {
+    auto U_plus_small = U;
+    U_plus_small.Add((i - 2) * epsilon, dU);
+    f_values[i] = f(U_plus_small);
+  }
+
+  // forward-difference approximations
+  mfem::Vector df1_fd[2]; 
+  df1_fd[0] = f_values[4];
+  df1_fd[0] -= f_values[2];
+  df1_fd[0] /= (2.0 * epsilon);
+
+  df1_fd[1] = f_values[3];
+  df1_fd[1] -= f_values[2];
+  df1_fd[1] /= epsilon;
+
+  // center-difference approximations
+  mfem::Vector df1_cd[2]; 
+  df1_cd[0] = f_values[4];
+  df1_cd[0] -= f_values[0];
+  df1_cd[0] /= (4.0 * epsilon);
+
+  df1_cd[1] = f_values[3];
+  df1_cd[1] -= f_values[1];
+  df1_cd[1] /= (2.0 * epsilon);
+
+  // halving epsilon should make the error decrease
+  // by about a factor of two for the forward-difference stencil
+  double e1 = df1_fd[0].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+  double e2 = df1_fd[1].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+  EXPECT_TRUE(fabs(e1 / e2 - 2.0) < 0.1 || fmin(e1, e2) < 1.0e-9);
+
+  // halving epsilon should make the error decrease
+  // by about a factor of four for the center-difference stencil
+  double e3 = df1_cd[0].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+  double e4 = df1_cd[1].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+  EXPECT_TRUE((fabs(e3 / e4 - 4.0) < 0.1) || fmin(e3, e4) < 1.0e-9);
 }
 
 template <typename T>
-void check_gradient(serac::Functional<T>& f, mfem::Vector& U, mfem::Vector& dU_dt)
+void check_gradient(serac::Functional<T>& f, mfem::Vector& U, mfem::Vector& dU_dt, double epsilon=1.0e-4)
 {
   int    seed    = 42;
-  double epsilon = 1.0e-8;
 
   mfem::Vector dU(U.Size());
   dU.Randomize(seed);
@@ -59,53 +80,103 @@ void check_gradient(serac::Functional<T>& f, mfem::Vector& U, mfem::Vector& dU_d
   mfem::Vector ddU_dt(U.Size());
   ddU_dt.Randomize(seed + 1);
 
-  auto U_plus = U;
-  U_plus.Add(epsilon, dU);
-
-  auto U_minus = U;
-  U_minus.Add(-epsilon, dU);
-
   {
-    mfem::Vector df1 = f(U_plus, dU_dt);
-    df1 -= f(U_minus, dU_dt);
-    df1 /= (2 * epsilon);
-
     auto [value, dfdU] = f(serac::differentiate_wrt(U), dU_dt);
-    mfem::Vector df2   = dfdU(dU);
+    mfem::Vector df_jvp1   = dfdU(dU);
 
     std::unique_ptr<mfem::HypreParMatrix> dfdU_matrix = assemble(dfdU);
 
-    mfem::Vector df3 = (*dfdU_matrix) * dU;
+    mfem::Vector df_jvp2 = (*dfdU_matrix) * dU;
+    double relative_error = df_jvp1.DistanceTo(df_jvp2.GetData()) / df_jvp1.Norml2();
+    EXPECT_NEAR(0., relative_error, 5.e-14);
 
-    double relative_error1 = df1.DistanceTo(df2.GetData()) / df1.Norml2();
-    double relative_error2 = df1.DistanceTo(df3.GetData()) / df1.Norml2();
+    mfem::Vector f_values[5];
+    for (int i = 0; i < 5; i++) {
+      auto U_plus_small = U;
+      U_plus_small.Add((i - 2) * epsilon, dU);
+      f_values[i] = f(U_plus_small, dU_dt);
+    }
 
-    EXPECT_NEAR(0., relative_error1, 5.e-6);
-    EXPECT_NEAR(0., relative_error2, 5.e-6);
+    // forward-difference approximations
+    mfem::Vector df1_fd[2]; 
+    df1_fd[0] = f_values[4];
+    df1_fd[0] -= f_values[2];
+    df1_fd[0] /= (2.0 * epsilon);
+
+    df1_fd[1] = f_values[3];
+    df1_fd[1] -= f_values[2];
+    df1_fd[1] /= epsilon;
+
+    // center-difference approximations
+    mfem::Vector df1_cd[2]; 
+    df1_cd[0] = f_values[4];
+    df1_cd[0] -= f_values[0];
+    df1_cd[0] /= (4.0 * epsilon);
+
+    df1_cd[1] = f_values[3];
+    df1_cd[1] -= f_values[1];
+    df1_cd[1] /= (2.0 * epsilon);
+
+    // halving epsilon should make the error decrease
+    // by about a factor of two for the forward-difference stencil
+    double e1 = df1_fd[0].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+    double e2 = df1_fd[1].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+    EXPECT_TRUE(fabs(e1 / e2 - 2.0) < 0.1 || fmin(e1, e2) < 1.0e-9);
+
+    // halving epsilon should make the error decrease
+    // by about a factor of four for the center-difference stencil
+    double e3 = df1_cd[0].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+    double e4 = df1_cd[1].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+    EXPECT_TRUE((fabs(e3 / e4 - 4.0) < 0.1) || fmin(e3, e4) < 1.0e-9);
   }
 
-  auto dU_dt_plus = dU_dt;
-  dU_dt_plus.Add(epsilon, ddU_dt);
-
-  auto dU_dt_minus = dU_dt;
-  dU_dt_minus.Add(-epsilon, ddU_dt);
-
   {
-    mfem::Vector df1 = f(U, dU_dt_plus);
-    df1 -= f(U, dU_dt_minus);
-    df1 /= (2 * epsilon);
-
     auto [value, df_ddU_dt] = f(U, serac::differentiate_wrt(dU_dt));
-    mfem::Vector df2        = df_ddU_dt(ddU_dt);
+    mfem::Vector df_jvp1    = df_ddU_dt(ddU_dt);
 
     std::unique_ptr<mfem::HypreParMatrix> df_ddU_dt_matrix = assemble(df_ddU_dt);
 
-    mfem::Vector df3 = (*df_ddU_dt_matrix) * ddU_dt;
+    mfem::Vector df_jvp2 = (*df_ddU_dt_matrix) * ddU_dt;
+    double relative_error = df_jvp1.DistanceTo(df_jvp2.GetData()) / df_jvp1.Norml2();
+    EXPECT_NEAR(0., relative_error, 5.e-14);
 
-    double relative_error1 = df1.DistanceTo(df2.GetData()) / df1.Norml2();
-    double relative_error2 = df1.DistanceTo(df3.GetData()) / df1.Norml2();
+    mfem::Vector f_values[5];
+    for (int i = 0; i < 5; i++) {
+      auto dU_dt_plus_small = dU_dt;
+      dU_dt_plus_small.Add((i - 2) * epsilon, ddU_dt);
+      f_values[i] = f(U, dU_dt_plus_small);
+    }
 
-    EXPECT_NEAR(0., relative_error1, 5.e-5);
-    EXPECT_NEAR(0., relative_error2, 5.e-5);
+    // forward-difference approximations
+    mfem::Vector df1_fd[2]; 
+    df1_fd[0] = f_values[4];
+    df1_fd[0] -= f_values[2];
+    df1_fd[0] /= (2.0 * epsilon);
+
+    df1_fd[1] = f_values[3];
+    df1_fd[1] -= f_values[2];
+    df1_fd[1] /= epsilon;
+
+    // center-difference approximations
+    mfem::Vector df1_cd[2]; 
+    df1_cd[0] = f_values[4];
+    df1_cd[0] -= f_values[0];
+    df1_cd[0] /= (4.0 * epsilon);
+
+    df1_cd[1] = f_values[3];
+    df1_cd[1] -= f_values[1];
+    df1_cd[1] /= (2.0 * epsilon);
+
+    // halving epsilon should make the error decrease
+    // by about a factor of two for the forward-difference stencil
+    double e1 = df1_fd[0].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+    double e2 = df1_fd[1].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+    EXPECT_TRUE(fabs(e1 / e2 - 2.0) < 0.1 || fmin(e1, e2) < 1.0e-9);
+
+    // halving epsilon should make the error decrease
+    // by about a factor of four for the center-difference stencil
+    double e3 = df1_cd[0].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+    double e4 = df1_cd[1].DistanceTo(df_jvp1.GetData()) / df_jvp1.Norml2();
+    EXPECT_TRUE((fabs(e3 / e4 - 4.0) < 0.1) || fmin(e3, e4) < 1.0e-9);
   }
 }
