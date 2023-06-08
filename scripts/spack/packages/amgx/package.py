1144604b607030568b31b6322ddfdb1ac6e6363c
Alex Tyler Chapman <chapman39@llnl.gov>
2023-06-07 17:48:16 -0700

    inherit amgx

--- scripts/spack/packages/amgx/package.py
+++ scripts/spack/packages/amgx/package.py
@@ -3,10 +3,9 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+from spack.pkg.builtin.amgx import Amgx as BuiltinAmgx
 
-import spack.pkg.builtin.amgx
 
-
-class Amgx(spack.pkg.builtin.amgx.Amgx):
+class Amgx(BuiltinAmgx):
     # white238: CUSPARSE_CSRMV_ALG2 undefined error in 2.3.0
     version("2.3.0.1", commit="d2344958f43c103893c4400fe8ad42d02ac773f5", submodules=True)
