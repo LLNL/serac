6fd22d335070b8f1025b8ec4247ad6520d71236e
Alex Tyler Chapman <chapman39@llnl.gov>
2023-06-23 14:29:23 -0700

    add option variable

--- scripts/spack/packages/umpire/package.py
+++ scripts/spack/packages/umpire/package.py
@@ -13,9 +13,9 @@ class Umpire(BuiltinUmpire):
     depends_on("blt@0.5.1:", type="build", when="@2022.03.0:")
 
     def initconfig_package_entries(self):
-        spec = self.spec
         entries = BuiltinUmpire.initconfig_package_entries(self)
-
+        spec = self.spec
+        option_prefix = "UMPIRE_" if spec.satisfies("@2022.03.0:") else ""
         entries.append(cmake_cache_option(
             "{}ENABLE_DEVICE_ALLOCATOR".format(option_prefix), "+device_alloc" in spec))
 
