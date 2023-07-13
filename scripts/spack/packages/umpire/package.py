1260ec595a93264c2ce6fa7036d3234caeb39671
Alex Tyler Chapman <100869159+chapman39@users.noreply.github.com>
2023-07-13 16:35:57 -0700

    Update scripts/spack/packages/umpire/package.py
    Co-authored-by: Chris White <white238@llnl.gov>

--- scripts/spack/packages/umpire/package.py
+++ scripts/spack/packages/umpire/package.py
@@ -1,5 +1,5 @@
-# Copyright 2023 Lawrence Livermore National Security, LLC and other
-# Spack Prjoect Developers. See top-level COPYRIGHT file for details.
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 or MIT)
 
