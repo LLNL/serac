3d2508ea737f84aa1e71efc839862c72db82e036
Alex Tyler Chapman <chapman39@llnl.gov>
2023-08-10 17:29:26 -0700

    create script to check missing headers

---
+++ scripts/check_missing_headers.py
@@ -0,0 +1,83 @@
+#!/bin/sh
+"exec" "python3" "-u" "-B" "$0" "$@"
+##############################################################################
+# Copyright (c) 2023, Lawrence Livermore National Security, LLC and
+# other Serac Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (BSD-3-Clause)
+##############################################################################
+
+
+import os
+import sys
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i",
+                        "--install-dir",
+                        type=str,
+                        default="",
+                        dest="install_dir",
+                        help="specify path of the install directory.")
+    parser.add_argument("-s",
+                        "--src-dir",
+                        type=str,
+                        default="",
+                        dest="src_dir",
+                        help="specify path of the src directory.")
+    return parser.parse_known_args()
+
+def main():
+    args, unknown_args = parse_arguments()
+
+    # ensure args are valid
+    install_dir = args.install_dir
+    if not os.path.exists(install_dir):
+        print("Error: install directory does not exist {}".format(install_dir))
+        return 1
+    install_dir = os.path.abspath(install_dir)
+
+    src_dir = args.src_dir
+    if not os.path.exists(src_dir):
+        print("Error: src directory does not exist {}".format(src_dir))
+        return 1
+    src_dir = os.path.abspath(src_dir)
+
+    print("============================================================")
+    print("install_dir: {0}".format(install_dir))
+    print("src_dir:     {0}".format(src_dir))
+    print("============================================================")
+
+    # grab headers from install and src
+    install_dir = os.path.join(install_dir, "include", "serac")
+    install_headers = []
+    for (dirpath, dirnames, filenames) in os.walk(install_dir):
+        for f in filenames:
+            if ".hpp" in f and ".in" not in f:
+                install_headers.append({"path": dirpath, "headerfile": f})
+
+    src_dir = os.path.join(src_dir, "serac")
+    src_headers = []
+    for (dirpath, dirnames, filenames) in os.walk(src_dir):
+        for f in filenames:
+            if ".hpp" in f and ".in" not in f:
+                src_headers.append({"path": dirpath, "headerfile": f})
+    
+    # check if each header in src is in install as well
+    success = True
+    for sh in src_headers:
+        found = False
+        for ih in install_headers:
+            if sh["headerfile"] == ih["headerfile"]:
+                found = True
+                break
+        if not found:
+            cmakelists_path = os.path.join(sh["path"], "CMakeLists.txt")
+            print("Header '{0}' missing in {1}".format(sh["headerfile"], cmakelists_path))
+            success = False
+
+    return success
+
+if __name__ == "__main__":
+    sys.exit(main())
