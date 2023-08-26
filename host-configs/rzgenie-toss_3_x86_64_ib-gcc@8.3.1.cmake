bc1de15293dc331c3f4092d7af2e784f82fc6599
EB Chin <chin23@llnl.gov>
2023-08-26 13:53:00 -0700

    update tribol too

--- host-configs/rzgenie-toss_3_x86_64_ib-gcc@8.3.1.cmake
+++ host-configs/rzgenie-toss_3_x86_64_ib-gcc@8.3.1.cmake
@@ -4,7 +4,7 @@
 # CMake executable path: /usr/tce/packages/cmake/cmake-3.23.1/bin/cmake
 #------------------------------------------------------------------------------
 
-set(CMAKE_PREFIX_PATH "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/tribol-0.1.0.5-tmzlp342jhspld3s2yrrbwqqe6wtz7v2;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/caliper-2.9.0-zhw5pxxxxpd3saopixz4bbtgl22qtwgv;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/axom-0.7.0.4-n6caxeqbnyllb2gews7zdiqbvxyhmsvx;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/umpire-2022.10.0-iccpnaemvrlfsvrdjz67nbv5nr3ivy4i;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/raja-2022.10.5-ensrzeslmhalfo4yyeectblnuwcbylzd;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/camp-2022.10.1-eioxipqv2u45sxpt2sewvu7jcyijaq52;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/mfem-4.5.3.2-t427oi3vjgoxg25zqr3fivajptukyxt2;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/superlu-dist-8.1.2-4tws7jbcfgheywky3w272imecvmmrmpd;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/sundials-6.5.1-4i7kcjcc73l2t7lu7x3hgy7cnxlir7ny;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/netcdf-c-4.7.4-fjeuuoj5u5pqt65wg5tu22zxhpbocpjb;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/hypre-2.26.0-efdpbbvfj6yqlplev4oj5krtrd724ajj;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/lua-5.4.4-jn6kavdbewwqf7hqiltjzevvxnwuzk6x;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/ncurses-6.4-nofrz54iun2qczh2wzjlbszqqo3nqtm3;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/conduit-0.8.8-yzxjs5cnu2mhdzwcficc45v4yemk3nox;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/parmetis-4.0.3-6pyrdnnjy27vu2ogb3tmrszc57nrlbfq;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/metis-5.1.0-zkjlxl7esudzngzx5ouhzqkruqxcxul3;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/hdf5-1.8.22-6auyywha2ifl3ujofuctwbcfhtskk7cv;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/blt-0.5.3-sms6gemdlburuzvgsert4kns57ofyo7j;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/adiak-0.2.2-ix5r3of6e6o2buetzoyqftojh7y3nbhq;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1/gmake-4.4.1-2hex2dpiveahunyaxdqatq3mdu65a67p;/usr/workspace/smithdev/devtools/toss_3_x86_64_ib/latest/python-3.10.10;/usr/workspace/smithdev/devtools/toss_3_x86_64_ib/latest/python-3.10.10;/usr/tce/packages/clang/clang-10.0.0;/usr/workspace/smithdev/devtools/toss_3_x86_64_ib/latest/doxygen-1.9.6;/usr/workspace/smithdev/devtools/toss_3_x86_64_ib/latest/cppcheck-2.9;/usr/tce/packages/python/python-3.8.2;/usr/tce/packages/mvapich2/mvapich2-2.3-gcc-8.3.1;/usr/tce/packages/cmake/cmake-3.23.1" CACHE PATH "")
+set(CMAKE_PREFIX_PATH "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/tribol-0.1.0.6-kcb237nlctdbowj6gyl4b2wykbwi3cye;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/caliper-2.9.0-zhw5pxxxxpd3saopixz4bbtgl22qtwgv;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/axom-0.7.0.4-n6caxeqbnyllb2gews7zdiqbvxyhmsvx;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/umpire-2022.10.0-iccpnaemvrlfsvrdjz67nbv5nr3ivy4i;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/raja-2022.10.5-ensrzeslmhalfo4yyeectblnuwcbylzd;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/camp-2022.10.1-eioxipqv2u45sxpt2sewvu7jcyijaq52;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/mfem-4.5.3.2-t427oi3vjgoxg25zqr3fivajptukyxt2;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/superlu-dist-8.1.2-4tws7jbcfgheywky3w272imecvmmrmpd;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/sundials-6.5.1-4i7kcjcc73l2t7lu7x3hgy7cnxlir7ny;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/netcdf-c-4.7.4-fjeuuoj5u5pqt65wg5tu22zxhpbocpjb;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/hypre-2.26.0-efdpbbvfj6yqlplev4oj5krtrd724ajj;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/lua-5.4.4-jn6kavdbewwqf7hqiltjzevvxnwuzk6x;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/ncurses-6.4-nofrz54iun2qczh2wzjlbszqqo3nqtm3;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/conduit-0.8.8-yzxjs5cnu2mhdzwcficc45v4yemk3nox;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/parmetis-4.0.3-6pyrdnnjy27vu2ogb3tmrszc57nrlbfq;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/metis-5.1.0-zkjlxl7esudzngzx5ouhzqkruqxcxul3;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/hdf5-1.8.22-6auyywha2ifl3ujofuctwbcfhtskk7cv;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/blt-0.5.3-sms6gemdlburuzvgsert4kns57ofyo7j;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/adiak-0.2.2-ix5r3of6e6o2buetzoyqftojh7y3nbhq;/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1/gmake-4.4.1-2hex2dpiveahunyaxdqatq3mdu65a67p;/usr/workspace/smithdev/devtools/toss_3_x86_64_ib/latest/python-3.10.10;/usr/workspace/smithdev/devtools/toss_3_x86_64_ib/latest/python-3.10.10;/usr/tce/packages/clang/clang-10.0.0;/usr/workspace/smithdev/devtools/toss_3_x86_64_ib/latest/doxygen-1.9.6;/usr/workspace/smithdev/devtools/toss_3_x86_64_ib/latest/cppcheck-2.9;/usr/tce/packages/python/python-3.8.2;/usr/tce/packages/mvapich2/mvapich2-2.3-gcc-8.3.1;/usr/tce/packages/cmake/cmake-3.23.1" CACHE PATH "")
 
 set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
 
@@ -15,11 +15,11 @@ set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
 #------------------------------------------------------------------------------
 if(DEFINED ENV{SPACK_CC})
 
-  set(CMAKE_C_COMPILER "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/spack/lib/spack/env/gcc/gcc" CACHE PATH "")
+  set(CMAKE_C_COMPILER "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/spack/lib/spack/env/gcc/gcc" CACHE PATH "")
 
-  set(CMAKE_CXX_COMPILER "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/spack/lib/spack/env/gcc/g++" CACHE PATH "")
+  set(CMAKE_CXX_COMPILER "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/spack/lib/spack/env/gcc/g++" CACHE PATH "")
 
-  set(CMAKE_Fortran_COMPILER "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/spack/lib/spack/env/gcc/gfortran" CACHE PATH "")
+  set(CMAKE_Fortran_COMPILER "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/spack/lib/spack/env/gcc/gfortran" CACHE PATH "")
 
 else()
 
@@ -59,7 +59,7 @@ set(ENABLE_OPENMP ON CACHE BOOL "")
 # TPLs
 #------------------------------------------------------------------------------
 
-set(TPL_ROOT "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_25_21_23_48/gcc-8.3.1" CACHE PATH "")
+set(TPL_ROOT "/usr/WS2/smithdev/libs/serac/toss_3_x86_64_ib/2023_08_26_12_16_08/gcc-8.3.1" CACHE PATH "")
 
 set(AXOM_DIR "${TPL_ROOT}/axom-0.7.0.4-n6caxeqbnyllb2gews7zdiqbvxyhmsvx" CACHE PATH "")
 
@@ -97,7 +97,7 @@ set(SUNDIALS_DIR "${TPL_ROOT}/sundials-6.5.1-4i7kcjcc73l2t7lu7x3hgy7cnxlir7ny" C
 
 set(UMPIRE_DIR "${TPL_ROOT}/umpire-2022.10.0-iccpnaemvrlfsvrdjz67nbv5nr3ivy4i" CACHE PATH "")
 
-set(TRIBOL_DIR "${TPL_ROOT}/tribol-0.1.0.5-tmzlp342jhspld3s2yrrbwqqe6wtz7v2" CACHE PATH "")
+set(TRIBOL_DIR "${TPL_ROOT}/tribol-0.1.0.6-kcb237nlctdbowj6gyl4b2wykbwi3cye" CACHE PATH "")
 
 #------------------------------------------------------------------------------
 # Devtools
