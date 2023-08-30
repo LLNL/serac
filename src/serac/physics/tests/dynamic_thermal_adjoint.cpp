7f179cab880ab0e16917e039d76bda7596426353
Michael Tupek <tupek2@llnl.gov>
2023-08-30 10:54:10 -0700

    Continue simplifying and dry.

--- src/serac/physics/tests/dynamic_thermal_adjoint.cpp
+++ src/serac/physics/tests/dynamic_thermal_adjoint.cpp
@@ -32,6 +32,48 @@ struct TimeSteppingInfo
 };
 
 
+std::unique_ptr<HeatTransfer<p,dim>> create_heat_transfer(const NonlinearSolverOptions& nonlinear_opts,
+                                                          const TimesteppingOptions& dyn_opts,
+                                                          const heat_transfer::IsotropicConductorWithLinearConductivityVsTemperature& mat)
+{
+  //eventually figure out how to clear out cider state 
+  //auto saveMesh = std::make_unique<mfem::ParMesh>(serac::StateManager::mesh());
+  //serac::StateManager::reset();
+  //static int iter = 0;
+  //serac::StateManager::initialize(data_store, "thermal_dynamic_solve"+std::to_string(iter++));
+  //std::string filename = std::string(SERAC_REPO_DIR) + "/data/meshes/star.mesh";
+  //mfem::ParMesh* mesh = serac::StateManager::setMesh(std::move(saveMesh));
+  static int iter = 0;
+  auto thermal = std::make_unique<HeatTransfer<p, dim>>(nonlinear_opts, heat_transfer::direct_linear_options, dyn_opts, thermal_prefix + std::to_string(iter++));
+  thermal->setMaterial(mat);
+  thermal->setTemperature([](const mfem::Vector&, double) { return 0.0; });
+  thermal->setTemperatureBCs({1}, [](const mfem::Vector&, double) { return 0.0; });
+  thermal->setSource([](auto /* X */, auto /* time */, auto /* u */, auto /* du_dx */) { return 1.0; });
+  thermal->completeSetup();
+  return thermal;
+}
+
+
+/* QOI */
+
+double computeStepQoi(const serac::FiniteElementState& temperature, double dt)
+{
+  // Compute qoi: \int_t \int_omega 0.5 * (T - T_target(x,t)^2)
+  double nodalTemperatureNormSquared = 0.0;
+  for (int n=0; n < temperature.Size(); ++n) {
+    nodalTemperatureNormSquared += temperature(n) * temperature(n);
+  }
+  return nodalTemperatureNormSquared * dt;
+}
+
+void computeStepAdjointLoad(const serac::FiniteElementState& temperature, serac::FiniteElementDual& d_qoi_d_temperature, double dt)
+{
+  for (int n=0; n < temperature.Size(); ++n) {
+    d_qoi_d_temperature(n) = dt * temperature(n);
+  }
+}
+
+
 double computeThermalQoiAdjustingInitalTemperature(axom::sidre::DataStore& /*data_store*/,
                                                    const NonlinearSolverOptions& nonlinear_opts,
                                                    const TimesteppingOptions& dyn_opts,
@@ -40,155 +82,87 @@ double computeThermalQoiAdjustingInitalTemperature(axom::sidre::DataStore& /*dat
                                                    int dofIndex,
                                                    double pertubation)
 {
-  //auto saveMesh = std::make_unique<mfem::ParMesh>(serac::StateManager::mesh());
-  //serac::StateManager::reset();
-  //static int iter = 0;
-  //serac::StateManager::initialize(data_store, "thermal_dynamic_solve"+std::to_string(iter++));
-  //std::string filename = std::string(SERAC_REPO_DIR) + "/data/meshes/star.mesh";
-  //mfem::ParMesh* mesh = serac::StateManager::setMesh(std::move(saveMesh));
+  auto thermal = create_heat_transfer(nonlinear_opts, dyn_opts, mat);
 
-  static int iter = 0;
-  HeatTransfer<p, dim> thermal(nonlinear_opts, heat_transfer::direct_linear_options, dyn_opts, thermal_prefix + std::to_string(iter++));
-  thermal.setMaterial(mat);
-  thermal.setTemperature([](const mfem::Vector&, double) { return 0.0; });
-  thermal.setTemperatureBCs({1}, [](const mfem::Vector&, double) { return 0.0; });
-  thermal.setSource([](auto /* X */, auto /* time */, auto /* u */, auto /* du_dx */) { return 1.0; });
-  thermal.completeSetup();
-
-  double dt = ts_info.totalTime / ts_info.num_timesteps;
-
-  auto& temperatureSol = thermal.temperature();
   // finite difference change to initial temperature
+  auto& temperatureSol = thermal->temperature();
   temperatureSol(dofIndex) += pertubation;
 
-  // Compute qoi: \int_t \int_omega 0.5 * (T - T_target(x,t)^2)
   double qoi = 0.0;
-  thermal.outputState();
+  thermal->outputState();
   for (int i = 0; i < ts_info.num_timesteps; ++i) {
-    thermal.advanceTimestep(dt);
-    thermal.outputState();
-
-    double nodalTemperatureNormSquared = 0.0;
-    for (int n=0; n < temperatureSol.Size(); ++n) {
-      nodalTemperatureNormSquared += temperatureSol(n) * temperatureSol(n);
-    }
-
-    qoi += nodalTemperatureNormSquared * dt;
+    double dt = ts_info.totalTime / ts_info.num_timesteps;
+    thermal->advanceTimestep(dt);
+    thermal->outputState();
+    qoi += computeStepQoi(thermal->temperature(), dt);
   }
-
   return 0.5 * qoi;
 }
 
 
 double computeThermalQoiAdjustingShape(axom::sidre::DataStore& /*data_store*/,
-                                      const NonlinearSolverOptions& nonlinear_opts,
-                                      const TimesteppingOptions& dyn_opts,
-                                      const heat_transfer::IsotropicConductorWithLinearConductivityVsTemperature& mat,
-                                      const TimeSteppingInfo& ts_info,
-                                      int dofIndex,
-                                      double pertubation)
+                                       const NonlinearSolverOptions& nonlinear_opts,
+                                       const TimesteppingOptions& dyn_opts,
+                                       const heat_transfer::IsotropicConductorWithLinearConductivityVsTemperature& mat,
+                                       const TimeSteppingInfo& ts_info,
+                                       int dofIndex,
+                                       double pertubation)
 {
-  //auto saveMesh = std::make_unique<mfem::ParMesh>(serac::StateManager::mesh());
-  //serac::StateManager::reset();
-  //static int iter = 0;
-  //serac::StateManager::initialize(data_store, "thermal_dynamic_solve"+std::to_string(iter++));
-  //std::string filename = std::string(SERAC_REPO_DIR) + "/data/meshes/star.mesh";
-  //mfem::ParMesh* mesh = serac::StateManager::setMesh(std::move(saveMesh));
+  auto thermal = create_heat_transfer(nonlinear_opts, dyn_opts, mat);
 
-  static int iter = 0;
-  HeatTransfer<p, dim> thermal(nonlinear_opts, heat_transfer::direct_linear_options, dyn_opts, thermal_prefix + std::to_string(iter++));
-  thermal.setMaterial(mat);
-  thermal.setTemperature([](const mfem::Vector&, double) { return 0.0; });
-  thermal.setTemperatureBCs({1}, [](const mfem::Vector&, double) { return 0.0; });
-  thermal.setSource([](auto /* X */, auto /* time */, auto /* u */, auto /* du_dx */) { return 1.0; });
-  thermal.completeSetup();
-
-  double dt = ts_info.totalTime / ts_info.num_timesteps;
-
-  auto& temperatureSol = thermal.temperature();
-  auto& shapeDisp = thermal.shapeDisplacement();
+  // finite difference change to shape displacement
+  auto& shapeDisp = thermal->shapeDisplacement();
   shapeDisp(dofIndex) += pertubation;
 
-  // Compute qoi: \int_t \int_omega 0.5 * (T - T_target(x,t)^2)
   double qoi = 0.0;
-  thermal.outputState();
+  thermal->outputState();
   for (int i = 0; i < ts_info.num_timesteps; ++i) {
-    thermal.advanceTimestep(dt);
-    thermal.outputState();
-
-    double nodalTemperatureNormSquared = 0.0;
-    for (int n=0; n < temperatureSol.Size(); ++n) {
-      nodalTemperatureNormSquared += temperatureSol(n) * temperatureSol(n);
-    }
-
-    qoi += nodalTemperatureNormSquared * dt;
+    double dt = ts_info.totalTime / ts_info.num_timesteps;
+    thermal->advanceTimestep(dt);
+    thermal->outputState();
+    qoi += computeStepQoi(thermal->temperature(), dt);
   }
-
   return 0.5 * qoi;
 }
 
 
-std::pair<double, std::vector<double>> computeThermalQoiAndGradient(axom::sidre::DataStore& /*data_store*/,
-                                                                    const NonlinearSolverOptions& nonlinear_opts,
-                                                                    const TimesteppingOptions& dyn_opts,
-                                                                    const heat_transfer::IsotropicConductorWithLinearConductivityVsTemperature& mat,
-                                                                    const TimeSteppingInfo& ts_info)
+std::pair<double, std::vector<double>> computeThermalQoiAndInitialTemperatureGradient(axom::sidre::DataStore& /*data_store*/,
+                                                                                      const NonlinearSolverOptions& nonlinear_opts,
+                                                                                      const TimesteppingOptions& dyn_opts,
+                                                                                      const heat_transfer::IsotropicConductorWithLinearConductivityVsTemperature& mat,
+                                                                                      const TimeSteppingInfo& ts_info)
 {
-  double dt = ts_info.totalTime / ts_info.num_timesteps;
+  auto thermal = create_heat_transfer(nonlinear_opts, dyn_opts, mat);
 
-  //auto saveMesh = std::make_unique<mfem::ParMesh>(serac::StateManager::mesh());
-  //serac::StateManager::reset();
-  //static int iter = 0;
-  //serac::StateManager::initialize(data_store, "thermal_dynamic_solve"+std::to_string(iter++));
-  //std::string filename = std::string(SERAC_REPO_DIR) + "/data/meshes/star.mesh";
-  //mfem::ParMesh* mesh = serac::StateManager::setMesh(std::move(saveMesh));
-
-  HeatTransfer<p, dim> thermal(nonlinear_opts, heat_transfer::direct_linear_options, dyn_opts, thermal_prefix + std::string("_"));
-  thermal.setMaterial(mat);
-  thermal.setTemperature([](const mfem::Vector&, double) { return 0.0; });
-  thermal.setTemperatureBCs({1}, [](const mfem::Vector&, double) { return 0.0; });
-  thermal.setSource([](auto /* X */, auto /* time */, auto /* u */, auto /* du_dx */) { return 1.0; });
-  thermal.completeSetup();
-
-  auto& temperature_solution = thermal.temperature();
-  int N = temperature_solution.Size();
-  size_t Nsize = static_cast<size_t>(N);
-
-  // Compute qoi: \int_t \int_omega 0.5 * (T - T_target(x,t)^2)
   double qoi = 0.0;
-  thermal.outputState();
+  thermal->outputState();
   for (int i = 0; i < ts_info.num_timesteps; ++i) {
-    thermal.advanceTimestep(dt);
-    thermal.outputState();
-
-    double nodalTemperatureNormSquared = 0.0;
-    for (int n=0; n < N; ++n) {
-      nodalTemperatureNormSquared += temperature_solution(n) * temperature_solution(n);
-    }
-    qoi += nodalTemperatureNormSquared * dt;
+    double dt = ts_info.totalTime / ts_info.num_timesteps;
+    thermal->advanceTimestep(dt);
+    thermal->outputState();
+    qoi += computeStepQoi(thermal->temperature(), dt);
   }
   qoi *= 0.5;
 
+  size_t Nsize = static_cast<size_t>(thermal->temperature().Size());
   std::vector<double> gradient(Nsize, 0.0);
-  serac::FiniteElementDual adjoint_load(thermal.temperature().space(), "adjoint_load");
-  
-  FiniteElementState prev_temperature(temperature_solution); // cannot get with thermal.previousTemperature(adjointCycle) yet, as adjointCycle is not defined until reverseAdjointTimestep is called.
+
+  serac::FiniteElementDual adjoint_load(thermal->temperature().space(), "adjoint_load");
+  FiniteElementState temperature_end_of_step(thermal->temperature()); // cannot get with thermal.previousTemperature(adjointCycle) yet, as adjointCycle is not defined until reverseAdjointTimestep is called.
 
   for (int i = ts_info.num_timesteps; i > 0; --i) {
-    for (int n=0; n < N; ++n) {
-      adjoint_load(n) = prev_temperature(n) * dt;
-    }
-
-    std::unordered_map<std::string, const serac::FiniteElementState&> adjoint_sol = thermal.reverseAdjointTimestep({{"temperature", adjoint_load}});
+    double dt = ts_info.totalTime / ts_info.num_timesteps;
+    computeStepAdjointLoad(temperature_end_of_step, adjoint_load, dt);
+    std::unordered_map<std::string, const serac::FiniteElementState&> adjoint_sol = thermal->reverseAdjointTimestep({{"temperature", adjoint_load}});
+    temperature_end_of_step = thermal->previousTemperature(thermal->cycle());
 
     if (i==1) {
+      // initial temperature sensitivity math
       auto mu = adjoint_sol.find("adjoint_d_temperature_dt")->second;
-      for (int n=0; n < N; ++n) {
-        gradient[static_cast<size_t>(n)] += mu(n);
+      for (size_t n=0; n < Nsize; ++n) {
+        gradient[n] += mu(int(n));
       }
     }
-
-    prev_temperature = thermal.previousTemperature(thermal.cycle());
   }
 
   return std::make_pair(qoi, gradient);
@@ -226,21 +200,39 @@ class HeatTransferSensitivityFixture : public ::testing::Test
 
 TEST_F(HeatTransferSensitivityFixture, InitialTemperatureSensitivities)
 {
-
-  auto qoiBase = computeThermalQoiAdjustingInitalTemperature(dataStore, nonlinear_opts, dyn_opts, mat, tsInfo, 0, 0.0);
+  std::pair<double, std::vector<double>> trueGrad = computeThermalQoiAndInitialTemperatureGradient(dataStore, nonlinear_opts, dyn_opts, mat, tsInfo);
+  double qoiBase = trueGrad.first;
+  const auto& adjGradient = trueGrad.second;
   int N = NumDofs();
+  //size_t Nsize = static_cast<size_t>(N);
 
   double eps = 1e-7;
   std::vector<double> numericalGradients(static_cast<size_t>(N));
 
   for (int i=0; i < N; ++i) {
     auto qoiPlus = computeThermalQoiAdjustingInitalTemperature(dataStore, nonlinear_opts, dyn_opts, mat, tsInfo, i, eps);
-    double grad = (qoiPlus-qoiBase)/eps;
-    numericalGradients[size_t(i)] = grad;
+    numericalGradients[size_t(i)] = (qoiPlus-qoiBase)/eps;
   }
 
-  std::pair<double, std::vector<double>> trueGrad = computeThermalQoiAndGradient(dataStore, nonlinear_opts, dyn_opts, mat, tsInfo);
+  for (size_t i=0; i < size_t(N); ++i) {
+    EXPECT_NEAR(numericalGradients[i], adjGradient[i], 1e-6);
+  }
+}
+
+TEST_F(HeatTransferSensitivityFixture, ShapeSensitivities)
+{
+  std::pair<double, std::vector<double>> trueGrad = computeThermalQoiAndInitialTemperatureGradient(dataStore, nonlinear_opts, dyn_opts, mat, tsInfo);
+  double qoiBase = trueGrad.first;
   const auto& adjGradient = trueGrad.second;
+  int N = NumDofs();
+
+  double eps = 1e-7;
+  std::vector<double> numericalGradients(static_cast<size_t>(N));
+
+  for (int i=0; i < N; ++i) {
+    auto qoiPlus = computeThermalQoiAdjustingShape(dataStore, nonlinear_opts, dyn_opts, mat, tsInfo, i, eps);
+    numericalGradients[size_t(i)] = (qoiPlus-qoiBase)/eps;
+  }
 
   for (size_t i=0; i < size_t(N); ++i) {
     EXPECT_NEAR(numericalGradients[i], adjGradient[i], 1e-6);
