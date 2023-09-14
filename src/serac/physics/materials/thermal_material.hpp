0d53bb83ec7cef5da864ff0b4b77c22d8164edbe
Michael Tupek <tupek2@llnl.gov>
2023-09-14 07:09:41 -0700

    Add doxygen.

--- src/serac/physics/materials/thermal_material.hpp
+++ src/serac/physics/materials/thermal_material.hpp
@@ -90,13 +90,18 @@ struct IsotropicConductorWithLinearConductivityVsTemperature {
   }
 
   /**
+<<<<<<< HEAD
    * @brief Material response call for a linear isotropic material with linear conductivity vs temperature
+=======
+   * @brief Material response call for a isotropic conductor with linear conductivity vs temperature
+>>>>>>> bc4f7527 (Add doxygen.)
    *
    * @tparam T1 Spatial position type
    * @tparam T2 Temperature type
    * @tparam T3 Temperature gradient type
-   * @param[in] temperature Temperature
-   * @param[in] temperature_gradient Temperature gradient
+=======
+   * @param[in] temperature
+   * @param[in] temperature_gradient
    * @return The calculated material response (tuple of volumetric heat capacity and thermal flux) for a linear
    * isotropic material
    */
