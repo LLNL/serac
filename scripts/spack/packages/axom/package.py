77e6b53d325f04d33ac78ee97a8da529f3b13ce0
Alex Tyler Chapman <chapman39@llnl.gov>
2023-09-14 17:15:37 -0700

    Update Axom to v0.8.1

--- scripts/spack/packages/axom/package.py
+++ scripts/spack/packages/axom/package.py
@@ -27,4 +27,4 @@ class Axom(BuiltinAxom):
     # Note: Make sure this sha coincides with the git submodule
     # Note: We add a number to the end of the real version number to indicate that we have
     #  moved forward past the release. Increment the last number when updating the commit sha.
-    version("0.7.0.5", commit="302442eb260adaf234247acce41dd6850b97cc0b", submodules=False)
+    version("0.8.1.0", commit="0da8a5b1be596887158ac2fcd321524ba5259e15", submodules=False)
