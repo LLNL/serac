6a8ff6aafda81f3cb97b198e572214ac3766a791
Brandon Talamini <talamini1@llnl.gov>
2023-09-29 19:48:48 -0700

    Add missing space

--- src/serac/numerics/functional/tensor.hpp
+++ src/serac/numerics/functional/tensor.hpp
@@ -1217,6 +1217,7 @@ SERAC_HOST_DEVICE constexpr auto det(const tensor<T, 3, 3>& A)
   return A[0][0] * A[1][1] * A[2][2] + A[0][1] * A[1][2] * A[2][0] + A[0][2] * A[1][0] * A[2][1] -
          A[0][0] * A[1][2] * A[2][1] - A[0][1] * A[1][0] * A[2][2] - A[0][2] * A[1][1] * A[2][0];
 }
+
 /**
  * @brief Take determinant of a matrix near the identity without losing precision
  *
