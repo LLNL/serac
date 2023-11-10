af461d3ad820d0ea5cb5cc300489875f08059231
Alex Tyler Chapman <chapman39@llnl.gov>
2023-11-10 15:22:33 -0800

    Rm temp hdf5 hack

--- cmake/thirdparty/SetupSeracThirdParty.cmake
+++ cmake/thirdparty/SetupSeracThirdParty.cmake
@@ -274,14 +274,6 @@ if (NOT SERAC_THIRD_PARTY_LIBRARIES_FOUND)
  
         set(MFEM_FOUND TRUE CACHE BOOL "" FORCE)
 
-        # Temporary hack to inject the hdf5_hl after netcdf and before hdf5
-        # This should go away when mfem fixes their FindNetCDF.cmake
-        get_target_property(_mfem_libraries mfem INTERFACE_LINK_LIBRARIES)
-        list(FIND _mfem_libraries ${NETCDF_DIR}/lib/libnetcdf.a _index)
-        math(EXPR _index "${_index} + 1")
-        list(INSERT _mfem_libraries ${_index} ${HDF5_C_LIBRARY_hdf5_hl})
-        target_link_libraries(mfem PUBLIC ${_mfem_libraries})
-
         # Patch the mfem target with the correct include directories
         get_target_property(_mfem_includes mfem INCLUDE_DIRECTORIES)
         target_include_directories(mfem SYSTEM INTERFACE ${_mfem_includes})
