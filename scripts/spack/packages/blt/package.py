69e7ca375c497ef1f2e1d5c2a1375fe6f1ddcce4
Chris White <white238@llnl.gov>
2024-01-17 13:05:10 -0800

    update to new versions across the chain, some of this can be removed when we bump spack after releases

--- scripts/spack/packages/blt/package.py
+++ scripts/spack/packages/blt/package.py
@@ -3,10 +3,9 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.pkg.builtin.axom import Axom as BuiltinAxom
+from spack.pkg.llnl.radiuss.blt import Blt as RadiussBlt
 
-class Axom(BuiltinAxom):
-    # Note: Make sure this sha coincides with the git submodule
+class Blt(RadiussBlt):
     # Note: We add a number to the end of the real version number to indicate that we have
     #  moved forward past the release. Increment the last number when updating the commit sha.
-    version("0.8.1.6", commit="7806dabff34cc0d10dcf190c25ee2c2f2708621d", submodules=False)
+    version("0.5.3.1", commit="9a904425b75caa48933c9d522873ec5320d66da0", submodules=False)
