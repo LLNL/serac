267705b9d2af41b6eb8dd554a03f2635fda126ae
Eric B. Chin <chin23@llnl.gov>
2024-01-23 15:26:17 -0800

    sphere with nurbs

--- examples/contact/sphere.cpp
+++ examples/contact/sphere.cpp
@@ -6,7 +6,9 @@
 
 #include <cmath>
 
+#include <mfem/fem/fe_coll.hpp>
 #include <string>
+#include <vector>
 
 #include "axom/slic/core/SimpleLogger.hpp"
 
@@ -35,9 +37,24 @@ int main(int argc, char* argv[])
   serac::StateManager::initialize(datastore, name + "_data");
 
   // Construct the appropriate dimension mesh and give it to the data store
-  std::string filename = SERAC_REPO_DIR "/data/meshes/twist_sphere.mesh";
+  int num_refinements { 3 };
 
-  auto mesh = serac::mesh::refineAndDistribute(serac::buildMeshFromFile(filename), 0, 0);
+  mfem::Mesh ball_mesh { SERAC_REPO_DIR "/data/meshes/ball-nurbs2.mesh" };
+  for (int i{0}; i < num_refinements; ++i)
+  {
+    ball_mesh.UniformRefinement();
+  }
+  ball_mesh.SetCurvature(p);
+
+  mfem::Mesh cube_mesh { SERAC_REPO_DIR "/data/meshes/onehex.mesh" };
+  for (int i{0}; i < num_refinements; ++i)
+  {
+    cube_mesh.UniformRefinement();
+  }
+  cube_mesh.SetCurvature(p);
+
+  std::vector<mfem::Mesh*> mesh_ptrs { &ball_mesh, &cube_mesh };
+  auto mesh = serac::mesh::refineAndDistribute(mfem::Mesh(mesh_ptrs.data(), static_cast<int>(mesh_ptrs.size())), 0, 0);
   serac::StateManager::setMesh(std::move(mesh), "sphere_mesh");
 
   serac::LinearSolverOptions linear_options{.linear_solver = serac::LinearSolver::Strumpack, .print_level = 1};
@@ -62,13 +79,13 @@ int main(int argc, char* argv[])
       serac::GeometricNonlinearities::On, name, "sphere_mesh", {"bulk_mod", "shear_mod"});
 
   serac::FiniteElementState K_field(serac::StateManager::newState(serac::L2<0>{}, "bulk_mod", "sphere_mesh"));
-  mfem::Vector              K_values({10.0, 100.0});
+  mfem::Vector              K_values({10.0, 10.0});
   mfem::PWConstCoefficient  K_coeff(K_values);
   K_field.project(K_coeff);
   solid_solver.setParameter(0, K_field);
 
   serac::FiniteElementState G_field(serac::StateManager::newState(serac::L2<0>{}, "shear_mod", "sphere_mesh"));
-  mfem::Vector              G_values({0.25, 2.5});
+  mfem::Vector              G_values({0.25, 0.25});
   mfem::PWConstCoefficient  G_coeff(G_values);
   G_field.project(G_coeff);
   solid_solver.setParameter(1, G_field);
@@ -77,12 +94,12 @@ int main(int argc, char* argv[])
   solid_solver.setMaterial(serac::DependsOn<0, 1>{}, mat);
 
   // Pass the BC information to the solver object
-  solid_solver.setDisplacementBCs({5}, [](const mfem::Vector&, mfem::Vector& u) {
+  solid_solver.setDisplacementBCs({3}, [](const mfem::Vector&, mfem::Vector& u) {
     u.SetSize(dim);
     u = 0.0;
   });
   double time = 0.0;
-  solid_solver.setDisplacementBCs({10}, [&time](const mfem::Vector& x, mfem::Vector& u) {
+  solid_solver.setDisplacementBCs({12}, [&time](const mfem::Vector& x, mfem::Vector& u) {
     u.SetSize(dim);
     u = 0.0;
     if (time <= 3.0 + 1.0e-12) {
@@ -97,7 +114,7 @@ int main(int argc, char* argv[])
   });
 
   // Add the contact interaction
-  solid_solver.addContactInteraction(0, {6}, {7}, contact_options);
+  solid_solver.addContactInteraction(0, {5}, {7}, contact_options);
 
   // Finalize the data structures
   solid_solver.completeSetup();
