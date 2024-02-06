1210fe090a35500d909be873c10fac421bb06ac0
Kenny Swartz <swartz10@llnl.gov>
2024-02-06 11:19:35 -0800

    remove domain from add custom domain integral

--- src/serac/physics/solid_mechanics.hpp
+++ src/serac/physics/solid_mechanics.hpp
@@ -765,11 +765,11 @@ public:
    * @note This method must be called prior to completeSetup()
    */
   template <int... active_parameters, typename callable, typename StateType = Nothing>
-  void addCustomDomainIntegral(DependsOn<active_parameters...>, callable qfunction, Domain domain,
+  void addCustomDomainIntegral(DependsOn<active_parameters...>, callable qfunction,
                                qdata_type<StateType> qdata = NoQData)
   {
     residual_->AddDomainIntegral(Dimension<dim>{}, DependsOn<0, 1, active_parameters + NUM_STATE_VARS...>{}, qfunction,
-                                 domain, qdata);
+                                 mesh_, qdata);
   }
 
   /**
