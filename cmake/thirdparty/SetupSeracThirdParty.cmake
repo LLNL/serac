f4898df2eb74caf4b2e96d291f12c303165480ac
Brandon Talamini <talamini1@llnl.gov>
2024-03-14 08:50:55 -0700

    Fix mfem failing to include link location for mpi_mpifh
    Temporary fix. Seems that under some situations, mfem does not
    properly include the location of all libraries it is linking
    against. We're not sure exactly what causes this, but Chris has
    an inkling that it may be when mfem is built with strumpack.
    
    Worked with Chris White.

--- cmake/thirdparty/SetupSeracThirdParty.cmake
+++ cmake/thirdparty/SetupSeracThirdParty.cmake
@@ -580,4 +580,18 @@ if (NOT SERAC_THIRD_PARTY_LIBRARIES_FOUND)
                     DESTINATION          lib)
         endif()
     endforeach()
+
+    set(_mfem_targets
+        mfem
+        axom::mfem
+        tribol::mfem)
+    foreach(_target ${_mfem_targets})
+        if(TARGET ${_target})
+            message(STATUS "Adding mpi link directory to target [${_target}]")
+            list(GET MPI_C_LIBRARIES 0 _first_mpi_lib)
+            get_filename_component(_mpi_lib_dir ${_first_mpi_lib} DIRECTORY)
+            target_link_directories(${_target} INTERFACE ${_mpi_lib_dir})
+            target_link_libraries(${_target} INTERFACE mpi)
+        endif()
+    endforeach()
 endif()
