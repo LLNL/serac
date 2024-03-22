da9a775bf1ab87901e5ee5c707944f5be25aff5c
Jamie A. Bramwell <bramwell1@llnl.gov>
2024-03-22 09:26:25 -0700

    Re-enable the existing benchmark test

--- src/serac/physics/benchmarks/benchmark_thermal.cpp
+++ src/serac/physics/benchmarks/benchmark_thermal.cpp
@@ -12,7 +12,7 @@
 #include "serac/serac_config.hpp"
 #include "serac/infrastructure/profiling.hpp"
 #include "serac/mesh/mesh_utils.hpp"
-#include "serac/physics/materials/thermal_functional_material.hpp"
+#include "serac/physics/materials/thermal_material.hpp"
 #include "serac/physics/state/state_manager.hpp"
 #include "serac/physics/heat_transfer.hpp"
 
@@ -34,16 +34,16 @@ void functional_test_static()
   std::string filename =
       (dim == 2) ? SERAC_REPO_DIR "/data/meshes/star.mesh" : SERAC_REPO_DIR "/data/meshes/beam-hex.mesh";
 
-  auto mesh = mesh::refineAndDistribute(buildMeshFromFile(filename), serial_refinement, parallel_refinement);
-  serac::StateManager::setMesh(std::move(mesh));
+  auto mesh = serac::mesh::refineAndDistribute(serac::buildMeshFromFile(filename), serial_refinement, parallel_refinement);
+  serac::StateManager::setMesh(std::move(mesh), "default_mesh");
 
   // Define a boundary attribute set
   std::set<int> ess_bdr = {1};
 
   // Construct a functional-based heat transfer solver
-  HeatTransfer<p, dim> thermal_solver(HeatTransfer<p, dim>::defaultQuasistaticOptions(), "thermal_functional");
+  serac::HeatTransfer<p, dim> thermal_solver(serac::heat_transfer::default_nonlinear_options, serac::heat_transfer::default_linear_options, serac::heat_transfer::default_static_options, "thermal_functional", "default_mesh");
 
-  tensor<double, dim, dim> cond;
+  serac::tensor<double, dim, dim> cond;
 
   // Define an anisotropic conductor material model
   if constexpr (dim == 2) {
@@ -54,7 +54,7 @@ void functional_test_static()
     cond = {{{1.5, 0.01, 0.0}, {0.01, 1.0, 0.0}, {0.0, 0.0, 1.0}}};
   }
 
-  Thermal::LinearConductor<dim> mat(1.0, 1.0, cond);
+  serac::heat_transfer::LinearConductor<dim> mat(1.0, 1.0, cond);
   thermal_solver.setMaterial(mat);
 
   // Define the function for the initial temperature and boundary condition
@@ -65,12 +65,8 @@ void functional_test_static()
   thermal_solver.setTemperature(one);
 
   // Define a constant source term
-  Thermal::ConstantSource source{1.0};
+  serac::heat_transfer::ConstantSource source{1.0};
   thermal_solver.setSource(source);
-
-  // Set the flux term to zero for testing code paths
-  Thermal::FluxBoundary flux_bc{0.0};
-  thermal_solver.setFluxBCs(flux_bc);
 
   // Finalize the data structures
   thermal_solver.completeSetup();
@@ -101,17 +97,17 @@ void functional_test_dynamic()
   std::string filename =
       (dim == 2) ? SERAC_REPO_DIR "/data/meshes/star.mesh" : SERAC_REPO_DIR "/data/meshes/beam-hex.mesh";
 
-  auto mesh = mesh::refineAndDistribute(buildMeshFromFile(filename), serial_refinement, parallel_refinement);
-  serac::StateManager::setMesh(std::move(mesh));
+  auto mesh = serac::mesh::refineAndDistribute(serac::buildMeshFromFile(filename), serial_refinement, parallel_refinement);
+  serac::StateManager::setMesh(std::move(mesh), "default_mesh");
 
   // Define a boundary attribute set
   std::set<int> ess_bdr = {1};
 
   // Construct a functional-based heat transfer solver
-  HeatTransfer<p, dim> thermal_solver(HeatTransfer<p, dim>::defaultDynamicOptions(), "thermal_functional");
+  serac::HeatTransfer<p, dim> thermal_solver(serac::heat_transfer::default_nonlinear_options, serac::heat_transfer::default_linear_options, serac::heat_transfer::default_timestepping_options, "thermal_functional", "default_mesh");
 
   // Define an isotropic conductor material model
-  Thermal::LinearIsotropicConductor mat(1.0, 1.0, 1.0);
+  serac::heat_transfer::LinearIsotropicConductor mat(1.0, 1.0, 1.0);
 
   thermal_solver.setMaterial(mat);
 
@@ -128,12 +124,8 @@ void functional_test_dynamic()
   thermal_solver.setTemperature(initial_temp);
 
   // Define a constant source term
-  Thermal::ConstantSource source{1.0};
+  serac::heat_transfer::ConstantSource source{1.0};
   thermal_solver.setSource(source);
-
-  // Set the flux term to zero for testing code paths
-  Thermal::FluxBoundary flux_bc{0.0};
-  thermal_solver.setFluxBCs(flux_bc);
 
   // Finalize the data structures
   thermal_solver.completeSetup();
