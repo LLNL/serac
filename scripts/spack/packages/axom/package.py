35b91970e685cde9708120f62fc43e6b46f2405b
Chris White <white238@llnl.gov>
2024-04-10 12:53:37 -0700

    fix axom's version and bump tribol

--- scripts/spack/packages/axom/package.py
+++ scripts/spack/packages/axom/package.py
@@ -9,4 +9,4 @@ class Axom(BuiltinAxom):
     # Note: Make sure this sha coincides with the git submodule
     # Note: We add a number to the end of the real version number to indicate that we have
     #  moved forward past the release. Increment the last number when updating the commit sha.
-    version("0.9.0.1", commit="b71b0264b33c7cf78f48af5bd634ab8afd14f083", submodules=False)
+    version("0.9.1.1", commit="b71b0264b33c7cf78f48af5bd634ab8afd14f083", submodules=False)
