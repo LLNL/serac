e4181e275d26252b80230972297d40ffbf824913
Chris White <white238@llnl.gov>
2024-04-26 12:37:48 -0700

    update copyrights

--- scripts/spack/packages/camp/package.py
+++ scripts/spack/packages/camp/package.py
@@ -1,7 +1,7 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright (c) 2019-2024, Lawrence Livermore National Security, LLC and
+# other Serac Project Developers. See the top-level COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+# SPDX-License-Identifier: (BSD-3-Clause)
 
 from spack.pkg.llnl.radiuss.camp import Camp as RadiussCamp
 
