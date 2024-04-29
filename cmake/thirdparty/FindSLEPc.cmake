edf6fffb91b621085d6d6fcc340f832e7ce1fa56
Alex Tyler Chapman <chapman39@llnl.gov>
2024-04-29 14:27:20 -0700

    Set verbose make builds on docker, improve slepc module

--- cmake/thirdparty/FindSLEPc.cmake
+++ cmake/thirdparty/FindSLEPc.cmake
@@ -26,17 +26,21 @@ if(NOT ARPACK_DIR)
   message(FATAL_ERROR "Could not find arpack. Slepc requires ARPACK_DIR.")
 endif()
 
-# Add missing arpack include dir and link libs
-target_include_directories(PkgConfig::SLEPC INTERFACE ${ARPACK_DIR}/include)
-target_link_libraries(PkgConfig::SLEPC INTERFACE ${ARPACK_DIR}/lib64/libparpack.so)
+# Give slepc petsc's link libs and dirs and add missing arpack include dir and link libs
 
-# Give slepc petsc's link libs and dirs
-get_target_property(_petsc_link_libs PkgConfig::PETSC INTERFACE_LINK_LIBRARIES)
-get_target_property(_petsc_link_dirs PkgConfig::PETSC INTERFACE_LINK_DIRECTORIES)
-target_link_libraries(PkgConfig::SLEPC INTERFACE ${_petsc_link_libs})
-target_link_directories(PkgConfig::SLEPC INTERFACE ${_petsc_link_dirs})
-
-# Remove duplicates from INTERFACE_LINK_LIBRARIES
+# Link libraries
 get_target_property(_slepc_link_libs PkgConfig::SLEPC INTERFACE_LINK_LIBRARIES)
+get_target_property(_petsc_link_libs PkgConfig::PETSC INTERFACE_LINK_LIBRARIES)
+list(APPEND _slepc_link_libs ${ARPACK_DIR}/lib64/libparpack.so ${_petsc_link_libs})
 blt_list_remove_duplicates(TO _slepc_link_libs)
 set_target_properties(PkgConfig::SLEPC PROPERTIES INTERFACE_LINK_LIBRARIES "${_slepc_link_libs}")
+
+# Include directories
+get_target_property(_slepc_include_dirs PkgConfig::SLEPC INTERFACE_INCLUDE_DIRECTORIES)
+list(APPEND _slepc_include_dirs ${ARPACK_DIR}/include)
+set_target_properties(PkgConfig::SLEPC PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${_slepc_include_dirs}")
+
+# Link directories
+get_target_property(_petsc_link_dirs PkgConfig::PETSC INTERFACE_LINK_DIRECTORIES)
+set(_slepc_link_dirs ${_petsc_link_dirs})
+set_target_properties(PkgConfig::SLEPC PROPERTIES INTERFACE_LINK_DIRECTORIES "${_slepc_link_dirs}")
