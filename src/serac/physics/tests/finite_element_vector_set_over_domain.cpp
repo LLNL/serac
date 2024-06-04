57629c71a69dd05c1cc41f252f725dbdae0bafee
Sam Mish <mish2@llnl.gov>
2024-06-04 14:19:54 -0700

    adding vector valued test, it's segfaulting in an mfem routine

--- src/serac/physics/tests/finite_element_vector_set_over_domain.cpp
+++ src/serac/physics/tests/finite_element_vector_set_over_domain.cpp
@@ -28,7 +28,7 @@ tensor<double, dim> average(std::vector<tensor<double, dim> >& positions)
   return total / double(positions.size());
 }
 
-TEST(FiniteElementVector, SetScalarFieldOverDomain)
+TEST(FiniteElementVector, SetScalarFieldOver2DDomain)
 {
   MPI_Barrier(MPI_COMM_WORLD);
 
@@ -64,6 +64,56 @@ TEST(FiniteElementVector, SetScalarFieldOverDomain)
   EXPECT_NEAR(u[2], 2.0, 1.0e-15);
   for (int i = 3; i < 9; i++) {
     EXPECT_NEAR(u[i], 0.0, 1.0e-15);
+  }
+
+}
+
+TEST(FiniteElementVector, SetVectorFieldOver2DDomain)
+{
+  constexpr int p   = 1;
+  constexpr int dim = 2;
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  // Create DataStore
+  axom::sidre::DataStore datastore;
+  serac::StateManager::initialize(datastore, "thermal_functional_parameterized_sensitivities");
+
+  //  6--7--8
+  //  |  |  |
+  //  3--4--5
+  //  |  |  |
+  //  0--1--2
+  auto mesh = buildRectangleMesh(2, 2, 1.0, 1.0);
+
+  auto pmesh = mesh::refineAndDistribute(std::move(mesh), 0, 0);
+
+  FiniteElementState u(*pmesh, H1<p, dim>{});
+
+  Domain essential_boundary = Domain::ofBoundaryElements(*pmesh, [](std::vector<serac::vec2> x, int /*attr*/){ 
+    return average(x)[1] < 0.1; 
+  }); 
+
+  mfem::VectorFunctionCoefficient func(dim, [](const mfem::Vector& x, mfem::Vector& u) { 
+    u[0] = x[0] + 1.0;
+    u[1] = x[0] + 2.0;
+  });
+
+  u = 0.0;
+  u.project(func, essential_boundary);
+
+  EXPECT_NEAR(u[0], 1.0, 1.0e-15);
+  EXPECT_NEAR(u[1], 1.5, 1.0e-15);
+  EXPECT_NEAR(u[2], 2.0, 1.0e-15);
+  for (int i = 3; i < 9; i++) {
+    EXPECT_NEAR(u[i], 0.0, 1.0e-15);
+  }
+
+  EXPECT_NEAR(u[9], 2.0, 1.0e-15);
+  EXPECT_NEAR(u[10], 2.5, 1.0e-15);
+  EXPECT_NEAR(u[11], 3.0, 1.0e-15);
+  for (int i = 3; i < 9; i++) {
+    EXPECT_NEAR(u[i + 9], 0.0, 1.0e-15);
   }
 
 }
