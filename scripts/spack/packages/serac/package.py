1ba755855a4299e4718143032620318d65857dc8
Alex Chapman <chapman39@llnl.gov>
2024-06-24 15:09:56 -0700

    fix ordering to show no diff

--- scripts/spack/packages/serac/package.py
+++ scripts/spack/packages/serac/package.py
@@ -109,10 +109,10 @@ class Serac(CachedCMakePackage, CudaPackage):
     depends_on("hypre@2.26.0~superlu-dist+mpi")
 
     depends_on("petsc", when="+petsc")
-    depends_on("petsc+openmp", when="+petsc+openmp")
-    depends_on("petsc~openmp", when="+petsc~openmp")
     depends_on("petsc+strumpack", when="+petsc+strumpack")
     depends_on("petsc~strumpack", when="+petsc~strumpack")
+    depends_on("petsc+openmp", when="+petsc+openmp")
+    depends_on("petsc~openmp", when="+petsc~openmp")
     depends_on("slepc+arpack", when="+slepc")
 
     depends_on("tribol", when="+tribol")
