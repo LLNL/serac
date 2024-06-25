15ed470b4595ac420dd4a9f2b48b5dc263bce2dc
Zach Atkins <atkins12@llnl.gov>
2024-06-25 15:59:56 -0700

    Fix parallel crashes and guards

--- src/serac/numerics/tests/equationsolver_petsc.cpp
+++ src/serac/numerics/tests/equationsolver_petsc.cpp
@@ -119,17 +119,17 @@ TEST_P(EquationSolverSuite, All)
 #ifdef SERAC_USE_SUNDIALS
 INSTANTIATE_TEST_SUITE_P(
     AllEquationSolverTests, EquationSolverSuite,
-    testing::Combine(
-        testing::Values(NonlinearSolver::Newton, NonlinearSolver::KINFullStep,
-                        NonlinearSolver::KINBacktrackingLineSearch, NonlinearSolver::KINPicard,
-                        NonlinearSolver::PetscNewton, NonlinearSolver::PetscNewtonBacktracking,
-                        NonlinearSolver::PetscNewtonCriticalPoint),
-        testing::Values(LinearSolver::CG, LinearSolver::GMRES, LinearSolver::PetscCG, LinearSolver::PetscGMRES),
-        testing::Values(Preconditioner::Petsc),
-        testing::Values(PetscPCType::JACOBI, PetscPCType::JACOBI_L1, PetscPCType::JACOBI_ROWSUM,
-                        PetscPCType::JACOBI_ROWMAX, PetscPCType::PBJACOBI, PetscPCType::BJACOBI, PetscPCType::LU,
-                        PetscPCType::ILU, PetscPCType::CHOLESKY, PetscPCType::ICC, PetscPCType::SVD, PetscPCType::ASM,
-                        PetscPCType::GASM, PetscPCType::GAMG, PetscPCType::HMG)));
+    testing::Combine(testing::Values(NonlinearSolver::Newton, NonlinearSolver::KINFullStep,
+                                     NonlinearSolver::KINBacktrackingLineSearch, NonlinearSolver::KINPicard,
+                                     NonlinearSolver::PetscNewton, NonlinearSolver::PetscNewtonBacktracking,
+                                     NonlinearSolver::PetscNewtonCriticalPoint),
+                     testing::Values(LinearSolver::CG, LinearSolver::GMRES, LinearSolver::PetscCG,
+                                     LinearSolver::PetscGMRES),
+                     testing::Values(Preconditioner::Petsc),
+                     testing::Values(PetscPCType::JACOBI, PetscPCType::JACOBI_L1, PetscPCType::JACOBI_ROWSUM,
+                                     PetscPCType::JACOBI_ROWMAX, PetscPCType::PBJACOBI, PetscPCType::BJACOBI,
+                                     PetscPCType::LU, PetscPCType::ILU, PetscPCType::CHOLESKY, PetscPCType::SVD,
+                                     PetscPCType::ASM, PetscPCType::GASM, PetscPCType::GAMG, PetscPCType::HMG)));
 #else
 INSTANTIATE_TEST_SUITE_P(
     AllEquationSolverTests, EquationSolverSuite,
