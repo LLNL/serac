2770ec8f57d9c7c870a577ef95716aca458d65d8
Alex Tyler Chapman <chapman39@llnl.gov>
2024-07-10 15:13:43 -0700

    Fix spacing issues with EXTRA_BUILD_OPTIONS

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -2,10 +2,14 @@
 
 echo "DO_INTEGRATION_TESTS=${DO_INTEGRATION_TESTS}"
 echo "EXTRA_BUILD_OPTIONS=${EXTRA_BUILD_OPTIONS}"
+echo "EXTRA_CMAKE_OPTIONS=${EXTRA_CMAKE_OPTIONS}"
 echo "HOST_CONFIG=${HOST_CONFIG}"
 
 # Build source, run unit tests, and test install examples
-python3 scripts/llnl/build_src.py -v --host-config ${HOST_CONFIG} "${EXTRA_BUILD_OPTIONS}"
+python3 scripts/llnl/build_src.py -v \
+    --host-config ${HOST_CONFIG} \
+    --extra-cmake-options "${EXTRA_CMAKE_OPTIONS}" \
+    ${EXTRA_BUILD_OPTIONS}
 if [ $? -ne 0 ]; then { echo "ERROR: build_src.py failed." ; exit 1; } fi
 
 # Run integration tests
