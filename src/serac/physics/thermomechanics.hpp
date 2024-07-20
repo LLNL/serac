f3813f29d6da15b12a4f673275dde29d3118c94f
Brandon Talamini <talamini1@llnl.gov>
2024-07-19 22:02:55 -0700

    Implement internal heat source heating

--- src/serac/physics/thermomechanics.hpp
+++ src/serac/physics/thermomechanics.hpp
@@ -31,6 +31,12 @@ namespace serac {
 template <int order, int dim, typename... parameter_space>
 class Thermomechanics : public BasePhysics {
 public:
+
+  /// @brief a container holding quadrature point data of the specified type
+  /// @tparam T the type of data to store at each quadrature point
+  template <typename T>
+  using qdata_type = std::shared_ptr<QuadratureData<T>>;
+
   /**
    * @brief Construct a new coupled Thermal-SolidMechanics object
    *
@@ -295,17 +301,49 @@ public:
      * @param parameters values and derivatives of any additional user-specified parameters
      */
     template <typename T1, typename T2, typename T3, typename T4, typename... param_types>
-    SERAC_HOST_DEVICE auto operator()(const T1& /* x */, const T2& temperature, const T3& temperature_gradient,
+    SERAC_HOST_DEVICE auto operator()(State& state, const T1& /* x */, const T2& temperature, const T3& temperature_gradient,
                                       const T4& displacement, param_types... parameters) const
     {
-      // BT: this will not update the state correctly. I just want to get the code compiling before plumbing the
-      // state variables.
-      State state{};
-
       auto [u, du_dX]                 = displacement;
       auto [T, heat_capacity, s0, q0] = mat(state, du_dX, temperature, temperature_gradient, parameters...);
 
       return serac::tuple{heat_capacity, q0};
+    }
+  };
+  
+  template <typename ThermalMechanicalMaterial>
+  struct ThermalMaterialSourceInterface {
+    using State = typename ThermalMechanicalMaterial::State;  ///< internal variables for the wrapped material model
+
+    const ThermalMechanicalMaterial mat;  ///< the wrapped material model
+
+    /// constructor
+    ThermalMaterialSourceInterface(const ThermalMechanicalMaterial& m) : mat(m)
+    {
+      // empty
+    }
+
+    /**
+     * @brief glue code to evaluate a thermomechanical material and extract the thermal outputs
+     *
+     * @tparam T1 the type of the spatial coordinate values
+     * @tparam T2 the type of the temperature value
+     * @tparam T3 the type of the temperature gradient values
+     * @tparam T4 the type of the displacement gradient values
+     * @tparam param_types the types of user-specified parameters
+     * @param temperature the temperature at this quadrature point
+     * @param temperature_gradient the gradient w.r.t. physical coordinates of the temperature
+     * @param displacement the value and gradient w.r.t. physical coordinates of the displacement
+     * @param parameters values and derivatives of any additional user-specified parameters
+     */
+    template <typename T1, typename T2, typename T3, typename T4, typename... param_types>
+    SERAC_HOST_DEVICE auto operator()(State& state, const T1& /* x */, const T2& temperature, const T3& temperature_gradient,
+                                      const T4& displacement, param_types... parameters) const
+    {
+      auto [u, du_dX]                 = displacement;
+      auto [T, heat_capacity, s0, q0] = mat(state, du_dX, temperature, temperature_gradient, parameters...);
+
+      return tuple{s0, zero{}};
     }
   };
 
@@ -381,9 +419,10 @@ public:
     // note: these parameter indices are offset by 1 since, internally, this module uses the first parameter
     // to communicate the temperature and displacement field information to the other physics module
     //
-    thermal_.setMaterial(DependsOn<0, active_parameters + 1 ...>{}, ThermalMaterialInterface<MaterialType>{material});
+    thermal_.setMaterial(DependsOn<0, active_parameters + 1 ...>{}, ThermalMaterialInterface<MaterialType>{material}, qdata);
     solid_.setMaterial(DependsOn<0, active_parameters + 1 ...>{}, MechanicalMaterialInterface<MaterialType>{material},
                        qdata);
+    thermal_.setMaterial(DependsOn<0, active_parameters + 1 ...>{}, ThermalMaterialSourceInterface<MaterialType>{material}, qdata);
   }
 
   /// @overload
