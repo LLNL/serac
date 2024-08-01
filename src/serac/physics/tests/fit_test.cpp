c3a378a7d9936b9c3cbf412098a3ad62578caced
Sam Mish <samuelpmish@gmail.com>
2024-08-01 12:28:47 -0700

    making a more interesting test

--- src/serac/physics/tests/fit_test.cpp
+++ src/serac/physics/tests/fit_test.cpp
@@ -34,8 +34,60 @@ void stress_extrapolation_test(mfem::ParMesh& mesh) {
 
   FiniteElementState u(mesh, input_space{}, "displacement");
 
-  // TODO
-  // set u := u(x, y)
+  // Create DataStore
+  axom::sidre::DataStore datastore;
+  serac::StateManager::initialize(datastore, "solid_mechanics_J2_test");
+
+  // Construct the appropriate dimension mesh and give it to the data store
+  std::string filename = SERAC_REPO_DIR "/data/meshes/beam-hex.mesh";
+
+  auto mesh = mesh::refineAndDistribute(buildMeshFromFile(filename), serial_refinement, parallel_refinement);
+
+  std::string mesh_tag{"mesh"};
+
+  serac::StateManager::setMesh(std::move(mesh), mesh_tag);
+
+  serac::LinearSolverOptions linear_options{.linear_solver = LinearSolver::SuperLU};
+
+  serac::NonlinearSolverOptions nonlinear_options{.nonlin_solver  = NonlinearSolver::Newton,
+                                                  .relative_tol   = 1.0e-12,
+                                                  .absolute_tol   = 1.0e-12,
+                                                  .max_iterations = 5000,
+                                                  .print_level    = 1};
+
+  SolidMechanics<p, dim> solid_solver(nonlinear_options, linear_options, solid_mechanics::default_quasistatic_options,
+                                      GeometricNonlinearities::Off, "solid_mechanics", mesh_tag);
+
+  solid_mechanics::Neohookean mat{
+    1.0,   // density
+    100.0, // bulk modulus
+    50.0   // shear modulus
+  };
+
+  solid_solver.setMaterial(mat);
+
+  // prescribe zero displacement at the supported end of the beam,
+  std::set<int> support           = {1};
+  auto          zero_displacement = [](const mfem::Vector&, mfem::Vector& u) -> void { u = 0.0; };
+  solid_solver.setDisplacementBCs(support, zero_displacement);
+
+  // apply a displacement along z to the the tip of the beam
+  auto translated_in_z = [](const mfem::Vector&, double t, mfem::Vector& u) -> void {
+    u    = 0.0;
+    u[2] = t * (t - 1);
+  };
+  std::set<int> tip = {2};
+  solid_solver.setDisplacementBCs(tip, translated_in_z);
+
+  solid_solver.setDisplacement(zero_displacement);
+
+  // Finalize the data structures
+  solid_solver.completeSetup();
+
+  solid_solver.outputStateToDisk("paraview");
+
+  double dt = 1.0;
+  solid_solver.advanceTimestep(dt);
 
   FiniteElementState sigma_01 = fit< dim, output_space(input_space) >([&](double /*t*/, auto /*x*/, auto displacement){ 
     auto du_dx = get<1>(displacement);
