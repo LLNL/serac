af632c3a4472b9130dfd832a8c60556b16f93e2b
Zach Atkins <atkins12@llnl.gov>
2024-08-05 15:42:39 -0700

    Document and add sample runs for cylinder example

--- examples/buckling/cylinder.cpp
+++ examples/buckling/cylinder.cpp
@@ -4,6 +4,26 @@
 //
 // SPDX-License-Identifier: (BSD-3-Clause)
 
+/**
+ * @file cylinder.cpp
+ *
+ * @brief A buckling cylinder under compression, run with or without contact
+ *
+ * @note Run with mortar contact and PETSc preconditioners:
+ * @code{.sh}
+ * ./build/examples/buckling_cylinder --contact-type 1 --preconditioner 6 \
+ *     -options_file examples/buckling/cylinder_petsc_options.yml
+ * @endcode
+ * @note Run with penalty contact and HYPRE BoomerAMG preconditioner
+ * @code{.sh}
+ * ./build/examples/buckling_cylinder --penalty 1e3
+ * @endcode
+ * @note Run without contact:
+ * @code{.sh}
+ * ./build/examples/buckling_cylinder --no-contact
+ * @endcode
+ */
+
 #include <set>
 #include <string>
 
@@ -25,18 +45,23 @@ std::function<std::string(const std::string&)> petscPCTypeValidator = [](const s
   return std::to_string(static_cast<int>(mfem_ext::stringToPetscPCType(in)));
 };
 
+/**
+ * @brief Run buckling cylinder example
+ *
+ * @note Based on doi:10.1016/j.cma.2014.08.012
+ */
 int main(int argc, char* argv[])
 {
   constexpr int dim = 3;
   constexpr int p   = 1;
 
   // Command line arguments
+  // Mesh options
   int    serial_refinement   = 0;
   int    parallel_refinement = 0;
-  bool   use_contact         = false;
-  auto   contact_type        = serac::ContactEnforcement::Penalty;
   double dt                  = 0.1;
 
+  // Solver options
   NonlinearSolverOptions nonlinear_options     = solid_mechanics::default_nonlinear_options;
   LinearSolverOptions    linear_options        = solid_mechanics::default_linear_options;
   nonlinear_options.nonlin_solver              = serac::NonlinearSolver::TrustRegion;
@@ -47,25 +72,27 @@ int main(int argc, char* argv[])
   nonlinear_options.max_line_search_iterations = 20;
   nonlinear_options.print_level                = 1;
 #ifdef SERAC_USE_PETSC
-  linear_options.linear_solver        = serac::LinearSolver::PetscGMRES;
-  linear_options.preconditioner       = serac::Preconditioner::Petsc;
-  linear_options.petsc_preconditioner = serac::PetscPCType::GAMG;
-  // linear_options.preconditioner       = serac::Preconditioner::HypreAMG;
+  linear_options.linear_solver  = serac::LinearSolver::GMRES;
+  linear_options.preconditioner = serac::Preconditioner::HypreAMG;
   linear_options.relative_tol   = 1e-8;
   linear_options.absolute_tol   = 1e-16;
   linear_options.max_iterations = 2000;
 #endif
 
   // Contact specific options
-  double penalty = 1e3;
+  double penalty      = 1e3;
+  bool   use_contact  = true;
+  auto   contact_type = serac::ContactEnforcement::Penalty;
 
+  // Initialize Serac and all of the external libraried
   serac::initialize(argc, argv);
-  axom::slic::setLoggingMsgLevel(axom::slic::message::Level::Debug);
 
   // Handle command line arguments
   axom::CLI::App app{"Hollow cylinder buckling example"};
+  // Mesh options
   app.add_option("--serial-refinement", serial_refinement, "Serial refinement steps", true);
   app.add_option("--parallel-refinement", parallel_refinement, "Parallel refinement steps", true);
+  // Solver options
   app.add_option("--nonlinear-solver", nonlinear_options.nonlin_solver, "Nonlinear solver", true);
   app.add_option("--linear-solver", linear_options.linear_solver, "Linear solver", true);
   app.add_option("--preconditioner", linear_options.preconditioner, "Preconditioner", true);
@@ -76,11 +103,14 @@ int main(int argc, char* argv[])
           },
           "Convert string to PetscPCType", "PetscPCTypeTransform");
   app.add_option("--dt", dt, "Size of pseudo-time step pre-contact", true);
-  app.add_flag("--contact", use_contact, "Use contact for the inner faces of the cylinder");
+  // Contact options
+  app.add_flag("--contact,!--no-contact", use_contact, "Use contact for the inner faces of the cylinder");
   app.add_option("--contact-type", contact_type,
                  "Type of contact enforcement, 0 for penalty or 1 for Lagrange multipliers", true)
       ->needs("--contact");
   app.add_option("--penalty", penalty, "Penalty for contact", true)->needs("--contact");
+
+  // Need to allow extra arguments for PETSc support
   app.set_help_flag("--help");
   app.allow_extras()->parse(argc, argv);
 
@@ -92,17 +122,19 @@ int main(int argc, char* argv[])
   axom::sidre::DataStore datastore;
   serac::StateManager::initialize(datastore, name + "_data");
 
-  std::set<int> xneg{2};
-  std::set<int> xpos{3};
-  std::set<int> bottom   = {1};
-  std::set<int> top      = {4};
-  std::string   filename = SERAC_REPO_DIR "/data/meshes/hollow-cylinder.mesh";
-
-  auto mesh  = serac::buildMeshFromFile(filename);
-  auto pmesh = mesh::refineAndDistribute(std::move(mesh), serial_refinement, parallel_refinement);
-
+  // Create and refine mesh
+  std::string filename = SERAC_REPO_DIR "/data/meshes/hollow-cylinder.mesh";
+  auto        mesh     = serac::buildMeshFromFile(filename);
+  auto        pmesh    = mesh::refineAndDistribute(std::move(mesh), serial_refinement, parallel_refinement);
   serac::StateManager::setMesh(std::move(pmesh), mesh_tag);
 
+  // Surface attributes for boundary conditions
+  std::set<int> xneg{2};
+  std::set<int> xpos{3};
+  std::set<int> bottom{1};
+  std::set<int> top{4};
+
+  // Create solver, either with or without contact
   std::unique_ptr<SolidMechanics<p, dim>> solid_solver;
   if (use_contact) {
     auto solid_contact_solver = std::make_unique<serac::SolidMechanicsContact<p, dim>>(
@@ -140,21 +172,25 @@ int main(int argc, char* argv[])
   solid_solver->setMaterial(mat);
 
   // Set up essential boundary conditions
+  // Bottom of cylinder is fixed
   auto clamp = [](const mfem::Vector&, mfem::Vector& u) {
     u.SetSize(dim);
     u = 0.0;
   };
+  solid_solver->setDisplacementBCs(bottom, clamp);
+
+  // Top of cylinder has prescribed displacement of magnitude in x-z direction
   auto compress = [&](const mfem::Vector&, double t, mfem::Vector& u) {
     u.SetSize(dim);
     u    = 0.0;
     u(0) = u(2) = -1.5 / std::sqrt(2.0) * t;
   };
-  solid_solver->setDisplacementBCs(bottom, clamp);
   solid_solver->setDisplacementBCs(top, compress);
 
   // Finalize the data structures
   solid_solver->completeSetup();
 
+  // Save initial state
   std::string paraview_name = name + "_paraview";
   solid_solver->outputStateToDisk(paraview_name);
 
@@ -167,6 +203,7 @@ int main(int argc, char* argv[])
     SLIC_INFO_ROOT(axom::fmt::format("time = {} (out of 1.0)", solid_solver->time()));
     serac::logger::flush();
 
+    // Refine dt as contact starts
     auto next_dt = solid_solver->time() < 0.65 ? dt : dt * 0.1;
     solid_solver->advanceTimestep(next_dt);
 
@@ -175,5 +212,6 @@ int main(int argc, char* argv[])
   }
   SLIC_INFO_ROOT(axom::fmt::format("final time = {}", solid_solver->time()));
 
+  // Exit without error
   serac::exitGracefully();
 }
