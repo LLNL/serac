08cef1134e8ae71007976054c117199bf9d658d1
EB Chin <chin23@llnl.gov>
2024-08-15 14:41:46 -0700

    update tribol hash again

--- scripts/spack/packages/tribol/package.py
+++ scripts/spack/packages/tribol/package.py
@@ -44,7 +44,7 @@ class Tribol(CachedCMakePackage, CudaPackage, ROCmPackage):
     # SERAC EDIT START
     # Note: We add a number to the end of the real version number to indicate that we have
     #  moved forward past the release. Increment the last number when updating the commit sha.
-    version("0.1.0.16", commit="4f9c9dc37502549d3135c63a6a7686affa4b7882", submodules=True, preferred=True)
+    version("0.1.0.16", commit="99068de157aa836e75c063080e011c4b1ed22200", submodules=True, preferred=True)
     # SERAC EDIT END
 
     # -----------------------------------------------------------------------
