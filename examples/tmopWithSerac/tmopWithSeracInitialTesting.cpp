082f4a64078a2605068b008880320c75191c28d1
Jorge Luis <barreracruz1@llnl.gov>
2024-10-10 14:19:54 -0700

    Copy tmop driver and mesh from lido

--- examples/tmopWithSerac/tmopWithSeracInitialTesting.cpp
+++ examples/tmopWithSerac/tmopWithSeracInitialTesting.cpp
@@ -11,73 +11,172 @@
  * the C++ API to configure the simulation
  */
 
-// _incl_heat_transfer_header_start
-#include "serac/physics/heat_transfer.hpp"
-#include "serac/physics/materials/thermal_material.hpp"
-// _incl_heat_transfer_header_end
-// _incl_state_manager_start
-#include "serac/physics/state/state_manager.hpp"
-// _incl_state_manager_end
-// _incl_infra_start
-#include "serac/infrastructure/initialize.hpp"
-#include "serac/infrastructure/terminator.hpp"
-// _incl_infra_end
-// _incl_mesh_start
+#include "mfem.hpp"
+
+#include <serac/infrastructure/terminator.hpp>
+#include <serac/numerics/functional/functional.hpp>
+#include "serac/numerics/functional/shape_aware_functional.hpp"
+#include <serac/physics/solid_mechanics.hpp>
+#include <serac/physics/state/state_manager.hpp>
+#include <serac/numerics/functional/domain.hpp>
+#include <serac/numerics/stdfunction_operator.hpp>
 #include "serac/mesh/mesh_utils.hpp"
-// _incl_mesh_end
+
+#include <algorithm>
+#include <cfenv>
+#include <memory>
+#include <numeric>
+#include <functional>
+
+#include <mfem/linalg/tensor.hpp>
+
 
 // _main_init_start
 int main(int argc, char* argv[])
 {
-  /*auto [num_procs, rank] = */ serac::initialize(argc, argv);
-  axom::sidre::DataStore datastore;
-  serac::StateManager::initialize(datastore, "without_input_file_example");
-  // _main_init_end
-  // _create_mesh_start
-  auto mesh = serac::mesh::refineAndDistribute(serac::buildRectangleMesh(10, 10));
+  // Initialize Serac
+  serac::initialize(argc, argv);
+  ::axom::sidre::DataStore datastore;
+  ::serac::StateManager::initialize(datastore, "sidreDataStore");
+  
+  // Define the spatial dimension of the problem and the type of finite elements used.
+  static constexpr int ORDER {1};
+  static constexpr int DIM {3};
+  using shapeFES = serac::H1<ORDER, DIM>;
 
-  std::string mesh_tag{"mesh"};
+  // auto inputFilename = lido::meshes::connecting_rod_mesh;
+  auto inputFilename = "../../data/meshes/cylOneElemThickness.g";
+  auto mesh = serac::buildMeshFromFile(inputFilename);
+  // auto mesh = ::mfem::Mesh::MakeCartesian3D(10, 5, 1, ::mfem::Element::HEXAHEDRON, 2, 1, 0.1);
+  int numElements = 354;
 
-  serac::StateManager::setMesh(std::move(mesh), mesh_tag);
-  // _create_mesh_end
+  auto pmesh = ::mfem::ParMesh(MPI_COMM_WORLD, mesh);
+  pmesh.EnsureNodes();
+  pmesh.ExchangeFaceNbrData();
 
-  // _create_module_start
-  // Create a Heat Transfer class instance with Order 1 and Dimensions of 2
-  constexpr int order = 1;
-  constexpr int dim   = 2;
+  // Create finite element space for design parameters, and register it with LiDO DataManager
+  auto [shape_fes, shape_fec] = serac::generateParFiniteElementSpace<shapeFES>(&pmesh);
+  mfem::HypreParVector node_disp_exact(shape_fes.get());
+  mfem::HypreParVector node_disp_computed(shape_fes.get());
+  std::unique_ptr<mfem::HypreParMatrix> dresidualdu;
 
-  serac::HeatTransfer<order, dim> heat_transfer(
-      serac::heat_transfer::default_nonlinear_options, serac::heat_transfer::default_linear_options,
-      serac::heat_transfer::default_static_options, "thermal_solver", mesh_tag);
-  // _create_module_end
+  node_disp_exact.Randomize(0);
 
-  // _conductivity_start
-  constexpr double                               kappa = 0.5;
-  serac::heat_transfer::LinearIsotropicConductor mat(1.0, 1.0, kappa);
-  heat_transfer.setMaterial(mat);
+  // Define the types for the test and trial spaces using the function arguments
+  using test_space  = serac::H1<ORDER, DIM>;
+  using trial_space = serac::H1<ORDER, DIM>;
 
-  // _conductivity_end
-  // _bc_start
-  const std::set<int> boundary_constant_attributes = {1};
-  constexpr double    boundary_constant            = 1.0;
+  // Construct the new functional object using the known test and trial spaces
+  serac::Functional<test_space(trial_space)> residual(
+                shape_fes.get(), {shape_fes.get()}); // shape, solution, and residual FESs
 
-  auto ebc_func = [boundary_constant](const auto&, auto) { return boundary_constant; };
-  heat_transfer.setTemperatureBCs(boundary_constant_attributes, ebc_func);
+  residual.AddDomainIntegral(
+    serac::Dimension<DIM>{}, serac::DependsOn<0>{},
+    [=](double /*t*/, auto position, auto nodeDisp) {
+      auto [X, dXdxi] = position;
+      auto du_dX = serac::get<1>(nodeDisp);
+      auto J = dXdxi + serac::dot(du_dX, dXdxi);
+      using std::pow;
+      // auto mu = (serac::squared_norm(J) / (3 * pow(serac::det(J), 2.0 / 3.0))) - 1.0; // serac::dot(J, J)
+      auto JJ    = serac::squared_norm(J); // serac::dot(J, J)
+      auto invJT = serac::inv(serac::transpose(J));
+      auto scale = (2.0 / (3.0 * pow(serac::det(J), 2.0 / 3.0) ));
+      if (serac::det(J) <= 0.0)
+      {
+        scale = 0.0;
+      }
+      auto flux       = scale * (J - (JJ/3.0) * invJT);
+      auto source     = serac::zero{};
+      return ::serac::tuple{source, flux};
+    },
+    pmesh
+  );
 
-  const std::set<int> boundary_function_attributes = {2, 3};
-  auto                boundary_function_coef       = [](const auto& vec, auto) { return vec[0] * vec[0] + vec[1] - 1; };
-  heat_transfer.setTemperatureBCs(boundary_function_attributes, boundary_function_coef);
-  // _bc_end
+  serac::Domain radial_boundary = serac::Domain::ofBoundaryElements(pmesh, serac::by_attr<DIM>(1));
 
-  // _run_sim_start
-  heat_transfer.completeSetup();
-  heat_transfer.outputStateToDisk();
+  auto omega = 1.0e1;
+  auto radius = 1.015;
+  auto x0 = 0.0;
+  auto y0 = 0.0;
+  residual.AddBoundaryIntegral(
+    serac::Dimension<DIM - 1>{}, serac::DependsOn<0>{},
+    [=](double /*t*/, auto position, auto nodeDisp) {
+      auto [X, dXdxi] = position;
+      auto u = serac::get<0>(nodeDisp);
+      auto x = X + u;
+      auto phiVal = pow(pow(x[0]-x0, 2.0) + pow(x[1]-y0, 2.0), 0.5) - radius;
+      // auto dphidXVal = (x[0] + x[1] - 2.0)* pow( pow(x[0]-x0, 2.0) + pow(x[1]-y0, 2.0), -0.5);
+      auto dphi = 0.0*x;
+      dphi[0] = (x[0] - x0)* pow( pow(x[0]-x0, 2.0) + pow(x[1]-y0, 2.0), -0.5);
+      dphi[1] = (x[1] - y0)* pow( pow(x[0]-x0, 2.0) + pow(x[1]-y0, 2.0), -0.5);
+      return 2.0 * omega * phiVal * dphi;
+    },
+    radial_boundary // whole_boundary
+  );
 
-  heat_transfer.advanceTimestep(1.0);
-  heat_transfer.outputStateToDisk();
-  // _run_sim_end
+  // Get dofs in z direction for all elements (pseudo 2D problem)
+  int totNumDofs = shape_fes->TrueVSize();
+  mfem::Array<int> ess_tdof_list, ess_bdr(mesh.bdr_attributes.Max());
+  ess_bdr = 0;
+  ess_bdr[1] = 1;
+  ess_bdr[2] = 1;
+  shape_fes->GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
+  mfem::Array<int> dofsZdirection(totNumDofs/3);
+  int counter = 0; 
+  for(auto iDof=DIM-1; iDof<totNumDofs; iDof += DIM){
+    dofsZdirection[counter] = ess_tdof_list[iDof];
+    counter++;
+  }
 
-  // _exit_start
-  serac::exitGracefully();
+  // wrap residual and provide Jacobian
+  serac::mfem_ext::StdFunctionOperator residual_opr(
+    totNumDofs,
+    [&dofsZdirection, &residual](const mfem::Vector& u, mfem::Vector& r) {
+      double dummy_time = 1.0;
+      const mfem::Vector res = residual(dummy_time, u);
+      r = res;
+      r.SetSubVector(dofsZdirection, 0.0);
+    },
+    [&residual, &dresidualdu](const mfem::Vector& u) -> mfem::Operator& { // &node_disp_exact,
+      double dummy_time = 1.0;
+      auto [val, dr_du] = residual(dummy_time, serac::differentiate_wrt(u));
+      dresidualdu       = assemble(dr_du);
+      // dresidualdu->Print("Jacobian");
+      return *dresidualdu;
+    }
+  );
+
+  const serac::LinearSolverOptions lin_opts = {
+                                        .linear_solver = ::serac::LinearSolver::CG,
+                                        // .linear_solver  = serac::LinearSolver::Strumpack,
+                                        .preconditioner = ::serac::Preconditioner::HypreJacobi,
+                                        .relative_tol   = 0.7*1.0e-8,
+                                        .absolute_tol   = 0.7*1.0e-10,
+                                        .max_iterations = 3*numElements,
+                                        .print_level    = 1};
+
+  const serac::NonlinearSolverOptions nonlin_opts = {
+                                              // .nonlin_solver = ::serac::NonlinearSolver::Newton,
+                                              .nonlin_solver  = serac::NonlinearSolver::TrustRegion,
+                                              // .nonlin_solver  = serac::NonlinearSolver::NewtonLineSearch,
+                                              .relative_tol   = 1.0e-10,
+                                              .absolute_tol   = 1.0e-12,
+                                              .min_iterations = 1, 
+                                              .max_iterations = 50, // 2000
+                                              .max_line_search_iterations = 30, //0
+                                              .print_level    = 1};
+
+  serac::EquationSolver eq_solver(nonlin_opts, lin_opts);
+  eq_solver.setOperator(residual_opr);
+  eq_solver.solve(node_disp_computed);
+
+  mfem::ParGridFunction nodeSolGF(shape_fes.get());
+  nodeSolGF.SetFromTrueDofs(node_disp_computed);
+
+  auto pd = mfem::ParaViewDataCollection("sol_mesh_morphing_serac", &pmesh);
+  pd.RegisterField("solution", &nodeSolGF);
+  pd.SetCycle(1);
+  pd.SetTime(1);
+  pd.Save();
 }
 // _exit_end
